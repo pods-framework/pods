86fd214fe2f3b80b6e98e373ff5f066d898cb696
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-09 15:23:05 +0000

    fix

--- ui/pagination.php
+++ ui/pagination.php
@@ -8,32 +8,33 @@ $type = $this->datatype;
 // Build the $_GET string
 $request_uri = explode('?', $_SERVER['REQUEST_URI']);
 $request_uri = $request_uri[0] . '?';
+$_GET = urlencode_deep($_GET);
 foreach ($_GET as $key => $val) {
     if ('pg' != $key)
-        $request_uri .= $key . '=' . $val . '&amp;';
+        $request_uri .= urlencode($key) . '=' . $val . '&amp;';
 }
 ?>
     <span class="pager"><?php echo $label; ?>
 <?php
 if (1 < $page) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=1'); ?>" class="pageNum firstPage">1</a>
+    <a href="<?php echo $request_uri . 'pg=1'; ?>" class="pageNum firstPage">1</a>
 <?php
 }
 if (1 < ($page - 100)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page - 100)); ?>" class="pageNum"><?php echo ($page - 100); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page - 100); ?>" class="pageNum"><?php echo ($page - 100); ?></a>
 <?php
 }
 if (1 < ($page - 10)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page - 10)); ?>" class="pageNum"><?php echo ($page - 10); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page - 10); ?>" class="pageNum"><?php echo ($page - 10); ?></a>
 <?php
 }
 for ($i = 2; $i > 0; $i--) {
     if (1 < ($page - $i)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page - $i)); ?>" class="pageNum"><?php echo ($page - $i); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page - $i); ?>" class="pageNum"><?php echo ($page - $i); ?></a>
 <?php
     }
 }
@@ -43,23 +44,23 @@ for ($i = 2; $i > 0; $i--) {
 for ($i = 1; $i < 3; $i++) {
     if ($total_pages > ($page + $i)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page + $i)); ?>" class="pageNum"><?php echo ($page + $i); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page + $i); ?>" class="pageNum"><?php echo ($page + $i); ?></a>
 <?php
     }
 }
 if ($total_pages > ($page + 10)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page + 10)); ?>" class="pageNum"><?php echo ($page + 10); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page + 10); ?>" class="pageNum"><?php echo ($page + 10); ?></a>
 <?php
 }
 if ($total_pages > ($page + 100)) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . ($page + 100)); ?>" class="pageNum"><?php echo ($page + 100); ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . ($page + 100); ?>" class="pageNum"><?php echo ($page + 100); ?></a>
 <?php
 }
 if ($page < $total_pages) {
 ?>
-    <a href="<?php echo esc_url($request_uri . 'pg=' . $total_pages); ?>" class="pageNum lastPage"><?php echo $total_pages; ?></a>
+    <a href="<?php echo $request_uri . 'pg=' . $total_pages; ?>" class="pageNum lastPage"><?php echo $total_pages; ?></a>
 <?php
 }
 ?>

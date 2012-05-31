238ba6bcba592f9ac79e6a64553786af0dc1ad29
Mike Green <mike.is.green@gmail.com>
2012-05-31 15:11:00 -0400

    Added simple button HTML to the media buttons context

--- ui/admin/media_button.php
+++ ui/admin/media_button.php
@@ -4,7 +4,9 @@
  */
 
 function pods_media_button($context) {
-
+	$button = '<a href="javascript:void(0)"><img src="' . PODS_URL . 'ui/images/icon16.png" alt="Add Pod" /></a>';
+	$context .= $button;
+	return $context;
 }
 add_filter('media_buttons_context', 'pods_media_button');
 

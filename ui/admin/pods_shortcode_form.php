22fefaa68a82d3b45d9f985513f29f21e30a4011
Mike Green <mike.is.green@gmail.com>
2012-05-31 22:57:34 -0400

    Added all fields necessary for shortcode, need to populate them

--- ui/admin/pods_shortcode_form.php
+++ ui/admin/pods_shortcode_form.php
@@ -23,16 +23,63 @@ div.section {
 				<div class="section">
 					<?php
 					$api = new PodsAPI();
-					$all_pods = $api->load_pods(array());
+					$all_pods = $api->load_pods(array(
+						'orderby' => 'name ASC',
+					));
 					?>
 					<label for="pod_select">Choose a Pod</label>
 					<select id="pod_select" name="pod_select">
 						<?php foreach($all_pods as $pod => $data) { ?>
-							<option name="<?php echo $pod; ?>">
+							<option value="<?php echo $pod; ?>">
 								<?php echo $pod; ?>
 							</option>
 						<?php } ?>
 					</select>
+				</div>
+				<div class="section">
+					<label for="pod_slug">Slug</label>
+					<input type="text" id="pod_slug" name="pod_slug" />
+				</div>
+				<div class="section">
+					<label for="pod_orderby">Order By</label>
+					<input type="text" id="pod_orderby" name="pod_orderby" />
+				</div>
+				<div class="section">
+					<label for="pod_sort_direction">Direction</label>
+					<select id="pod_sort_direction" name="pod_sort_direction">
+						<option value="ASC">
+							Ascending
+						</option>
+						<option value="DESC">
+							Descending
+						</option>
+					</select>
+				</div>
+				<div class="section">
+					<?php
+					//TODO: Load all templates
+					?>
+					<label for="pod_template">Template</label>
+					<select id="pod_template" name="pod_template">
+						
+					</select>
+				</div>
+				<div class="section">
+					<label for="pod_limit">Limit</label>
+					<input type="text" id="pod_limit" name="pod_limit" />
+				</div>
+				<div class="section">
+					<label for="pod_column">Column</label>
+					<input type="text" id="pod_column" name="pod_column" />
+				</div>
+				<div class="section">
+				    <label for="pod_helper">Helper</label>
+					<select id="pod_helper" name="pod_helper">
+					
+					</select>
+				</div>
+				<div class="section">
+					<a class="button" href="#">Insert</a>
 				</div>
 			</form>
 		</div>

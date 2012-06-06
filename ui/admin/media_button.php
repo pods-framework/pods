0237f372bbce904801510bdb977788f3b5216ee0
Mike Green <mike.is.green@gmail.com>
2012-06-06 09:54:45 -0400

    Restricted shortcode form to only show up on post/page pages

--- ui/admin/media_button.php
+++ ui/admin/media_button.php
@@ -6,11 +6,15 @@
 /**
  * Add a button to the media buttons context
  */
+
+$current_page = basename($_SERVER['PHP_SELF']);
+
 function pods_media_button($context) {
     $button = '<a href="#TB_inline?inlineId=pods_shortcode_form&width=640" class="thickbox" id="add_pod_button"><img src="' . PODS_URL . 'ui/images/icon16.png" alt="Add Pod" /></a>';
     $context .= $button;
     return $context;
 }
+
 add_filter('media_buttons_context', 'pods_media_button');
 
 /**
@@ -112,6 +116,7 @@ function add_pods_mce_popup() {
     <?php
     require_once PODS_DIR . 'ui/admin/pods_shortcode_form.php';
 }
-add_action('admin_footer', 'add_pods_mce_popup');
+if (in_array($current_page, array('post.php', 'page.php', 'page-new.php', 'post-new.php')))
+	add_action('admin_footer', 'add_pods_mce_popup');
 
 ?>

8ca2b5e9bc34738755a84987cf30b8c2ac90598b
Mike Green <mike.is.green@gmail.com>
2012-06-07 15:26:51 -0400

    Finished backend interface for column widget

--- classes/widgets/PodsColumnWidget.php
+++ classes/widgets/PodsColumnWidget.php
@@ -7,8 +7,8 @@ class PodsColumnWidget extends WP_Widget {
         $this->WP_Widget(
             'pods_column_widget',
             'Pod Column',
-            array('classname' => 'pods_single_widget', 'description' => "Display a single Pod item's column value"),
-            array('width' => 400)
+            array('classname' => 'pods_column_widget', 'description' => "Display a single Pod item's column value"),
+            array('width' => 200)
         );
     }
 
@@ -24,13 +24,25 @@ class PodsColumnWidget extends WP_Widget {
      * @returns array $instance Updated instance
      */
     public function update($new_instance, $old_instance) {
-    
+        $instance = $old_instance;
+        $instance['title']    = $new_instance['title'];
+        $instance['pod_type'] = $new_instance['pod_type'];
+        $instance['slug']     = $new_instance['slug'];
+        $instance['column']   = $new_instance['column'];
+        $instance['helper']   = $new_instance['helper'];
+
+        return $instance;
     }
 
     /**
      * Widget Form
      */
     public function form($instance) {
+        $title    = $instance['title'];
+        $pod_type = $instance['pod_type'];
+        $slug     = $instance['slug'];
+        $column   = $instance['column'];
+        $helper   = $instance['helper'];
         require_once PODS_DIR . 'ui/admin/widgets/pods_column_widget_form.php'; 
     }
 }

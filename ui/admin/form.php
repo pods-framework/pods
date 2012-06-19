e2ebfa306cca83ec56001a8674d9c52efa8914ba
Mike Green <mike.is.green@gmail.com>
2012-06-19 14:18:48 -0400

    Got single plupload input use case working

--- ui/admin/form.php
+++ ui/admin/form.php
@@ -695,6 +695,19 @@
                                     ));
                                     ?>
                                 </li>
+                                
+                                <li class="pods-field pods-file pods-plupload-context" id="field-pods-field-file10">
+                                    <?php echo PodsForm::label('file10', 'Single File via Form API'); ?>
+                                    <br/>
+                                    <?php
+                                    echo PodsForm::field('file10', null, 'file', array(
+                                        'file_format_type' => 'single',
+                                        'file_uploader' => 'plupload',
+                                        'file_limit' => 1,
+                                        'file_type' => 'images'
+                                    ));
+                                    ?>
+                                </li>
 
                                 <!-- Inline plupload input -->
                                 <li class="pods-field pods-file pods-plupload-context" id="field-pods-field-file4">
--- ui/fields/plupload.php
+++ ui/fields/plupload.php
@@ -1,12 +1,35 @@
 <?php
-	$attributes = array();
-	$attributes['value'] = $value;
+    $attributes = array();
+    $attributes['value'] = $value;
     $attributes = PodsForm::merge_attributes( $attributes, $name, PodsForm::$field_type, $options );
-	$css_id = $attributes['id'];
-    $file_limit = 1;
+    $css_id = $attributes['id'];
+    $file_limit = isset($options['file_limit']) ? $options['file_limit'] : 1000;
+    $plupload_init = array(
+        'runtimes' => 'html5,silverlight,flash,html4',
+        'browse_button' => $css_id . '-browse',
+        'url' => PODS_URL . 'ui/admin/misc.php',
+        'file_data_name' => 'Filedata',
+        'multiple_queues' => false,
+        'max_file_size' => wp_max_upload_size().'b',
+        'flash_swf_url' => includes_url('js/plupload/plupload.flash.swf'),
+        'silverlight_xap_url' => includes_url('js/plupload/plupload.silverlight.xap'),
+        'filters' => array(array('title' => __('Allowed Files', 'pods'), 'extensions' => '*')),
+        'multipart' => true,
+        'urlstream_upload' => true,
+        'multipart_params' => array(
+            '_wpnonce' => wp_create_nonce('pods-wp_handle_upload_advanced'),
+            'action' => 'wp_handle_upload_advanced',
+            'method' => 'upload_file',
+            'pods_ajax' => 1,
+        ),
+    );
+    $plupload_init = apply_filters('plupload_init', $plupload_init);
 
     if ( !wp_script_is( 'plupload-all', 'queue' ) && !wp_script_is( 'plupload-all', 'to_do' ) && !wp_script_is( 'plupload-all', 'done' ) )
         wp_print_scripts( 'plupload-all' );
+
+// If this is a multi-file sortable queue...
+if ('multiple' === $options['file_format_type']):
 ?>
 <ul class="pods-files" id="<?php echo $css_id; ?>-files">
     <?php foreach($value as $val): ?>
@@ -21,26 +44,6 @@
 <p class="plupload-queue" id="<?php echo $css_id; ?>-queue">
 </p>
 <?php
-$plupload_init = array(
-    'runtimes' => 'html5,silverlight,flash,html4',
-    'browse_button' => $css_id . '-browse',
-    'url' => PODS_URL . 'ui/admin/misc.php',
-    'file_data_name' => 'Filedata',
-    'multiple_queues' => false,
-    'max_file_size' => wp_max_upload_size().'b',
-    'flash_swf_url' => includes_url('js/plupload/plupload.flash.swf'),
-    'silverlight_xap_url' => includes_url('js/plupload/plupload.silverlight.xap'),
-    'filters' => array(array('title' => __('Allowed Files', 'pods'), 'extensions' => '*')),
-    'multipart' => true,
-    'urlstream_upload' => true,
-    'multipart_params' => array(
-        '_wpnonce' => wp_create_nonce('pods-wp_handle_upload_advanced'),
-        'action' => 'wp_handle_upload_advanced',
-        'method' => 'upload_file',
-        'pods_ajax' => 1,
-    ),
-);
-$plupload_init = apply_filters('plupload_init', $plupload_init);
 ?>
 <script>
     jQuery( function ($) {
@@ -80,15 +83,15 @@ $plupload_init = apply_filters('plupload_init', $plupload_init);
 
         // Plupload UploadProgress Event Handler
         pods_uploader.bind( 'UploadProgress', function ( up, file ) {
-			var prog_bar = $('#' + file.id).find('.progress-bar');
-			prog_bar.css('width', file.percent + '%');
+            var prog_bar = $('#' + file.id).find('.progress-bar');
+            prog_bar.css('width', file.percent + '%');
         } );
 
         // Plupload FileUploaded Event Handler
         pods_uploader.bind( 'FileUploaded', function ( up, file, resp ) {
             var file_div = jQuery( '#' + file.id );
             var file_limit = <?php echo $file_limit; ?>;
-            file_div.find( '.progress-bar' ).remove();
+            file_div.remove();
 
             if ( "Error" == resp.response.substr( 0, 5 ) ) {
                 var response = resp.response.substr( 7 );
@@ -99,26 +102,118 @@ $plupload_init = apply_filters('plupload_init', $plupload_init);
                 file_div.append( resp.response );
             }
             else {
-				$.fn.reverse = [].reverse;
-				var json = eval( '(' + resp.response.match( /\{(.*)\}/gi ) + ')' ),
-					response = json,
-					sort_array = $('#<?php echo esc_js( $css_id ); ?>-files'),
-					maxFiles = <?php echo esc_js( $file_limit ); ?>;
+                $.fn.reverse = [].reverse;
+                var json = eval( '(' + resp.response.match( /\{(.*)\}/gi ) + ')' ),
+                    response = json,
+                    sort_array = $('#<?php echo esc_js( $css_id ); ?>-files'),
+                    maxFiles = <?php echo esc_js( $file_limit ); ?>;
 
-				sort_array.append('<li><span class="pods-file-reorder"><img src="' + PODS_URL + 'ui/images/handle.gif" alt="reorder"/></span><span class="pods-file-thumb"><span><img class="pinkynail" src="' + json.guid + '" /></span><input type="hidden" name="file3[]" value="' + json.ID + '" /></span><span class="pods-file-name">' + file.name + '</span><span class="pods-file-remove"><img src="' + PODS_URL + 'ui/images/del.png"/></span>');
+                sort_array.append('<li><span class="pods-file-reorder"><img src="' + PODS_URL + 'ui/images/handle.gif" alt="reorder"/></span><span class="pods-file-thumb"><span><img class="pinkynail" src="' + json.guid + '" /></span><input type="hidden" name="file3[]" value="' + json.ID + '" /></span><span class="pods-file-name">' + file.name + '</span><span class="pods-file-remove"><img src="' + PODS_URL + 'ui/images/del.png"/></span>');
 
-				var items = sort_array.find('li'), itemCount = items.size();
-				
-				if (itemCount > maxFiles) {
-					var reversed = items.reverse();
+                var items = sort_array.find('li'), itemCount = items.size();
+                
+                if (itemCount > maxFiles) {
+                    var reversed = items.reverse();
 
-					reversed.each(function(idx, elem) {
-						if (idx + 1 > maxFiles) {
-							jQuery(elem).remove();
-						}
-					});
-				}
+                    reversed.each(function(idx, elem) {
+                        if (idx + 1 > maxFiles) {
+                            jQuery(elem).remove();
+                        }
+                    });
+                }
             }
         } );
     } );
 </script>
+<?php else: ?>
+    <?php $file_limit = 1; ?>
+    <a class="button plupload-add" id="<?php echo $css_id; ?>-browse" href="">Select + Upload</a>
+    <a class="button" href="" id="<?php echo $css_id; ?>-browse-server">Browse Server</a>
+    <ul class="pods-inline-files" id="<?php echo $css_id; ?>-inline-files">
+    </ul>
+
+    <script type="text/javascript">
+    jQuery(function($) {
+        var pods_uploader = new plupload.Uploader(<?php echo json_encode( $plupload_init ); ?>);
+        pods_uploader.init();
+
+        pods_uploader.bind('FilesAdded', function(up, files) {
+            var queue = $('#<?php echo esc_js( $css_id ); ?>-inline-files'),
+                items = queue.find('li'),
+                itemCount = items.size(),
+                fileCount = files.length,
+                maxFiles = <?php echo esc_js( $file_limit ); ?>;
+
+            $.each(files, function(idx, file) {
+                var list_item = $('<li/>'),
+                    prog_wrap = $('<div/>', {
+                        'class': 'plupload-progress',
+                        'id': file.id
+                    }),
+                    prog_name = $('<span/>', {
+                        'class': 'file-name',
+                        text: file.name
+                    }),
+                    prog_bar = $('<span/>', {
+                        'class': 'progress-bar',
+                        css: {
+                            width: '0%'
+                        }
+                    });
+                prog_wrap.append(prog_name).append(prog_bar).appendTo(list_item);
+                list_item.appendTo(queue);
+            });
+
+            if (itemCount + fileCount > maxFiles) {
+                $.fn.reverse = [].reverse;
+                var reversed = queue.find('li').reverse();
+
+                reversed.each(function(idx, elem) {
+                    if (idx + 1 > maxFiles) {
+                        $(elem).remove();
+                    }
+                });
+            }
+
+            up.refresh();
+            up.start();
+        });
+
+        pods_uploader.bind('UploadProgress', function(up, file) {
+            var upbar = $('#' + file.id),
+                prog  = upbar.find('.progress-bar');
+            prog.css('width', file.percent + '%');
+        });
+
+        pods_uploader.bind('FileUploaded', function(up, file, resp) {
+            var upbar = $('#' + file.id),
+                prog = upbar.find('.progress-bar'),
+                response = resp.response;
+
+            if (response.substr(0, 5) == "Error") {
+                var div = response.substr(7);
+                alert($(div).text());
+                upbar.remove();
+            } else if (response.substr(0, 3) == "<e>") {
+                var r = response.substr(4);
+                alert(r);
+                upbar.remove();
+            } else {
+                var json = $.parseJSON(response);
+                var input = $('<input/>', {
+                    'type': 'hidden',
+                    name: "<?php echo esc_js( $field_name ); ?>[]",
+                    value: json.ID
+                });
+                prog.remove();
+                upbar.prepend('<span class="remove"><img src="<?php echo PODS_URL; ?>ui/images/del.png" alt="remove" /></span>');
+                upbar.append(input);
+            }
+        });
+
+        $('span.remove').live('click', function(evt) {
+            $(this).closest('li').remove();
+        });
+    });
+    </script>
+<?php endif; ?>

fef624c0430bf4a75527281df5ae4b8d3e257539
Mike Green <mike.is.green@gmail.com>
2012-07-13 13:39:06 -0400

    Added page, template, and helper migration functions

--- sql/update.php
+++ sql/update.php
@@ -43,7 +43,53 @@ function migrate_pods() {
 }
 
 function migrate_templates() {
+	$api = new PodsAPI;
+	$templates = pods_query("SELECT * FROM `@wp_pod_templates`");
+	$results = array();
 
+	if (0 == count($templates))
+		return true;
+
+	foreach ($templates as $tpl) {
+		$params = array(
+			'name' => $tpl['name'],
+			'code' => $tpl['code'],
+		);
+
+		$results[] = $api->save_template($params);
+	}
+
+	return $results;
+}
+
+function migrate_helpers() {
+	$api = new PodsAPI;
+	$results = array();
+	$helpers = pods_query("SELECT * FROM `@wp_pod_helpers`");
+
+	foreach ($helpers as $hlpr) {
+		$params = array(
+			'name' => $hlpr['name'],
+			'helper_type' => $hlpr['helper_type'],
+			'phpcode' => $hlpr['phpcode'],
+		);
+
+		$results[] = $api->save_helper($params);
+	}
+
+	return $results;
+}
+
+function migrate_pages() {
+	$api = new PodsAPI;
+	$results = array();
+	$pages = pods_query("SELECT * FROM `@wp_pod_pages`");
+
+	foreach ($pages as $page) {
+		$results[] = $api->save_page($page);
+	}
+
+	return $results;
 }
 
 if (version_compare($pods_version, '2.0.0', '<')) {

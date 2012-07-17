ba0a51a46bc1dfd3a2b411cd73365e8461bd9f11
Mike Green <mike.is.green@gmail.com>
2012-07-17 12:19:34 -0400

    I18n-ed the widget forms in ui/admin

--- ui/admin/widgets/form.php
+++ ui/admin/widgets/form.php
@@ -12,7 +12,7 @@
 
 <ol class="pods_form_widget_form">
     <li>
-		<label for="<?php echo $this->get_field_id('title'); ?>">Title</label>
+	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'pods')</label>
 		<input class="widefat" type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>">
     </li>
     <li>
@@ -20,7 +20,7 @@
 		$api = new PodsAPI();
 		$all_pods = $api->load_pods(array());
 		?>
-		<label for="<?php echo $this->get_field_id('pod_type'); ?>">Pod Type</label>
+		<label for="<?php echo $this->get_field_id('pod_type'); ?>"><?php _e('Pod Type', 'pods'); ?></label>
 		<?php if (0 < count($all_pods)): ?>
 			<select id="<?php echo $this->get_field_id('pod_type'); ?>" name="<?php echo $this->get_field_name('pod_type'); ?>">
 				<?php foreach ($all_pods as $pod): ?>
--- ui/admin/widgets/single.php
+++ ui/admin/widgets/single.php
@@ -12,7 +12,7 @@
 
 <ol class="pods_single_widget_form">
     <li>
-        <label for="<?php echo $this->get_field_id( 'title' ); ?>"> Title </label>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"> <?php _e('Title', 'pods'); ?></label>
         <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $title; ?>" />
     </li>
 
@@ -21,7 +21,7 @@
             $api = new PodsAPI();
             $all_pods = $api->load_pods( array() );
         ?>
-        <label for="<?php echo $this->get_field_id( 'pod_type' ); ?>"> Pod Type </label>
+		<label for="<?php echo $this->get_field_id( 'pod_type' ); ?>"> <?php _e('Pod Type', 'pods'); ?> </label>
         <?php if ( 0 < count( $all_pods ) ): ?>
             <select id="<?php $this->get_field_id( 'pod_type' ); ?>" name="<?php echo $this->get_field_name( 'pod_type' ); ?>">
                 <?php foreach ( $all_pods as $pod ): ?>
@@ -32,7 +32,7 @@
                 <?php endforeach; ?>
             </select>
         <?php else: ?>
-            <strong class="red">None Found</strong>
+			<strong class="red"><?php _e('None Found', 'pods'); ?></strong>
         <?php endif; ?>
     </li>
 
@@ -40,7 +40,7 @@
         <?php
             $all_templates = (array) $api->load_templates( array() );
         ?>
-        <label for="<?php echo $this->get_field_id( 'template' ); ?>"> Template </label>
+		<label for="<?php echo $this->get_field_id( 'template' ); ?>"> <?php _e('Template', 'pods'); ?> </label>
         <select name="<?php echo $this->get_field_name( 'template' ); ?>" id="<?php echo $this->get_field_id( 'template' ); ?>">
             <option value="">- Custom Template -</option>
             <?php foreach ( $all_templates as $tpl ): ?>
@@ -53,7 +53,7 @@
     </li>
 
     <li>
-        <label for="<?php echo $this->get_field_id( 'template_custom' ); ?>"> Custom Template </label>
+		<label for="<?php echo $this->get_field_id( 'template_custom' ); ?>"> <?php _e('Custom Template', 'pods'); ?> </label>
         <textarea name="<?php echo $this->get_field_name( 'template_custom' ); ?>" id="<?php echo $this->get_field_id( 'template_custom' ); ?>" cols="10" rows="10" class="widefat"><?php echo esc_html( $template_custom ); ?></textarea>
     </li>
 </ol>

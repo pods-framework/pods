bb9ed8d62f4e54676fe1e865293dea99d9609f4d
Mike Green <mike.is.green@gmail.com>
2012-07-17 11:36:33 -0400

    Fixed and added I18n strings to half the files in ui/admin

--- ui/admin/advanced.php
+++ ui/admin/advanced.php
@@ -8,27 +8,27 @@
 			<div id="pods-parts-right" class="pods right">
 				<div id="side-sortables">
 					<div id="pods-parts-pages" class="postbox">
-						<h3><span>Pods Pages</span></h3>
+						<h3><span><?php _e( 'Pods Pages', 'pods' ); ?></span></h3>
 						<div class="inside">
-							<a href="#" id="pods-parts-pages-edit" class="pods-parts-icon-edit pods-parts-icon">Edit Pods Pages</a>
+							<a href="#" id="pods-parts-pages-edit" class="pods-parts-icon-edit pods-parts-icon"><?php _e( 'Edit Pods Pages', 'pods' ); ?></a>
 							<br />
-							<a href="#" id="pods-parts-pages-add" class="pods-parts-icon-add pods-parts-icon">Add New Pods Page</a>
+							<a href="#" id="pods-parts-pages-add" class="pods-parts-icon-add pods-parts-icon"><?php _e( 'Add New Pods Page', 'pods' ); ?></a>
 						</div>
 					</div><!-- #pods-parts-pages -->
 					<div id="pods-parts-templates" class="postbox">
-						<h3><span>Pods Templates</span></h3>
+						<h3><span><?php _e( 'Pods Templates', 'pods' ); ?></span></h3>
 						<div class="inside">
-							<a href="#" id="pods-parts-templates-edit" class="pods-parts-icon-edit pods-parts-icon">Edit Pods Templates</a>
+							<a href="#" id="pods-parts-templates-edit" class="pods-parts-icon-edit pods-parts-icon"><?php _e( 'Edit Pods Templates', 'pods' ); ?></a>
 							<br />
-							<a href="#" id="pods-parts-templates-add" class="pods-parts-icon-add pods-parts-icon">Add New Pods Template</a>
+							<a href="#" id="pods-parts-templates-add" class="pods-parts-icon-add pods-parts-icon"><?php _e( 'Add New Pods Template', 'pods' ); ?></a>
 						</div>
 					</div><!-- #pods-parts-templates -->
 					<div id="pods-parts-helpers" class="postbox">
-						<h3><span>Pods Helpers</span></h3>
+						<h3><span><?php _e( 'Pods Helpers', 'pods' ); ?></span></h3>
 						<div class="inside">
-							<a href="#" id="pods-parts-helpers-edit" class="pods-parts-icon-edit pods-parts-icon">Edit Pods Helpers</a>
+							<a href="#" id="pods-parts-helpers-edit" class="pods-parts-icon-edit pods-parts-icon"><?php _e( 'Edit Pods Helpers', 'pods' ); ?></a>
 							<br />
-							<a href="#" id="pods-parts-helpers-add" class="pods-parts-icon-add pods-parts-icon">Add New Pods Helper</a>
+							<a href="#" id="pods-parts-helpers-add" class="pods-parts-icon-add pods-parts-icon"><?php _e( 'Add New Pods Helper', 'pods' ); ?></a>
 						</div>
 					</div><!-- #pods-parts-helpers -->
 				</div><!-- #side-sortables -->

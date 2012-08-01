5e2a7fe25ab9574de9e91b6631b0467def1a1ac8
Scott Kingsley Clark <lol@scottkclark.com>
2012-08-01 15:50:03 -0500

    $search_val flip update

--- classes/PodsAPI.php
+++ classes/PodsAPI.php
@@ -915,7 +915,10 @@ class PodsAPI {
 
             if ( !empty( $lookup_option ) && !empty( $lookup_built_in ) && isset( $built_in[ $lookup_built_in ] ) ) {
                 foreach ( $built_in[ $lookup_built_in ] as $built_in_object => $val ) {
-                    $search_val = 1 ^ $val;
+                    $search_val = 1;
+
+                    if ( 1 == $val )
+                        $search_val = 0;
 
                     $query = "SELECT p.ID FROM {$wpdb->posts} AS p
                                 LEFT JOIN {$wpdb->postmeta} AS pm ON pm.post_id = p.ID AND pm.meta_key = '{$lookup_option}'

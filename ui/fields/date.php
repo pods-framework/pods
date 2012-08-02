2642f7173bd48c241eb194bb23043cc186d87ca0
Scott Kingsley Clark <lol@scottkclark.com>
2012-08-02 16:16:46 -0500

    HTML5 optional date field fix

--- ui/fields/date.php
+++ ui/fields/date.php
@@ -80,16 +80,18 @@
     $date = DateTime::createFromFormat( $format, (string) $value );
     $date_default = DateTime::createFromFormat( 'Y-m-d H:i:s', (string) $value );
 
-    $formatted_date = $value;
+    if ( 'text' != $type ) {
+        $formatted_date = $value;
 
-    if ( false !== $date )
-        $value = $date->format( $html5_format );
-    elseif ( false !== $date_default )
-        $value = $date_default->format( $html5_format );
-    elseif ( !empty( $value ) )
-        $value = date_i18n( $html5_format, strtotime( (string) $value ) );
-    else
-        $value = date_i18n( $html5_format );
+        if ( false !== $date )
+            $value = $date->format( $html5_format );
+        elseif ( false !== $date_default )
+            $value = $date_default->format( $html5_format );
+        elseif ( !empty( $value ) )
+            $value = date_i18n( $html5_format, strtotime( (string) $value ) );
+        else
+            $value = date_i18n( $html5_format );
+    }
 
     $args = apply_filters( 'pods_form_ui_field_date_args', $args, $type, $options, $attributes, $name, PodsForm::$field_type );
 
@@ -101,6 +103,10 @@
 <script>
     jQuery( function () {
         var <?php echo pods_clean_name( $attributes[ 'id' ] ); ?>_args = <?php echo json_encode($args); ?>;
+
+        <?php
+            if ( 'text' != $type ) {
+        ?>
 
         if ( 'undefined' == typeof pods_test_date_field_<?php echo $type; ?> ) {
         // Test whether or not the browser supports date inputs
@@ -130,5 +136,16 @@
             jQuery( 'input#<?php echo $attributes[ 'id' ]; ?>' ).val( '<?php echo $formatted_date; ?>' );
             jQuery( 'input#<?php echo $attributes[ 'id' ]; ?>' ).<?php echo $method; ?>( <?php echo pods_clean_name( $attributes[ 'id' ] ); ?>_args );
         }
+
+        <?php
+            }
+            else {
+        ?>
+
+        jQuery( 'input#<?php echo $attributes[ 'id' ]; ?>' ).<?php echo $method; ?>( <?php echo pods_clean_name( $attributes[ 'id' ] ); ?>_args );
+
+        <?php
+            }
+        ?>
     } );
 </script>

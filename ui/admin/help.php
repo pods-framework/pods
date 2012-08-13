3e256da4f6f4d63bfa23678264b6607be1fe0fb4
Scott Kingsley Clark <lol@scottkclark.com>
2012-08-13 09:17:37 -0500

    Help text update

--- ui/admin/help.php
+++ ui/admin/help.php
@@ -2,10 +2,11 @@
     <div id="icon-pods" class="icon32"><br /></div>
     <h2><?php _e( 'Pods Help', 'pods' ); ?></h2>
     <p>
-        This plugin is an <strong>alpha</strong> version, it is not meant for use on production/live sites and is not 100% complete with functionality yet.
-        This plugin has been released early to developers so they can help test, review the new UI, and provide feedback regarding Pods 2.0
+        This plugin is an <strong>beta</strong> version, it is not meant for use on production/live sites and is not 100% complete with functionality yet.
+        This plugin has been released for developers and users so they can help test, review the new UI, and provide feedback regarding Pods 2.0 and let us know of any bugs they come across.
     </p>
-    <p>To report bugs or request features, go to our <a href="https://github.com/pods-framework/pods/issues?milestone=1&sort=created&direction=desc&state=open" target="_blank">GitHub</a></p>
+    <p>To report bugs or request features, go to our <a href="https://github.com/pods-framework/pods/issues?milestone=1&sort=created&direction=desc&state=open" target="_blank">GitHub</a>.</p>
+    <p>It's open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?</p>
 
     <hr />
     <h2><?php _e( 'Pods Developer Tools', 'pods' ); ?></h2>

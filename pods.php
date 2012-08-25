911f1f36a55dc5bd348e3903c505ac55fe6a52b2
Scott Kingsley Clark <lol@scottkclark.com>
2012-08-25 13:25:45 -0500

    Testing a fix for #296

--- pods.php
+++ pods.php
@@ -70,7 +70,7 @@ if ( !defined( 'PODS_VERSION' ) && !defined( 'PODS_DIR' ) ) {
             'raw_url' => 'https://raw.github.com/' . $user . '/' . $repo . '/' . $branch, // the github raw url of your github repo
             'github_url' => 'https://github.com/' . $user . '/' . $repo, // the github url of your github repo
             'zip_url' => 'https://github.com/' . $user . '/' . $repo . '/zipball/' . $branch, // the zip url of the github repo
-            'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+            'sslverify' => false, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
             'requires' => '3.4', // which version of WordPress does your plugin require?
             'tested' => '3.4.1', // which version of WordPress is your plugin tested up to?
             'version' => $version

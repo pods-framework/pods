38ab85ad887d92a3fba72f6a3093d8482e11f0a5
Dan Stefan Oprean <dan@designxenon.com>
2012-09-14 04:39:48 +0300

    Actually call the change trigger when collapsing a row on the rows children, should make js performance problems more easy to debug while really not affecting anything.

--- ui/js/jquery.pods.js
+++ ui/js/jquery.pods.js
@@ -894,6 +894,9 @@
                     $row.toggleClass( 'pods-manage-row-expanded' );
                     $row_label.prop( 'colspan', '3' );
                     $row_content.slideDown();
+                    $row_content.find('.pods-dependency .pods-dependent-toggle' ).each( function () {
+                       $( this ).trigger( 'change' );
+                    });
                 }
 
                 $( this ).css( 'cursor', 'pointer' );

e3b0b34ed5680e4e3e645a2c499c78e10e8757b4
Scott Kingsley Clark <lol@scottkclark.com>
2012-09-19 14:13:07 -0500

    Fix relationship selection in field editor

--- ui/admin/setup-edit-field.php
+++ ui/admin/setup-edit-field.php
@@ -1,7 +1,12 @@
 <?php
     $field = array_merge( $field_settings[ 'field_defaults' ], $field );
+
+    $data = array(
+        'row' => $pods_i,
+        'pick-object' => trim( pods_var( 'pick_object', $field ) . '-' . pods_var( 'pick_val', $field ), '-' )
+    );
 ?>
-<tr id="row-<?php echo $pods_i; ?>" class="pods-manage-row pods-field-init pods-field-<?php echo esc_attr( pods_var( 'name', $field ) ) . ( '--1' === $pods_i ? ' flexible-row' : ' pods-submittable-fields' ); ?>" valign="top" data-row="<?php echo $pods_i; ?>">
+<tr id="row-<?php echo $pods_i; ?>" class="pods-manage-row pods-field-init pods-field-<?php echo esc_attr( pods_var( 'name', $field ) ) . ( '--1' === $pods_i ? ' flexible-row' : ' pods-submittable-fields' ); ?>" valign="top"<?php PodsForm::data( $data ); ?>>
     <th scope="row" class="check-field pods-manage-sort">
         <img src="<?php echo PODS_URL; ?>ui/images/handle.gif" alt="<?php esc_attr_e( 'Move', 'pods' ); ?>" />
     </th>

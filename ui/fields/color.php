d4eb96974e96d04b817594ae95da9e0fa6120ebf
Scott Kingsley Clark <lol@scottkclark.com>
2012-09-20 17:02:12 -0500

    Fix Color Field JS

--- ui/fields/color.php
+++ ui/fields/color.php
@@ -20,36 +20,36 @@ $attributes = PodsForm::merge_attributes( $attributes, $name, PodsForm::$field_t
         var pods_farbastic_changing = false;
     }
 
-    jQuery( 'document' ).ready( function () {
-            jQuery( '#color_<?php echo $attributes[ 'id' ]; ?>' ).hide();
+    jQuery( function () {
+        jQuery( '#color_<?php echo $attributes[ 'id' ]; ?>' ).hide();
 
-            var pods_farbtastic_<?php echo pods_clean_name( $attributes[ 'id' ] ); ?> = jQuery.farbtastic(
-                    '#color_<?php echo $attributes[ 'id' ]; ?>',
-                    function ( color ) {
-                        pods_pickColor( '#<?php echo $attributes[ 'id' ]; ?>', color );
-                    }
-            );
-
-            jQuery( '#<?php echo $attributes[ 'id' ]; ?>' ).on( 'focus blur', function () {
-                jQuery( '#color_<?php echo $attributes[ 'id' ]; ?>' ).slideToggle();
-            } );
-
-            jQuery( '#<?php echo $attributes[ 'id' ]; ?>' ).on( 'keyup', function () {
-                var color = jQuery( this ).val();
-
-                pods_farbastic_changing = true;
-
-                if ( '' != color.replace( '#', '' ) && color.match( '#' ) )
-                    pods_farbtastic_<?php echo pods_clean_name( $attributes[ 'id' ] ); ?>.setColor( color );
-
-                pods_farbastic_changing = false;
-            } );
-
-            if ( 'undefined' == pods_pickColor ) {
-                function pods_pickColor ( id, color ) {
-                    if ( !pods_farbastic_changing )
-                        jQuery( id ).val( color.toUpperCase() );
+        var pods_farbtastic_<?php echo pods_clean_name( $attributes[ 'id' ] ); ?> = jQuery.farbtastic(
+                '#color_<?php echo $attributes[ 'id' ]; ?>',
+                function ( color ) {
+                    pods_pickColor( '#<?php echo $attributes[ 'id' ]; ?>', color );
                 }
+        );
+
+        jQuery( '#<?php echo $attributes[ 'id' ]; ?>' ).on( 'focus blur', function () {
+            jQuery( '#color_<?php echo $attributes[ 'id' ]; ?>' ).slideToggle();
+        } );
+
+        jQuery( '#<?php echo $attributes[ 'id' ]; ?>' ).on( 'keyup', function () {
+            var color = jQuery( this ).val();
+
+            pods_farbastic_changing = true;
+
+            if ( '' != color.replace( '#', '' ) && color.match( '#' ) )
+                pods_farbtastic_<?php echo pods_clean_name( $attributes[ 'id' ] ); ?>.setColor( color );
+
+            pods_farbastic_changing = false;
+        } );
+
+        if ( 'undefined' == pods_pickColor ) {
+            function pods_pickColor ( id, color ) {
+                if ( !pods_farbastic_changing )
+                    jQuery( id ).val( color.toUpperCase() );
             }
+        }
     } );
 </script>

a83f9f0e5e2d82e899ffc64bfc347fbde6a60629
Scott Kingsley Clark <lol@scottkclark.com>
2012-10-17 23:59:39 -0500

    Fix widget output notice

--- ui/front/widgets.php
+++ ui/front/widgets.php
@@ -4,6 +4,6 @@
     if ( !empty( $title ) )
         echo $before_title . $title . $after_title;
 
-    echo pods_shortcode( $args, $content );
+    echo pods_shortcode( $args, ( isset( $content ) ? $content : null ) );
 
     echo $after_widget;

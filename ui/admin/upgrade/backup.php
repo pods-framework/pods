e2f3380c6187c317874816290b5eae9e815c5231
Scott Kingsley Clark <lol@scottkclark.com>
2012-11-27 18:31:37 -0600

    Abstract backup information across all upgrade screens

---
+++ ui/admin/upgrade/backup.php
@@ -0,0 +1,24 @@
+<p class="padded"><?php _e( 'We recommend that you back your database up, it can really save you in a bind or a really weird situation that you may not be expecting. Check out a few options we think are <em>great</em> below.', 'pods' ); ?></p>
+
+<div id="pods-wizard-options">
+    <div class="pods-wizard-option">
+        <a href="http://ithemes.com/member/go.php?r=31250&i=l44" target="_blank"> <img src="<?php echo PODS_URL; ?>ui/images/logo_backupbuddy.png" alt="Backup Buddy" />
+
+            <p><?php _e( 'Receive 25% off', 'pods' ); ?></p>
+
+            <p><?php _e( 'Coupon Code', 'pods' ); ?>: <strong>PODS25</strong></p>
+        </a>
+
+        <p><em><?php _e( 'The all-in-one WordPress backup plugin to easily backup, restore, and migrate to any number of local or external locations.', 'pods' ); ?></em></p>
+    </div>
+    <div class="pods-wizard-option">
+        <a href="http://vaultpress.com/podsframework/" target="_blank"> <img src="<?php echo PODS_URL; ?>ui/images/logo_vaultpress.png" alt="Vaultpress" />
+
+            <p><?php _e( '1 free month', 'pods' ); ?></p>
+
+            <p><strong><?php _e( 'Click to sign up', 'pods' ); ?></strong></p>
+        </a>
+
+        <p><em><?php _e( 'A service that provides realtime continuous backups, restores, and security scanning.', 'pods' ); ?></em></p>
+    </div>
+</div>

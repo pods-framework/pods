26d9166a454e75dba357fed17220091ddde2a624
Scott Kingsley Clark <lol@scottkclark.com>
2012-12-17 09:52:10 -0600

    get_called_class doesn't belong here..

--- deprecated/deprecated.php
+++ deprecated/deprecated.php
@@ -22,32 +22,6 @@ if (!function_exists('json_encode')) {
     }
 }
 
-// Get Called Class
-if (!function_exists('get_called_class')) {
-    function get_called_class () {
-        $bt = debug_backtrace();
-        $l = 0;
-        do {
-            $l++;
-            $lines = file($bt[$l]['file']);
-            $callerLine = $lines[$bt[$l]['line'] - 1];
-            preg_match('/([a-zA-Z0-9\_]+)::' . $bt[$l]['function'] . '/',
-                       $callerLine,
-                       $matches);
-
-            if ($matches[1] == 'self') {
-                $line = $bt[$l]['line'] - 1;
-                while ($line > 0 && strpos($lines[$line], 'class') === false) {
-                    $line--;
-                }
-                preg_match('/class[\s]+(.+?)[\s]+/si', $lines[$line], $matches);
-            }
-        }
-        while ($matches[1] == 'parent' && $matches[1]);
-        return $matches[1];
-    }
-}
-
 /**
  * Mapping function to new function name (following normalization of function names from pod_ to pods_)
  *

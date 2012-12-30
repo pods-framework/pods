84c122bf8177e022a5af285d27789e9714aae43d
Scott Kingsley Clark <lol@scottkclark.com>
2012-12-29 20:05:45 -0600

    Fixes 3920

--- components/Pages.php
+++ components/Pages.php
@@ -213,7 +213,7 @@ class Pods_Pages extends PodsComponent {
             if ( is_object( $id ) ) {
                 $old_post = $id;
 
-                pods_transient_clear( 'pods_object_page_' . $old_post->post_name );
+                pods_transient_clear( 'pods_object_page_' . $old_post->post_title );
                 pods_cache_clear( $old_post->post_title, 'pods_object_page_wildcard' );
             }
         }
@@ -221,8 +221,7 @@ class Pods_Pages extends PodsComponent {
         if ( $this->object_type != $post->post_type )
             return;
 
-        pods_transient_clear( 'pods_object_page' );
-        pods_transient_clear( 'pods_object_page_' . $post->post_name );
+        pods_transient_clear( 'pods_object_page_' . $post->post_title );
         pods_cache_clear( $post->post_title, 'pods_object_page_wildcard' );
     }
 

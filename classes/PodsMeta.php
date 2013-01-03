60d3b83f63886370d99d8aa0043c19f96734c4d3
Scott Kingsley Clark <lol@scottkclark.com>
2013-01-03 08:36:58 -0600

    Fixes pods_meta_save check for post meta integration

--- classes/PodsMeta.php
+++ classes/PodsMeta.php
@@ -677,7 +677,7 @@ class PodsMeta {
 
         $blacklisted_types = apply_filters( 'pods_meta_save_post_blacklist_types', $blacklisted_types, $post_id, $post );
 
-        if ( empty( $_POST ) || 1 == pods_var( 'pods_meta_save', 'post' ) )
+        if ( empty( $_POST ) || 1 != pods_var( 'pods_meta_save', 'post' ) )
             return $post_id;
 
         // @todo Figure out how to hook into autosave for saving meta

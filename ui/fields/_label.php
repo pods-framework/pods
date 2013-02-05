2c7853ba199ae42904b07aa12fca8a76f02f3ad3
Scott Kingsley Clark <lol@scottkclark.com>
2013-02-05 12:23:26 -0600

    Fixes label to show required* in post forms

--- ui/fields/_label.php
+++ ui/fields/_label.php
@@ -2,7 +2,7 @@
     <?php
     echo $label;
 
-    if ( 1 == pods_var( 'required', $options, 0 ) )
+    if ( 1 == pods_var( 'required', $options, pods_var( 'options', $options, $options ) ) )
         echo '<abbr title="required" class="required">*</abbr>';
 
     if ( 0 == pods_var( 'grouped', $options, 0, null, true ) && !empty( $help ) && 'help' != $help )

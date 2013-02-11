5513f7615ef1b0baeacff34efdbb57ce78668eae
Scott Kingsley Clark <lol@scottkclark.com>
2013-02-11 16:43:48 -0600

    Pods 2.3 Alpha 8

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 Plugin Name: Pods - Custom Content Types and Fields
 Plugin URI: http://pods.io/
 Description: Pods is a framework for creating, managing, and deploying customized content types and fields
-Version: 2.3 Alpha 7
+Version: 2.3 Alpha 8
 Author: Pods Framework Team
 Author URI: http://pods.io/about/
 Text Domain: pods
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 // Prevent conflicts with Pods 1.x
 if ( !defined( 'PODS_VERSION' ) && !defined( 'PODS_DIR' ) && !function_exists( 'pods_ui_manage' ) ) {
-    define( 'PODS_VERSION', '2.3-a-7' );
+    define( 'PODS_VERSION', '2.3-a-8' );
 
     // Version tracking between DB updates themselves
     define( 'PODS_DB_VERSION', '2.0.3' );

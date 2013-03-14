47bf376bcbabb5be80480e6fd87fa0a0186f68aa
Scott Kingsley Clark <lol@scottkclark.com>
2013-03-13 20:21:37 -0500

    Pods 2.3 Beta 2 - Caching improvements and less queries

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 Plugin Name: Pods - Custom Content Types and Fields
 Plugin URI: http://pods.io/
 Description: Pods is a framework for creating, managing, and deploying customized content types and fields
-Version: 2.3 Beta 1
+Version: 2.3 Beta 2
 Author: Pods Framework Team
 Author URI: http://pods.io/about/
 Text Domain: pods
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 // Prevent conflicts with Pods 1.x
 if ( !defined( 'PODS_VERSION' ) && !defined( 'PODS_DIR' ) && !function_exists( 'pods_ui_manage' ) ) {
-    define( 'PODS_VERSION', '2.3-b-1' );
+    define( 'PODS_VERSION', '2.3-b-2' );
 
     // Version tracking between DB updates themselves
     define( 'PODS_DB_VERSION', '2.3-b-1' ); // @todo Update this to 2.3 when it's released

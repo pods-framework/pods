499756e39df017e0cfd388cbd714d04c90a07f0b
Scott Kingsley Clark <lol@scottkclark.com>
2013-06-26 22:36:06 -0500

    Fixes #1497

--- sql/update.php
+++ sql/update.php
@@ -23,31 +23,6 @@ if ( version_compare( $pods_version, '2.3', '<' ) ) {
 
     $_GET = $oldget;
 
-    // Set autoload on all necessary options to avoid extra queries
-    $autoload_options = array(
-        'pods_framework_version' => '',
-        'pods_framework_version_last' => '',
-        'pods_framework_db_version' => '',
-        'pods_framework_upgraded_1_x' => '0',
-        'pods_version' => '',
-
-        'pods_component_settings' => '',
-
-        'pods_disable_file_browser' => '0',
-        'pods_files_require_login' => '1',
-        'pods_files_require_login_cap' => '',
-        'pods_disable_file_upload' => '0',
-        'pods_upload_require_login' => '1',
-        'pods_upload_require_login_cap' => ''
-    );
-
-    foreach ( $autoload_options as $option_name => $default ) {
-        $option_value = get_option( $option_name, $default );
-
-        delete_option( $option_name );
-        add_option( $option_name, $option_value, '', 'yes' );
-    }
-
     update_option( 'pods_framework_version', '2.3' );
 }
 
@@ -75,4 +50,32 @@ if ( version_compare( $pods_version, '2.3.5', '<' ) ) {
     }
 
     update_option( 'pods_framework_version', '2.3.5' );
+}
+
+// Update to 2.3.9
+if ( version_compare( $pods_version, '2.3.9-a-1', '<' ) ) {
+    // Set autoload on all necessary options to avoid extra queries
+    $autoload_options = array(
+        'pods_framework_version' => '',
+        'pods_framework_version_last' => '',
+        'pods_framework_db_version' => '',
+        'pods_framework_upgraded_1_x' => '0',
+        'pods_version' => '',
+        'pods_component_settings' => '',
+        'pods_disable_file_browser' => '0',
+        'pods_files_require_login' => '1',
+        'pods_files_require_login_cap' => '',
+        'pods_disable_file_upload' => '0',
+        'pods_upload_require_login' => '1',
+        'pods_upload_require_login_cap' => ''
+    );
+
+    foreach ( $autoload_options as $option_name => $default ) {
+        $option_value = get_option( $option_name, $default );
+
+        delete_option( $option_name );
+        add_option( $option_name, $option_value, '', 'yes' );
+    }
+
+    update_option( 'pods_framework_version', '2.3.9-a-1' );
 }

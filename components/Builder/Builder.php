9b4d178ee5cdd0af5f83bccf0f0b153027b288e0
Scott Kingsley Clark <lol@scottkclark.com>
2013-08-29 08:53:44 -0500

    Include Builder 'PodsView' module, previously the file wasn't included

--- components/Builder/Builder.php
+++ components/Builder/Builder.php
@@ -20,6 +20,7 @@ if ( !function_exists( 'pods_builder_modules_init' ) ) {
         require_once( PODS_DIR . 'components/Builder/modules/form/PodsBuilderModuleForm.php' );
         require_once( PODS_DIR . 'components/Builder/modules/list/PodsBuilderModuleList.php' );
         require_once( PODS_DIR . 'components/Builder/modules/single/PodsBuilderModuleSingle.php' );
+        require_once( PODS_DIR . 'components/Builder/modules/view/PodsBuilderModuleView.php' );
     }
     add_action( 'builder_modules_loaded', 'pods_builder_modules_init' );
 }

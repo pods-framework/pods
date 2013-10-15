3836a95e69afc52a72619539629b7819dfb8ea7c
Josh <JPollock412@gmail.com>
2013-10-14 22:02:19 -0400

    List of support resources in help admin page.

--- ui/admin/help.php
+++ ui/admin/help.php
@@ -3,10 +3,19 @@
     <h2><?php _e( 'Pods Help', 'pods' ); ?></h2>
     <img src="<?php echo PODS_URL; ?>ui/images/pods-logo-notext-rgb-transparent.png" class="pods-leaf-watermark-right" />
 
-    <p>To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.</p>
+    <h3>Pods Support</h3>
 
-    <p>It's open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?</p>
+    <p>There are many resources available to help you learn to use Pods on our site, <a href="http://pods.io/" target="_blank">Pods.io</a>:</p>
+    <ul class="ul-disc">
+        <li>To learn more about using Pods, see the <a href="http://pods.io/docs/" target="_blank">documentation</a>, or our <a href="http://pods.io/tutorials/" target="_blank">tutorials section</a>.
 
+        <li>To get help with a specific issue, you can ask in our <a href="http://pods.io/forums/" target="_blank">support forums</a>, or in <a href="http://pods.io/forums/chat/" target="_blank">our chat</a>.
+
+        <li>To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.</li>
+
+        <li>Pods is open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?</li>
+    </ul>
+    
     <hr />
 
     <h3>Recommended Plugins</h3>

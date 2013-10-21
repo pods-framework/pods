6fe47de04cd34564db536d84384ef55215b2aaf7
Scott Kingsley Clark <lol@scottkclark.com>
2013-10-21 05:57:14 -0500

    Fixes #1788

--- ui/admin/form.php
+++ ui/admin/form.php
@@ -218,7 +218,7 @@ if ( 0 < $pod->id() ) {
                     </div>
                     <!-- /#submitdiv --><!-- END PUBLISH DIV --><!-- TODO: minor column fields -->
                     <?php
-                        if ( pods_var_raw( 'action' ) == 'edit' && !$duplicate && !in_array( 'navigate', $obj->actions_disabled ) && !in_array( 'navigate', $obj->actions_hidden ) ) {
+                        if ( 'settings' != $pod->pod_data[ 'type' ] && 'edit' == pods_v( 'action' ) && !$duplicate && !in_array( 'navigate', $obj->actions_disabled ) && !in_array( 'navigate', $obj->actions_hidden ) ) {
                             if ( !isset( $singular_label ) )
                                 $singular_label = ucwords( str_replace( '_', ' ', $pod->pod_data[ 'name' ] ) );
 

515ebf651c3942629638087dc823d43033e4a270
tysonlt <tysonlt@gmail.com>
2013-12-09 19:13:30 +1100

    Added fields for before and after content to help print lists and tables.

--- ui/front/widgets.php
+++ ui/front/widgets.php
@@ -4,6 +4,12 @@
     if ( !empty( $title ) )
         echo $before_title . $title . $after_title;
 
+    if ( !empty( $before_content ) )
+        echo $before_content;
+
     echo pods_shortcode( $args, ( isset( $content ) ? $content : null ) );
+
+    if ( !empty( $after_content ) )
+        echo $after_content;
 
     echo $after_widget;

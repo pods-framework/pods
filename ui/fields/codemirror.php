617b0dd1a72ed9e0d9cdfc95f786387cd7bc320a
Scott Kingsley Clark <lol@scottkclark.com>
2013-12-22 17:29:00 -0600

    Codemirror update from @unknownnf

--- ui/fields/codemirror.php
+++ ui/fields/codemirror.php
@@ -1,43 +1,52 @@
 <?php
-    wp_enqueue_script( 'pods-codemirror' );
-    wp_enqueue_style( 'pods-codemirror' );
-	wp_enqueue_style( 'pods-codemirror-pods');
-    wp_enqueue_script( 'pods-codemirror-loadmode' );
+	wp_enqueue_script( 'pods-codemirror' );
+	wp_enqueue_style( 'pods-codemirror' );
+	wp_enqueue_style( 'pods-codemirror-pods' );
+	wp_enqueue_script( 'pods-codemirror-loadmode' );
 
-    $type = 'textarea';
-    $attributes = array();
-    $attributes[ 'tabindex' ] = 2;
-    $attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options, 'pods-ui-field-codemirror' );
+	$type = 'textarea';
+	$attributes = array();
+	$attributes[ 'tabindex' ] = 2;
+	$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options, 'pods-ui-field-codemirror' );
 ?>
 <div class="code-toolbar"><!-- Placeholder --></div>
 <textarea<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?>><?php echo esc_textarea( $value ); ?></textarea>
 <div class="code-footer"><!-- Placeholder --></div>
 
 <script>
-    var $textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>, codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>;
+	var $textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>, codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>;
 
-    jQuery( function ( $ ) {
-        $textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?> = jQuery( 'textarea#<?php echo $attributes[ 'id' ]; ?>' );
+	jQuery ( function ( $ ) {
+		$textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?> = jQuery ( 'textarea#<?php echo $attributes[ 'id' ]; ?>' );
 
-        CodeMirror.modeURL = "<?php echo PODS_URL ?>ui/js/codemirror/mode/%N/%N.js";
-        if ( 'undefined' == typeof codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ) ?> ) {
+		CodeMirror.modeURL = "<?php echo PODS_URL ?>ui/js/codemirror/mode/%N/%N.js";
+		if ( 'undefined' == typeof codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ) ?> ) {
 
-            codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?> = CodeMirror.fromTextArea( document.getElementById( "<?php echo $attributes[ 'id' ] ?>" ), {
-                lineNumbers : true,
-                matchBrackets : true,
-                mode : "application/x-httpd-php",
-                indentUnit : 4,
-                indentWithTabs : false,
-                lineWrapping : true,
-                enterMode : "keep",
-                tabMode : "shift",
-                onBlur : function () {
-                    var value = codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>.getValue();
-                    $textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>.val( value );
-                }
-            } );
+			// Get the width of the parent table cell element
+			var width = $textarea_<?php echo pods_clean_name($attributes['name']) ?>.width ();
 
-            CodeMirror.autoLoadMode( codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>, 'php' );
-        }
-    } );
+			codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?> = CodeMirror.fromTextArea ( document.getElementById ( "<?php echo $attributes[ 'id' ] ?>" ), {
+				lineNumbers : true,
+				matchBrackets : true,
+				mode : "application/x-httpd-php",
+				indentUnit : 4,
+				indentWithTabs : false,
+				lineWrapping : true,
+				enterMode : "keep",
+				tabMode : "shift",
+				onBlur : function () {
+					var value = codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>.getValue ();
+					$textarea_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>.val ( value );
+				}
+			} );
+
+			// Hack to fix percentage problems with percentage based sizes and long code lines on Webkit based browsers, https://github.com/pods-framework/pods/issues/1887
+			// TODO: Find a way to make this solution work for window resizing.
+			if ( navigator.userAgent.match ( /Chrome/ ) ) {
+				codemirror_<?php echo pods_clean_name($attributes['name']) ?>.setSize ( width );
+			}
+
+			CodeMirror.autoLoadMode ( codemirror_<?php echo pods_clean_name( $attributes[ 'name' ] ); ?>, 'php' );
+		}
+	} );
 </script>

ae5ac2bf53e8ee2b50e4a91b48cd73f4377933e1
Scott Kingsley Clark <lol@scottkclark.com>
2014-01-06 07:23:26 -0600

    Fallback handling for old classes with a class forwarder PodsInit >> Pods_Init

--- classes/Pods/ClassLoader.php
+++ classes/Pods/ClassLoader.php
@@ -22,6 +22,8 @@ class Pods_ClassLoader {
 
 	private $_namespaceSeparator = '\\';
 
+	private $_fallback = false;
+
 	/**
 	 * Creates a new instance that loads classes of the specified path, also works for namespaces.
 	 *
@@ -61,9 +63,13 @@ class Pods_ClassLoader {
 	 *
 	 * @param string $className The name of the class to load.
 	 *
-	 * @return void
+	 * @return void|boolean|null
 	 */
-	public function loadClass( $className ) {
+	public function loadClass( $_className ) {
+
+		$className = $_className;
+
+		$foundClass = false;
 
 		if ( null === $this->_namespace || $this->_namespace . $this->_namespaceSeparator === substr( $className, 0, strlen( $this->_namespace . $this->_namespaceSeparator ) ) ) {
 			$fileName = '';
@@ -79,19 +85,110 @@ class Pods_ClassLoader {
 			// Make sure we have a file before trying to include it, otherwise it will break WordPress
 			if ( file_exists( ( $this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '' ) . $fileName ) ) {
 				require_once( ( $this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '' ) . $fileName );
+
+				$foundClass = true;
 			}
 			// Deprecated class loading
 			elseif ( null !== $this->_deprecatedIncludePath ) {
 				// Not many deprecated classes yet, only two
 				if ( file_exists( $this->_deprecatedIncludePath . DIRECTORY_SEPARATOR . $className . $this->_fileExtension ) ) {
 					require_once( $this->_includePath . DIRECTORY_SEPARATOR . $className . $this->_fileExtension );
+
+					$foundClass = true;
 				}
 				// PSR-0 version of the above
 				elseif ( file_exists( $this->_deprecatedIncludePath . DIRECTORY_SEPARATOR . $fileName ) ) {
 					require_once( $this->_includePath . DIRECTORY_SEPARATOR . $fileName );
+
+					$foundClass = true;
 				}
 			}
+
+			// Fallback handling for old class style (PodsInit >> Pods_Init)
+			if ( !$this->_fallback && null !== $this->_namespace ) {
+				$fallbackClass = trim( preg_replace( '/([A-Z])/', '_$1', $_className ), '_' );
+
+				// Load main class if it doesn't exist
+				if ( !class_exists( $fallbackClass ) ) {
+					$this->_fallback = true;
+
+					$foundFallback = $this->loadClass( $fallbackClass );
+
+					// Setup fallback class
+					if ( $foundFallback ) {
+						$this->forwardClass( $_className, $fallbackClass );
+					}
+
+					$this->_fallback = false;
+				}
+			}
+			elseif ( $this->_fallback ) {
+				return $foundClass;
+			}
 		}
+
+		return null;
+
+	}
+
+	/**
+	 * Creates a fallback class that maps to the correct class (PodsInit >> Pods_Init).
+	 *
+	 * @param string $fromClass The name of the original class to map from.
+	 * @param string $toClass The name of the class to map to.
+	 *
+	 * @return void
+	 */
+	public function forwardClass( $fromClass, $toClass ) {
+
+		eval( "
+			class {$fromClass} {
+
+				private \$__obj;
+
+				public function __construct() {
+
+					\$this->__obj = new {$toClass}( func_get_args() );
+
+				}
+
+				public function __call( \$name, \$arguments ) {
+
+					return call_user_func_array( array( \$this->__obj, \$name ), \$arguments );
+
+				}
+
+				public static function __callStatic( \$name, \$arguments ) {
+
+					return forward_static_call_array( array( '{$toClass}', \$name ), \$arguments );
+
+				}
+
+				public function __set( \$offset, \$value ) {
+
+					return \$this->__obj->{\$offset} = \$value;
+
+				}
+
+				public function __get( \$offset ) {
+
+					return \$this->__obj->{\$offset};
+
+				}
+
+				public function __isset( \$offset ) {
+
+					return isset( \$this->__obj->{\$offset} );
+
+				}
+
+				public function __unset( \$offset ) {
+
+					unset( \$this->__obj->{\$offset} );
+
+				}
+			}
+		" );
 
 	}
 }

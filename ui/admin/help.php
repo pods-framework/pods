9b65ed0d928d136d4420f798e61e1d34e2ffc920
Josh Pollock <jpollock412@gmail.com>
2014-03-14 13:43:33 -0400

    Changing content in admin->help

--- ui/admin/help.php
+++ ui/admin/help.php
@@ -3,20 +3,17 @@
     <h2><?php _e( 'Pods Help', 'pods' ); ?></h2>
     <img src="<?php echo PODS_URL; ?>ui/images/pods-logo-notext-rgb-transparent.png" class="pods-leaf-watermark-right" />
 
-    <p>To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.</p>
+    <h3>Pods Support</h3>
 
-    <p>It's open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?</p>
-
-    <hr />
-
-    <h3>Recommended Plugins</h3>
-
-    <p>These plugins work together with Pods to bring your site even more organization and functionality.</p>
-
+    <p>There are many resources available to help you learn to use Pods on our site, <a href="http://pods.io/" target="_blank">Pods.io</a>:</p>
     <ul class="ul-disc">
-        <li><a href="http://wordpress.org/extend/plugins/codepress-admin-columns/" target="_blank">CodePress Admin Columns</a> - <em>Change the columns shown on the manage tables for Post Types, Taxonomies, and others areas</em></li>
-        <li><a href="http://wordpress.org/extend/plugins/tabify-edit-screen/" target="_blank">Tabify Edit Screens</a> - <em>Separate all of those pesky meta boxes that plugins add into tabs to more easily get to</em></li>
-        <li><a href="http://wordpress.org/extend/plugins/polylang/" target="_blank">Polylang</a> - <em>Make your site multi-lingual with ease, add as many languages as you need</em></li>
+        <li>To learn more about using Pods, see the <a href="http://pods.io/docs/" target="_blank">documentation</a>, or our <a href="http://pods.io/tutorials/" target="_blank">tutorials section</a>.
+
+        <li>To get help with a specific issue, you can ask in our <a href="http://pods.io/forums/" target="_blank">support forums</a>, or in <a href="http://pods.io/forums/chat/" target="_blank">our chat</a>.
+
+        <li>To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.</li>
+
+        <li>Pods is open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?</li>
     </ul>
 
     <hr />

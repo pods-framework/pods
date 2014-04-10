6c4bac247e541465f5050a8feadbc02ad7304613
Dan Stefan Oprean <dan@designxenon.com>
2014-04-10 13:44:11 +0300

    Move scss files to their own sources folder. Rework sass task in gruntfile.js to use dynamic mapping, also test if i can push.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,19 +7,17 @@ module.exports = function(grunt) {
 			pods : {
 				options : {
 					style : 'expanded',
-					sourcemap: true
+					sourcemap : true
 				},
-				files : {
-					'ui/css/front-end-styles.css' : 'ui/css/sass/front-end-styles.scss',
-					'ui/css/meta-boxes.css' : 'ui/css/sass/meta-boxes.scss',
-					'ui/css/pods-admin.css' : 'ui/css/sass/pods-admin.scss',
-					'ui/css/pods-advanced.css' : 'ui/css/sass/pods-advanced.scss',
-					'ui/css/pods-form.css' : 'ui/css/sass/pods-form.scss',
-					'ui/css/pods-front.css' : 'ui/css/sass/pods-front.scss',
-					'ui/css/pods-manage.css' : 'ui/css/sass/pods-manage.scss',
-					'ui/css/pods-ui-list-table.css' : 'ui/css/sass/pods-ui-list-table.scss',
-					'ui/css/pods-wizard.css' : 'ui/css/sass/pods-wizard.scss'
-				}
+				files : [
+					{
+						expand : true,
+						src : ['sources/ui/css/**/*.scss'],
+						dest : 'ui/css',
+						ext: '.css',
+						flatten: true
+					}
+				]
 			}
 		},
 		uglify : {

e0d2faec51e2c3e0d81215f1a62baa4597c47991
Josh Pollock <jpollock412@gmail.com>
2014-05-22 17:31:41 -0400

    In Pods_Admin::admin_ajax() Fix $params with unslashed data

--- classes/Pods/Admin.php
+++ classes/Pods/Admin.php
@@ -1689,13 +1689,17 @@ class Pods_Admin {
 		$params = pods_unslash( (array) $_POST );
 
 		foreach ( $params as $key => $value ) {
-			if ( 'action' == $key ) {
+			if ( 'action' == $key )
 				continue;
-			}
 
-			unset( $params[$key] );
+			// Fixup $_POST data
+			$_POST[ str_replace( '_podsfix_', '', $key ) ] = $_POST[ $key ];
 
-			$params[str_replace( '_podsfix_', '', $key )] = $value;
+			// Fixup $params with unslashed data
+			$params[ str_replace( '_podsfix_', '', $key ) ] = $value;
+
+			// Unset the _podsfix_* keys
+			unset( $params[ $key ] );
 		}
 
 		$params = (object) $params;

355511195eba59c83f000c330d738e5dee277497
Markus Stefanko <markus.stefanko@gmail.com>
2014-06-13 22:20:52 +0700

    fixes issues #2212, #1721, #2167, #881

--- components/Migrate-Packages/Migrate-Packages.php
+++ components/Migrate-Packages/Migrate-Packages.php
@@ -372,6 +372,11 @@ class Pods_Migrate_Packages extends PodsComponent {
                     if ( isset( $field[ 'pod_id' ] ) )
                         unset( $pod[ 'fields' ][ $k ][ 'pod_id' ] );
 
+                    if ( isset( $existing_fields[ $field['name'] ] ) ) {
+                        if ( $existing_field = pods_api()->load_field( array( 'name' => $field['name'], 'pod' => $pod[ 'name' ] ) ) )
+                            $pod['fields'][$k]['id'] = $existing_field['id'];
+                    }
+
                     if ( isset( $field[ 'pod' ] ) )
                         unset( $pod[ 'fields' ][ $k ][ 'pod' ] );
                 }

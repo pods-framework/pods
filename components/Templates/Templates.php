dcbafd23398d6b0c9651551de13e4693326d0f4c
Scott Kingsley Clark <lol@scottkclark.com>
2014-06-19 19:11:07 -0500

    Fixes #2251

--- components/Templates/Templates.php
+++ components/Templates/Templates.php
@@ -94,9 +94,8 @@ class Pods_Templates extends PodsComponent {
 
 			add_action( 'dbx_post_advanced', array( $this, 'edit_page_form' ), 10 );
 
-			// removed the old editor meta box - replaced with the frontier base template editor
-			//add_action( 'pods_meta_groups', array( $this, 'add_meta_boxes' ) );
-			
+			add_action( 'pods_meta_groups', array( $this, 'add_meta_boxes' ) );
+
 			add_filter( 'get_post_metadata', array( $this, 'get_meta' ), 10, 4 );
 			add_filter( 'update_post_metadata', array( $this, 'save_meta' ), 10, 4 );
 
@@ -281,18 +280,10 @@ class Pods_Templates extends PodsComponent {
 			'type' => 'post_type'
 		);
 
+		var_dump( isset( PodsMeta::$post_types[ $pod[ 'name' ] ] ) );
+
 		if ( isset( PodsMeta::$post_types[ $pod[ 'name' ] ] ) )
 			return;
-
-		$fields = array(
-			array(
-				'name' => 'code',
-				'label' => __( 'Content', 'pods' ),
-				'type' => 'code'
-			)
-		);
-
-		pods_group_add( $pod, __( 'Template', 'pods' ), $fields, 'normal', 'high' );
 
 		$fields = array(
 			array(

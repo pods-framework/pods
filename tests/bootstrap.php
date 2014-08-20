91d4d20ef7ead9aed5467814c13649a4697e2348
clubduece <daryl@clubduece.com>
2014-08-20 14:07:20 +0000

    Update to new WP unit testing framework Implement environment variables for WP test framework location

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,16 @@
 <?php
 
-ini_set('display_errors','on');
+//ini_set('display_errors','on');
 error_reporting(E_ALL);
 define( 'PODS_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . '/'  );
 
-require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
+require_once getenv('WP_TESTS_DIR') . '/tests/phpunit/includes/functions.php';
 
 function _install_and_load_pods() {
 	require dirname( __FILE__ ) . '/includes/loader.php';
 }
 tests_add_filter( 'muplugins_loaded', '_install_and_load_pods' );
 
-require dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/bootstrap.php';
+require getenv('WP_TESTS_DIR') . '/tests/phpunit/includes/bootstrap.php';
 
 require dirname( __FILE__ ) . '/framework/testcase.php';

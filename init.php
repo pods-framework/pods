6ad3b782f8ecd007bf1af51437e3b8ab73d683e1
Josh Pollock <jpollock412@gmail.com>
2014-09-26 15:05:31 -0400

    fix #825

--- init.php
+++ init.php
@@ -182,11 +182,15 @@ else {
                 require_once( PODS_DIR . 'deprecated/deprecated.php' );
 			}
 
-            if ( false !== pods_compatibility_check() && !is_network_admin()) {
-                $pods_form = pods_form();
+	        if ( false !== pods_compatibility_check() ) {
+		        $pods_form = pods_form();
 
-                $pods_init = pods_init();
-            }
+		        if ( ! is_network_admin() ) {
+			        $pods_init = pods_init();
+		        }
+
+	        }
+	        
         }
     }
 }

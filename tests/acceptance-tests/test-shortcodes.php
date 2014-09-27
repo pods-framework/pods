4f45a1b1183d8333804105d80646228796d0fec2
clubduece <daryl@clubduece.com>
2014-09-27 17:14:52 +0000

    Remove unused variable assignments

--- tests/acceptance-tests/test-shortcodes.php
+++ tests/acceptance-tests/test-shortcodes.php
@@ -35,19 +35,19 @@ class Test_Shortcodes extends \Pods_Unit_Tests\Pods_UnitTestCase {
 		$items = array();
 
 		//add an item
-		$item[1] = $this->pod->add( array( 'name' => 'Felucia', 'number_of_moons' => 5 ) );
+		$this->pod->add( array( 'name' => 'Felucia', 'number_of_moons' => 5 ) );
 
 		//test shortcode
 		$this->assertEquals( '5', do_shortcode( '[pods name ="planet" where="t.number_of_moons=5"]{@number_of_moons}[/pods]' ) );
 
 		//add another item
-		$item[2] = $this->pod->add( array( 'name' => 'Geonosis', 'number_of_moons' => 7 ) );
+		$this->pod->add( array( 'name' => 'Geonosis', 'number_of_moons' => 7 ) );
 
 		//test shortcode
 		$this->assertEquals( '5', do_shortcode( '[pods name ="planet" where="t.number_of_moons=5"]{@number_of_moons}[/pods]' ) );
 
 		//add third item
-		$item[3] = $this->pod->add( array( 'name' => 'Naboo', 'number_of_moons' => 5 ) );
+		$this->pod->add( array( 'name' => 'Naboo', 'number_of_moons' => 5 ) );
 
 		//test shortcode
 		$this->assertEquals( '55', do_shortcode( '[pods name ="planet" where="t.number_of_moons=5"]{@number_of_moons}[/pods]' ) );

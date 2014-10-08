bb5dd4d15a2adfbcd46f99c7d03249d73bbe5543
clubduece <daryl@clubduece.com>
2014-10-08 13:29:16 +0000

    Additional Pods_Field_Text tests

--- tests/unit-tests/test_pods_field_text.php
+++ tests/unit-tests/test_pods_field_text.php
@@ -11,6 +11,10 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	}
 
 	public function tearDown() {
+		if ( shortcode_exists( 'fooshortcode') ) {
+			remove_shortcode( 'fooshortcode' );
+		}
+
 		unset( $this->field );
 	}
 
@@ -79,6 +83,7 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	/**
 	 * @covers  Pods_Field_Text::schema
 	 * @depends test_method_exists_schema
+	 * @uses    ::pods_v
 	 */
 	public function test_method_schema_returns_varchar_default() {
 		$this->assertEquals( 'VARCHAR(255)', $this->field->schema() );
@@ -87,19 +92,19 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	/**
 	 * @covers  Pods_Field_Text::schema
 	 * @depends test_method_exists_schema
+	 * @uses    ::pods_v
 	 */
 	public function test_method_schema_returns_mediumtext() {
-		$this->markTestIncomplete();
-		$this->assertEquals( 'MEDIUMTEXT', $this->field->schema( $options ) );
+		$this->assertEquals( 'MEDIUMTEXT', $this->field->schema( array( 'text_max_length' => 500 ) ) );
 	}
 
 	/**
 	 * @covers  Pods_Field_Text::schema
 	 * @depends test_method_exists_schema
+	 * @uses    ::pods_v
 	 */
 	public function test_method_schema_returns_longtext() {
-		$this->markTestIncomplete();
-		$this->assertEquals( 'LONGTEXT', $this->field->schema( $options ) );
+		$this->assertEquals( 'LONGTEXT', $this->field->schema( array( 'text_max_length' => 16777216 ) ) );
 	}
 
 	/**
@@ -112,6 +117,8 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	/**
 	 * @covers  Pods_Field_Text::display
 	 * @depends test_method_exists_display
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
 	 */
 	public function test_method_display_defaults() {
 		$this->assertEquals( '', $this->field->display() );
@@ -120,6 +127,8 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	/**
 	 * @covers  Pods_Field_Text::display
 	 * @depends test_method_exists_display
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
 	 */
 	public function test_method_display_value() {
 		$this->assertEquals( 'foo', $this->field->display( 'foo' ) );
@@ -128,9 +137,75 @@ class Test_Pods_Field_Text extends Pods_UnitTestCase {
 	/**
 	 * @covers  Pods_Field_Text::display
 	 * @depends test_method_exists_display
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
 	 */
 	public function test_method_display_value_allow_shortcode() {
-		$this->markTestIncomplete();
+		add_shortcode( 'fooshortcode', function() { return 'foobar'; } );
+
+		$this->assertEquals( 'foobar', $this->field->display( '[fooshortcode]foo[/fooshortcode]', 'bar', array( 'text_allow_shortcode' => 1 ) ) );
+	}
+
+	/**
+	 * @covers Pods_Field_Text::pre_save
+	 */
+	public function test_method_exists_pre_save() {
+		$this->assertTrue( method_exists( 'Pods_Field_Text', 'pre_save' ), 'Pods_Field_Text::pre_save does not exist.' );
+	}
+
+	/**
+	 * @covers  Pods_Field_Text::pre_save
+	 * @depends test_method_exists_pre_save
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
+	 * @uses    ::pods_mb_strlen
+	 * @uses    ::pods_mb_substr
+	 */
+	public function test_method_pre_save_defaults() {
+		$this->assertEquals( 'foo', $this->field->pre_save( 'foo' ) );
+	}
+
+	/**
+	 * @covers  Pods_Field_Text::pre_save
+	 * @depends test_method_exists_pre_save
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
+	 * @uses    ::pods_mb_strlen
+	 * @uses    ::pods_mb_substr
+	 */
+	public function test_method_pre_save_truncate() {
+		$this->assertEquals( 'foo', $this->field->pre_save( 'foobar', null, null, array( 'text_max_length' => 3 ) ) );
+	}
+
+	/**
+	 * @covers Pods_Field_Text::validate
+	 */
+	public function test_method_exists_validate() {
+		$this->assertTrue( method_exists( 'Pods_Field_Text', 'validate' ), 'Pods_Field_Text::validate does not exist.' );
+	}
+
+	/**
+	 * @covers  Pods_Field_Text::validate
+	 * @depends test_method_exists_pre_save
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
+	 * @uses    ::pods_mb_strlen
+	 * @uses    ::pods_mb_substr
+	 */
+	public function test_method_validate() {
+		$this->assertTrue( $this->field->validate( 'foobar' ) );
+	}
+
+	/**
+	 * @covers  Pods_Field_Text::validate
+	 * @depends test_method_exists_pre_save
+	 * @uses    Pods_Field_Text::strip_html
+	 * @uses    ::pods_v
+	 * @uses    ::pods_mb_strlen
+	 * @uses    ::pods_mb_substr
+	 */
+	public function test_method_validate_empty_value() {
+		$this->assertTrue( $this->field->validate( '' ) );
 	}
 
 	/**

55cd6bceb3e5b14c27dcebbe9ffcb10407a801b5
clubduece <daryl@clubduece.com>
2014-11-11 19:12:35 +0000

    Add factory for pod

--- tests/framework/factory.php
+++ tests/framework/factory.php
@@ -1,91 +1,67 @@
 <?php
 namespace Pods_Unit_Tests;
+	use \WP_UnitTest_Factory_For_Thing;
 
 class Pods_UnitTest_Factory extends \WP_UnitTest_Factory {
 	/**
-	 * @var Pods_UnitTest_Factory_For_Download
+	 * @var Pods_UnitTest_Factory_For_Pod
 	 */
-	public $download;
+	public $pod;
 
 	/**
-	 * @var Pods_UnitTest_Factory_For_Discount
+	 * @var Pods_UnitTest_Factory_For_Field
 	 */
-	public $discount;
-
-	/**
-	 * @var Pods_UnitTest_Factory_For_Payment
-	 */
-	public $payment;
+	public $field;
 
 	public function __construct() {
 		parent::__construct();
-
-		/*$this->download = new Pods_UnitTest_Factory_For_Download( $this );
-		$this->discount = new Pods_UnitTest_Factory_For_Discount( $this );
-		$this->payment = new Pods_UnitTest_Factory_For_Payment( $this );*/
+		
+		$this->pod = new Pods_UnitTest_Factory_For_Pod( $this );
 	}
 }
 
-class Pods_UnitTest_Factory_For_Download extends \WP_UnitTest_Factory_For_Thing {
+class Pods_UnitTest_Factory_For_Pod extends WP_UnitTest_Factory_For_Thing {
 	public function __construct( $factory = null ) {
 		parent::__construct( $factory );
 		$this->default_generation_definitions = array(
-			'post_status' => 'publish',
-			'post_title' => new \WP_UnitTest_Generator_Sequence( 'Download title %s' ),
-			'post_content' => new \WP_UnitTest_Generator_Sequence( 'Download content %s' ),
-			'post_excerpt' => new \WP_UnitTest_Generator_Sequence( 'Download excerpt %s' ),
-			'post_type' => 'download'
+			'id'         => new \WP_UnitTest_Generator_Sequence( '%s' ),
+			'name'       => new \WP_UnitTest_Generator_Sequence( 'test-pod-%s' ),
+			'label'      => new \WP_UnitTest_Generator_Sequence( 'Test Pod %s' ),
 		);
 	}
 
 	public function create_object( $args ) {
-		return wp_insert_post( $args );
+		return pods_api()->save_pod( $args );
 	}
-
+	
 	public function update_object( $post_id, $fields ) {
-		$fields['ID'] = $post_id;
-		return wp_update_post( $fields );
+		// not yet implemented
 	}
 
 	public function get_object_by_id( $post_id ) {
-		return get_post( $post_id );
+		// not yet implemented
 	}
 }
 
-class Pods_UnitTest_Factory_For_Discount extends \WP_UnitTest_Factory_For_Thing {
+class Pods_UnitTest_Factory_For_Field extends WP_UnitTest_Factory_For_Thing {
 	public function __construct( $factory = null ) {
 		parent::__construct( $factory );
+		$this->default_generation_definitions = array(
+			'id'         => new \WP_UnitTest_Generator_Sequence( '%s' ),
+			'name'       => new \WP_UnitTest_Generator_Sequence( 'test-field-%s' ),
+			'label'      => new \WP_UnitTest_Generator_Sequence( 'Test Field %s' ),
+		);
 	}
 
 	public function create_object( $args ) {
-		return wp_insert_post( $args );
+		// not yet implemented
 	}
-
+	
 	public function update_object( $post_id, $fields ) {
-		$fields['ID'] = $post_id;
-		return wp_update_post( $fields );
+		// not yet implemented
 	}
 
 	public function get_object_by_id( $post_id ) {
-		return get_post( $post_id );
-	}
-}
-
-class Pods_UnitTest_Factory_For_Payment extends \WP_UnitTest_Factory_For_Thing {
-	public function __construct( $factory = null ) {
-		parent::__construct( $factory );
-	}
-
-	public function create_object( $args ) {
-		return wp_insert_post( $args );
-	}
-
-	public function update_object( $post_id, $fields ) {
-		$fields['ID'] = $post_id;
-		return wp_update_post( $fields );
-	}
-
-	public function get_object_by_id( $post_id ) {
-		return get_post( $post_id );
+		// not yet implemented
 	}
 }

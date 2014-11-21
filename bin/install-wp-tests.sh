db1d86a7b3b69fc51921904419b5ac0da46a1f3b
Scott Kingsley Clark <lol@scottkclark.com>
2014-11-21 17:33:13 -0600

    Fix drop to no beep/silent

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -78,7 +78,7 @@ install_db() {
 	fi
 
 	# drop database
-	mysqladmin drop $DB_NAME --force --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	mysqladmin drop $DB_NAME --force --no-beep --silent --user="$DB_USER" --password="$DB_PASS"$EXTRA
 
 	# create database
 	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA

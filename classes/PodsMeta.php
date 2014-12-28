e835fb676a17bb3e54dca625387b13c9f1f2f4ba
Scott Kingsley Clark <lol@scottkclark.com>
2014-12-28 06:28:35 -0600

    Merge in ACT groups for 2.x admin form

--- classes/PodsMeta.php
+++ classes/PodsMeta.php
@@ -753,7 +753,7 @@ class PodsMeta {
      *
      * @return array
      */
-    public function groups_get ( $type, $name ) {
+    public function groups_get ( $type, $name, $default_fields = null ) {
         if ( 'post_type' == $type && 'attachment' == $name ) {
             $type = 'media';
             $name = 'media';
@@ -774,8 +774,10 @@ class PodsMeta {
             $object = self::$user;
         elseif ( 'comment' == $type )
             $object = self::$comment;
+        elseif ( 'pod' == $type )
+            $object = self::$advanced_content_types;
 
-        if ( 'pod' != $type && !empty( $object ) && is_array( $object ) && isset( $object[ $name ] ) )
+        if ( !empty( $object ) && is_array( $object ) && isset( $object[ $name ] ) )
             $fields = $object[ $name ][ 'fields' ];
         else {
             if ( empty( self::$current_pod_data ) || !is_object( self::$current_pod_data ) || self::$current_pod_data[ 'name' ] != $name )
@@ -785,6 +787,10 @@ class PodsMeta {
 
             if ( !empty( $pod ) )
                 $fields = $pod[ 'fields' ];
+        }
+
+        if ( null !== $default_fields ) {
+            $fields = $default_fields;
         }
 
         $defaults = array(
@@ -874,10 +880,11 @@ class PodsMeta {
 
             }
         }
-	if ( $field_found ) {
-		// Only add the classes to forms that actually have pods fields
-		add_action( 'post_edit_form_tag', function() { echo ' class="pods-submittable pods-form"'; } );
-	}
+
+		if ( $field_found ) {
+			// Only add the classes to forms that actually have pods fields
+			add_action( 'post_edit_form_tag', function() { echo ' class="pods-submittable pods-form"'; } );
+		}
     }
 
     /**

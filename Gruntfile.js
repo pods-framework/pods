9493f985d96639552253c7209cae04147ba2993a
Josh Pollock <jpollock412@gmail.com>
2015-01-26 00:17:38 -0500

    automate version release process for 2.x branch #2648

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,30 @@
 module.exports = function (grunt) {
 
+	//setup file list for copying/ not copying for SVN
+	files_list = [
+		'**',
+		'!node_modules/**',
+		'!release/**',
+		'!.git/**',
+		'!.sass-cache/**',
+		'!Gruntfile.js',
+		'!package.json',
+		'!.gitignore',
+		'!.gitmodules',
+		'!bin/**',
+		'!tests/**',
+		'!.gitattributes',
+		'!.travis.yml',
+		'!composer.lock',
+		'!composer.json',
+		'!CONTRIBUTING.md',
+		'!git-workflow.md',
+		'!phpunit.xml.dist'
+	];
+
 	// Project configuration.
 	grunt.initConfig({
-		pkg     : grunt.file.readJSON('package.json'),
+		pkg     : grunt.file.readJSON( 'package.json' ),
 		glotpress_download : {
 			core : {
 				options : {
@@ -12,11 +34,109 @@ module.exports = function (grunt) {
 					textdomain : 'pods'
 				}
 			}
+		},
+		clean: {
+			post_build: [
+				'build'
+			]
+		},
+		copy: {
+			svn_trunk: {
+				options : {
+					mode :true
+				},
+				src:  files_list,
+				dest: 'build/<%= pkg.name %>/trunk/'
+			},
+			svn_tag: {
+				options : {
+					mode :true
+				},
+				src:  files_list,
+				dest: 'build/<%= pkg.name %>/tags/<%= pkg.version %>/'
+			}
+		},
+		gittag: {
+			addtag: {
+				options: {
+					tag: '2.x/<%= pkg.version %>',
+					message: 'Version <%= pkg.version %>'
+				}
+			}
+		},
+		gitcommit: {
+			commit: {
+				options: {
+					message: 'Version <%= pkg.version %>',
+					noVerify: true,
+					noStatus: false,
+					allowEmpty: true
+				},
+				files: {
+					src: [ 'README.md', 'readme.txt', 'init.php', 'package.json', 'languages/**' ]
+				}
+			}
+		},
+		gitpush: {
+			push: {
+				options: {
+					tags: true,
+					remote: 'origin',
+					branch: 'master'
+				}
+			}
+		},
+		replace: {
+			version: {
+				src: [ 'README.md', 'readme.txt', 'init.php' ],
+				overwrite: true,
+				replacements: [{
+					from: "<%= pkg.last_version %>",
+					to: "<%= pkg.version %>"
+				}]
+			}
+		},
+		svn_checkout: {
+			make_local: {
+				repos: [
+					{
+						path: [ 'release' ],
+						repo: 'http://plugins.svn.wordpress.org/pods'
+					}
+				]
+			}
+		},
+		push_svn: {
+			options: {
+				remove: true
+			},
+			main: {
+				src: 'release/<%= pkg.name %>',
+				dest: 'http://plugins.svn.wordpress.org/pods',
+				tmp: 'build/make_svn'
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-glotpress');
+	//load modules
+	grunt.loadNpmTasks( 'grunt-glotpress' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-git' );
+	grunt.loadNpmTasks( 'grunt-text-replace' );
+	grunt.loadNpmTasks( 'grunt-svn-checkout' );
+	grunt.loadNpmTasks( 'grunt-push-svn' );
+	grunt.loadNpmTasks( 'grunt-remove' );
 
-	grunt.registerTask('default', ['glotpress_download']);
+	//register default task
+	grunt.registerTask( 'default', [ 'glotpress_download' ]);
+
+	//release tasks
+	grunt.registerTask( 'pre_vcs', [ 'replace:version', 'glotpress_download' ] );
+	grunt.registerTask( 'do_svn', [ 'svn_checkout', 'copy:svn_trunk', 'copy:svn_tag', 'push_svn' ] );
+	grunt.registerTask( 'do_git', [ 'gitcommit', 'gittag', 'gitpush' ] );
+
+	grunt.registerTask( 'release', [ 'pre_vcs', 'do_svn', 'do_git', 'clean:post_build' ] );
+
 
 };

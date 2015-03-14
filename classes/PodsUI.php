84cdc976092a0703d7b555e268b8da2ec7f8e8e7
Scott Kingsley Clark <lol@scottkclark.com>
2015-03-13 20:05:40 -0500

    Enforce strict DB column name usage for $_GET['orderby'] in PodsUI

--- classes/PodsUI.php
+++ classes/PodsUI.php
@@ -857,7 +857,12 @@ class PodsUI {
 
         $options->validate( 'orderby_dir', strtoupper( pods_var_raw( 'orderby_dir' . $options[ 'num' ], 'get', $this->orderby_dir, null, true ) ), 'in_array', array( 'ASC', 'DESC' ) );
 
-        $orderby = pods_var_raw( 'orderby' . $options->num, 'get', $this->orderby, null, true );
+	    $orderby = $this->orderby;
+
+	    // Enforce strict DB column name usage
+	    if ( ! empty( $_GET[ 'orderby' . $options->num ] ) ) {
+		    $orderby = pods_clean_name( $_GET[ 'orderby' . $options->num ], true, false );
+	    }
 
         if ( !empty( $orderby ) ) {
             $orderby = array(

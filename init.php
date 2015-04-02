e155b68105bc413097c853ee58ecddc14b6e9198
pglewis <pglewis13@gmail.com>
2015-04-02 05:21:31 -0400

    Set Github updater plugin to release/3.0

--- init.php
+++ init.php
@@ -9,7 +9,7 @@ Author URI: http://pods.io/about/
 Text Domain: pods
 Domain Path: /languages/
 GitHub Plugin URI: https://github.com/pods-framework/pods
-GitHub Branch: 2.x
+GitHub Branch: release/3.0
 
 Copyright 2009-2015  Pods Foundation, Inc  (email : contact@podsfoundation.org)
 

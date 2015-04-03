d5f0913f0a5ab53586dd0fe99f3209b8e1e6982f
pglewis <pglewis13@gmail.com>
2015-04-03 01:11:49 -0400

    classes/Pods/Field.php

--- classes/Pods/Field.php
+++ classes/Pods/Field.php
@@ -214,6 +214,7 @@ class Pods_Field {
 	 */
 	public function input( $name, $value = null, $options = null, $pod = null, $id = null ) {
 
+		$options         = (array) $options;
 		$form_field_type = Pods_Form::$field_type;
 
 		if ( is_array( $value ) ) {

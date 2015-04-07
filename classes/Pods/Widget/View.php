b3a2f28bd5e195c6c13c49ec6b6787d0a5922b46
pglewis <pglewis13@gmail.com>
2015-04-07 09:47:50 -0400

    #2822 merge and formatting normalization: classes/Pods/Widget/View.php

--- classes/Pods/Widget/View.php
+++ classes/Pods/Widget/View.php
@@ -3,27 +3,32 @@
 /**
  * @package Pods\Widgets
  */
-class Pods_Widget_View extends
-	WP_Widget {
+class Pods_Widget_View extends WP_Widget {
 
 	/**
 	 * Register the widget
 	 */
 	public function Pods_Widget_View() {
+
 		$this->WP_Widget( 'pods_widget_view',
 			'Pods - View',
-			array( 'classname' => 'pods_widget_view', 'description' => "Include a file from a theme, with caching options" ),
-			array( 'width' => 200 ) );
+			array( 'classname'   => 'pods_widget_view',
+			       'description' => "Include a file from a theme, with caching options"
+			),
+			array( 'width' => 200 )
+		);
+
 	}
 
 	/**
-	 * @see Pods_Widget_Field::widget()
+	 * {@inheritdoc}
 	 */
 	public function widget( $args, $instance ) {
+
 		extract( $args );
 
 		// Get widget fields
-		$title = apply_filters( 'widget_title', $instance['title'] );
+		$title = apply_filters( 'widget_title', $instance[ 'title' ] );
 
 		$args = array(
 			'view'       => trim( pods_v( 'view', $instance, '' ) ),
@@ -34,39 +39,45 @@ class Pods_Widget_View extends
 			'shortcodes' => (int) pods_v( 'shortcodes', $instance, 0 )
 		);
 
-		if ( 0 < strlen( $args['view'] ) ) {
+		if ( 0 < strlen( $args[ 'view' ] ) ) {
 			require PODS_DIR . 'ui/front/widgets.php';
 		}
+
 	}
 
 	/**
-	 * {@inheritDocs}
+	 * {@inheritdoc}
 	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance               = $old_instance;
-		$instance['title']      = pods_v( 'title', $new_instance, '' );
-		$instance['view']       = pods_v( 'view', $new_instance, '' );
-		$instance['expires']    = (int) pods_var_raw( 'expires', $new_instance, ( 60 * 5 ) );
-		$instance['cache_mode'] = pods_v( 'cache_mode', $new_instance, 'none', true );
-		$instance['before']     = pods_v( 'before', $new_instance, '' );
-		$instance['after']      = pods_v( 'after', $new_instance, '' );
-		$instance['shortcodes'] = (int) pods_v( 'shortcodes', $new_instance, 0 );
+
+		$instance = $old_instance;
+		$instance[ 'title' ] = pods_v( 'title', $new_instance, '' );
+		$instance[ 'view' ] = pods_v( 'view', $new_instance, '' );
+		$instance[ 'expires' ] = (int) pods_var_raw( 'expires', $new_instance, ( 60 * 5 ) );
+		$instance[ 'cache_mode' ] = pods_v( 'cache_mode', $new_instance, 'none', true );
+		$instance[ 'before' ] = pods_v( 'before', $new_instance, '' );
+		$instance[ 'after' ] = pods_v( 'after', $new_instance, '' );
+		$instance[ 'shortcodes' ] = (int) pods_v( 'shortcodes', $new_instance, 0 );
 
 		return $instance;
+
 	}
 
 	/**
-	 * {@inheritDocs}
+	 * {@inheritdoc}
 	 */
 	public function form( $instance ) {
-		$title      = pods_v( 'title', $instance, '' );
-		$view       = pods_v( 'view', $instance, '' );
-		$expires    = (int) pods_var_raw( 'expires', $instance, ( 60 * 5 ) );
+
+		$title = pods_v( 'title', $instance, '' );
+		$view = pods_v( 'view', $instance, '' );
+		$expires = (int) pods_var_raw( 'expires', $instance, ( 60 * 5 ) );
 		$cache_mode = pods_v( 'cache_mode', $instance, 'none', true );
-		$before     = pods_v( 'before', $instance, '' );
-		$after      = pods_v( 'after', $instance, '' );
+		$before = pods_v( 'before', $instance, '' );
+		$after = pods_v( 'after', $instance, '' );
 		$shortcodes = (int) pods_v( 'shortcodes', $instance, 0 );
 
 		require PODS_DIR . 'ui/admin/widgets/view.php';
+
 	}
+
 }

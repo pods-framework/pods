cabcb863f9b9224bcfe22b27cf33b8bb42a6523d
pglewis <pglewis13@gmail.com>
2015-04-10 01:34:26 -0400

    #2822 merge and formatting normalization: ui/fields/textarea.php

--- ui/fields/textarea.php
+++ ui/fields/textarea.php
@@ -1,19 +1,18 @@
 <?php
-$type                   = 'textarea';
-$attributes             = array();
-$attributes['tabindex'] = 2;
-$attributes             = Pods_Form::merge_attributes( $attributes, $name, $form_field_type, $options );
+$type                     = 'textarea';
+$attributes               = array();
+$attributes[ 'tabindex' ] = 2;
+$attributes               = Pods_Form::merge_attributes( $attributes, $name, $form_field_type, $options );
 
 if ( pods_v( 'readonly', $options, false ) ) {
-	$attributes['readonly'] = 'READONLY';
-
-	$attributes['class'] .= ' pods-form-ui-read-only';
+	$attributes[ 'readonly' ] = 'READONLY';
+	$attributes[ 'class' ] .= ' pods-form-ui-read-only';
 }
 
 $rows = (int) pods_v( Pods_Form::$field_type . '_rows', $options, 0 );
 
 if ( 0 < $rows ) {
-	$attributes['rows'] = $rows;
+	$attributes[ 'rows' ] = $rows;
 }
 ?>
 	<textarea<?php Pods_Form::attributes( $attributes, $name, $form_field_type, $options ); ?>><?php echo esc_textarea( $value ); ?></textarea>

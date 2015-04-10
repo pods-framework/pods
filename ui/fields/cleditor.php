aec9377d8cdeed3bd1c964ea95110702e9f987f3
pglewis <pglewis13@gmail.com>
2015-04-10 00:26:43 -0400

    #2822 merge and formatting normalization: ui/fields/cleditor.php

--- ui/fields/cleditor.php
+++ ui/fields/cleditor.php
@@ -2,17 +2,17 @@
 wp_enqueue_script( 'pods-cleditor' );
 wp_enqueue_style( 'pods-cleditor' );
 
-$type                   = 'textarea';
-$attributes             = array();
-$attributes['tabindex'] = 2;
-$attributes             = Pods_Form::merge_attributes( $attributes, $name, $form_field_type, $options, 'pods-ui-field-cleditor' );
+$type = 'textarea';
+$attributes = array();
+$attributes[ 'tabindex' ] = 2;
+$attributes = Pods_Form::merge_attributes( $attributes, $name, $form_field_type, $options, 'pods-ui-field-cleditor' );
 ?>
 <textarea<?php Pods_Form::attributes( $attributes, $name, $form_field_type, $options ); ?>><?php echo esc_textarea( $value ); ?></textarea>
 <script>
-	jQuery(function($) {
-		var $textarea = $('textarea#<?php echo $attributes[ 'id' ]; ?>');
+	jQuery( function ( $ ) {
+		var $textarea = $( 'textarea#<?php echo $attributes[ 'id' ]; ?>' );
 		var editorWidth = $textarea.outerWidth();
-		$textarea.cleditor({
+		$textarea.cleditor( {
 			width : editorWidth<?php
 				$rows = (int) pods_v( 'wysiwyg_rows', $options, 0 );
 
@@ -23,6 +23,6 @@ $attributes             = Pods_Form::merge_attributes( $attributes, $name, $form
 				}
 
 			?>
-		});
-	});
+		} );
+	} );
 </script>

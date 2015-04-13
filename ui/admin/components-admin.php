3c130e8aa8549c0a12ec6ff2012abcd1612112bf
pglewis <pglewis13@gmail.com>
2015-04-13 16:29:11 -0400

    #2822 merge and formatting normalization: ui/admin/components-admin.php

--- ui/admin/components-admin.php
+++ ui/admin/components-admin.php
@@ -10,7 +10,7 @@
 			<h2><?php _e( 'Settings', 'pods' ); ?>: <?php echo $component_label; ?></h2>
 
 			<?php
-			if ( isset( $_GET['do'] ) ) {
+			if ( isset( $_GET[ 'do' ] ) ) {
 				pods_message( __( 'Settings saved successfully.', 'pods' ) );
 			}
 			?>
@@ -19,61 +19,61 @@
 				<?php
 				$depends_on = false;
 
-				foreach ( $options as $field_name => $field_option) {
-					$field_option = Pods_Form::field_setup( $field_option, null, $field_option['type'] );
+				foreach ($options as $field_name => $field_option) {
+				$field_option = Pods_Form::field_setup( $field_option, null, $field_option[ 'type' ] );
 
-					$depends = Pods_Form::dependencies( $field_option );
+				$depends = Pods_Form::dependencies( $field_option );
 
-					if (( ! empty( $depends_on ) || ! empty( $depends ) ) && $depends_on != $depends) {
-						if ( ! empty( $depends_on ) ) {
+				if (( ! empty( $depends_on ) || ! empty( $depends ) ) && $depends_on != $depends) {
+				if ( ! empty( $depends_on ) ) {
 					?>
 					</tbody>
 				<?php
-						}
+				}
 
-						if (! empty( $depends )) {
+				if ( ! empty( $depends )) {
 				?>
-				<tbody class="pods-field-option-container <?php echo $depends; ?>">
+				<tbody class="pods-field-option-container <?php echo esc_attr( $depends ); ?>">
 					<?php
-						}
+					}
 					}
 
-					if ( ! is_array( $field_option['group'] ) ) {
-						$value = pods_v( $field_name, $settings, $field_option['default'] );
+					if ( ! is_array( $field_option[ 'group' ] ) ) {
+						$value = pods_v( $field_name, $settings, $field_option[ 'default' ] );
 						?>
-						<tr valign="top" class="pods-field-option" id="pods-setting-<?php echo $field_name; ?>">
+						<tr valign="top" class="pods-field-option" id="pods-setting-<?php echo esc_attr( $field_name ); ?>">
 							<th>
-								<?php echo Pods_Form::label( 'pods_setting_' . $field_name, $field_option['label'], $field_option['help'], $field_option ); ?>
+								<?php echo Pods_Form::label( 'pods_setting_' . $field_name, $field_option[ 'label' ], $field_option[ 'help' ], $field_option ); ?>
 							</th>
 							<td>
-								<?php echo Pods_Form::field( 'pods_setting_' . $field_name, $value, $field_option['type'], $field_option ); ?>
+								<?php echo Pods_Form::field( 'pods_setting_' . $field_name, $value, $field_option[ 'type' ], $field_option ); ?>
 							</td>
 						</tr>
 					<?php
 					} else {
 						?>
-						<tr valign="top" class="pods-field-option-group" id="pods-setting-<?php echo $field_name; ?>">
+						<tr valign="top" class="pods-field-option-group" id="pods-setting-<?php echo esc_attr( $field_name ); ?>">
 							<th class="pods-field-option-group-label">
-								<?php echo $field_option['label']; ?>
+								<?php echo $field_option[ 'label' ]; ?>
 							</th>
 							<td class="pods-pick-values pods-pick-checkbox">
 								<ul>
 									<?php
-									foreach ( $field_option['group'] as $field_group_name => $field_group_option ) {
-										$field_group_option = Pods_Form::field_setup( $field_group_option, null, $field_group_option['type'] );
+									foreach ( $field_option[ 'group' ] as $field_group_name => $field_group_option ) {
+										$field_group_option = Pods_Form::field_setup( $field_group_option, null, $field_group_option[ 'type' ] );
 
-										if ( 'boolean' != $field_group_option['type'] ) {
+										if ( 'boolean' != $field_group_option[ 'type' ] ) {
 											continue;
 										}
 
-										$field_group_option['boolean_yes_label'] = $field_group_option['label'];
+										$field_group_option[ 'boolean_yes_label' ] = $field_group_option[ 'label' ];
 
 										$depends_option = Pods_Form::dependencies( $field_group_option );
 
-										$value = pods_v( $field_group_name, $settings, $field_group_option['default'] );
+										$value = pods_v( $field_group_name, $settings, $field_group_option[ 'default' ] );
 										?>
-										<li class="<?php echo $depends_option; ?>">
-											<?php echo Pods_Form::field( 'pods_setting_' . $field_group_name, $value, $field_group_option['type'], $field_group_option ); ?>
+										<li class="<?php echo esc_attr( $depends_option ); ?>">
+											<?php echo Pods_Form::field( 'pods_setting_' . $field_group_name, $value, $field_group_option[ 'type' ], $field_group_option ); ?>
 										</li>
 									<?php
 									}
@@ -89,7 +89,7 @@
 					}
 					}
 
-					if (! empty( $depends_on )) {
+					if ( ! empty( $depends_on )) {
 					?>
 				</tbody>
 			<?php
@@ -98,22 +98,23 @@
 			</table>
 
 			<p class="submit">
-				<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php esc_attr_e( 'Save Changes', 'pods' ); ?>"> <img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" />
+				<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php esc_attr_e( 'Save Changes', 'pods' ); ?>">
+				<img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" />
 			</p>
 		</div>
 	</form>
 </div>
 
 <script type="text/javascript">
-	jQuery(function($) {
-		$(document).Pods('validate');
-		$(document).Pods('submit');
-		$(document).Pods('dependency');
-		$(document).Pods('confirm');
-		$(document).Pods('exit_confirm');
-	});
+	jQuery( function ( $ ) {
+		$( document ).Pods( 'validate' );
+		$( document ).Pods( 'submit' );
+		$( document ).Pods( 'dependency' );
+		$( document ).Pods( 'confirm' );
+		$( document ).Pods( 'exit_confirm' );
+	} );
 
-	var pods_admin_submit_callback = function(id) {
-		document.location = '<?php echo pods_slash( pods_var_update( array( 'do' => 'save' ) ) ); ?>';
+	var pods_admin_submit_callback = function ( id ) {
+		document.location = '<?php echo pods_slash( pods_query_arg( array( 'do' => 'save' ) ) ); ?>';
 	}
 </script>

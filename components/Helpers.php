cefba77bedf37ef3325b5ec47c4d0e802ea69de7
pglewis <pglewis13@gmail.com>
2015-04-14 04:23:50 -0400

    #2822 merge and formatting normalization: components/Helpers.php

--- components/Helpers.php
+++ components/Helpers.php
@@ -21,8 +21,7 @@ if ( class_exists( 'Pods_Helpers' ) ) {
 	return;
 }
 
-class Pods_Helpers extends
-	Pods_Component {
+class Pods_Helpers extends Pods_Component {
 
 	/**
 	 * Pods object
@@ -43,9 +42,10 @@ class Pods_Helpers extends
 	private $object_type = '_pods_helper';
 
 	/**
-	 * {@inheritDocs}
+	 * {@inheritdoc}
 	 */
 	public function __construct() {
+
 		$args = array(
 			'label'        => 'Pod Helpers',
 			'labels'       => array( 'singular_name' => 'Pod Helper' ),
@@ -62,7 +62,7 @@ class Pods_Helpers extends
 		);
 
 		if ( ! pods_is_admin() ) {
-			$args['capability_type'] = 'pods_helper';
+			$args[ 'capability_type' ] = 'pods_helper';
 		}
 
 		$args = Pods_Init::object_label_fix( $args, 'post_type' );
@@ -86,12 +86,15 @@ class Pods_Helpers extends
 
 			add_filter( 'builder_layout_filter_non_layout_post_types', array( $this, 'disable_builder_layout' ) );
 		}
+
 	}
 
 	public function disable_builder_layout( $post_types ) {
-		$post_types[] = $this->object_type;
+
+		$post_types[ ] = $this->object_type;
 
 		return $post_types;
+
 	}
 
 	/**
@@ -103,6 +106,7 @@ class Pods_Helpers extends
 	 * @since 2.0.2
 	 */
 	public function setup_updated_messages( $messages ) {
+
 		global $post, $post_ID;
 
 		$post_type = get_post_type_object( $this->object_type );
@@ -115,7 +119,7 @@ class Pods_Helpers extends
 			3  => __( 'Custom field deleted.', 'pods' ),
 			4  => sprintf( __( '%s updated.', 'pods' ), $labels->singular_name ),
 			/* translators: %s: date and time of the revision */
-			5  => isset( $_GET['revision'] ) ? sprintf( __( '%s restored to revision from %s', 'pods' ), $labels->singular_name, wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+			5  => isset( $_GET[ 'revision' ] ) ? sprintf( __( '%s restored to revision from %s', 'pods' ), $labels->singular_name, wp_post_revision_title( (int) $_GET[ 'revision' ], false ) ) : false,
 			6  => sprintf( __( '%s published. <a href="%s">%s</a>', 'pods' ), $labels->singular_name, esc_url( get_permalink( $post_ID ) ), $labels->view_item ),
 			7  => sprintf( __( '%s saved.', 'pods' ), $labels->singular_name ),
 			8  => sprintf( __( '%s submitted. <a target="_blank" href="%s">Preview %s</a>', 'pods' ),
@@ -132,17 +136,18 @@ class Pods_Helpers extends
 		);
 
 		if ( false === (boolean) $post_type->public ) {
-			$messages[ $post_type->name ][1]  = sprintf( __( '%s updated.', 'pods' ), $labels->singular_name );
-			$messages[ $post_type->name ][6]  = sprintf( __( '%s published.', 'pods' ), $labels->singular_name );
-			$messages[ $post_type->name ][8]  = sprintf( __( '%s submitted.', 'pods' ), $labels->singular_name );
-			$messages[ $post_type->name ][9]  = sprintf( __( '%s scheduled for: <strong>%1$s</strong>.', 'pods' ),
+			$messages[ $post_type->name ][ 1 ] = sprintf( __( '%s updated.', 'pods' ), $labels->singular_name );
+			$messages[ $post_type->name ][ 6 ] = sprintf( __( '%s published.', 'pods' ), $labels->singular_name );
+			$messages[ $post_type->name ][ 8 ] = sprintf( __( '%s submitted.', 'pods' ), $labels->singular_name );
+			$messages[ $post_type->name ][ 9 ] = sprintf( __( '%s scheduled for: <strong>%1$s</strong>.', 'pods' ),
 				$labels->singular_name,
 				// translators: Publish box date format, see http://php.net/date
 				date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ) );
-			$messages[ $post_type->name ][10] = sprintf( __( '%s draft updated.', 'pods' ), $labels->singular_name );
+			$messages[ $post_type->name ][ 10 ] = sprintf( __( '%s draft updated.', 'pods' ), $labels->singular_name );
 		}
 
 		return $messages;
+
 	}
 
 	/**
@@ -151,65 +156,96 @@ class Pods_Helpers extends
 	 * @since 2.0
 	 */
 	public function admin_assets() {
+
 		wp_enqueue_style( 'pods-admin' );
+
 	}
 
 	/**
 	 * Fix filters, specifically removing balanceTags
 	 *
 	 * @since 2.0.1
+	 *
+	 * @param $data
+	 * @param null $pod
+	 * @param null $id
+	 * @param null $groups
+	 * @param null $post
+	 *
 	 */
 	public function fix_filters( $data, $pod = null, $id = null, $groups = null, $post = null ) {
+
 		remove_filter( 'content_save_pre', 'balanceTags', 50 );
+
 	}
 
 	/**
 	 * Remove unused row actions
 	 *
 	 * @since 2.0.5
+	 *
+	 * @param $actions
+	 * @param $post
+	 *
+	 * @return
 	 */
 	function remove_row_actions( $actions, $post ) {
+
 		global $current_screen;
 
 		if ( ! is_object( $current_screen ) || $this->object_type != $current_screen->post_type ) {
 			return $actions;
 		}
 
-		if ( isset( $actions['view'] ) ) {
-			unset( $actions['view'] );
+		if ( isset( $actions[ 'view' ] ) ) {
+			unset( $actions[ 'view' ] );
 		}
 
-		if ( isset( $actions['inline hide-if-no-js'] ) ) {
-			unset( $actions['inline hide-if-no-js'] );
+		if ( isset( $actions[ 'inline hide-if-no-js' ] ) ) {
+			unset( $actions[ 'inline hide-if-no-js' ] );
 		}
 
 		// W3 Total Cache
-		if ( isset( $actions['pgcache_purge'] ) ) {
-			unset( $actions['pgcache_purge'] );
+		if ( isset( $actions[ 'pgcache_purge' ] ) ) {
+			unset( $actions[ 'pgcache_purge' ] );
 		}
 
 		return $actions;
+
 	}
 
 	/**
 	 * Remove unused bulk actions
 	 *
 	 * @since 2.0.5
+	 *
+	 * @param $actions
+	 *
+	 * @return
 	 */
 	public function remove_bulk_actions( $actions ) {
-		if ( isset( $actions['edit'] ) ) {
-			unset( $actions['edit'] );
+
+		if ( isset( $actions[ 'edit' ] ) ) {
+			unset( $actions[ 'edit' ] );
 		}
 
 		return $actions;
+
 	}
 
 	/**
 	 * Clear cache on save
 	 *
 	 * @since 2.0
+	 *
+	 * @param $data
+	 * @param null $pod
+	 * @param null $id
+	 * @param null $groups
+	 * @param null $post
 	 */
 	public function clear_cache( $data, $pod = null, $id = null, $groups = null, $post = null ) {
+
 		$old_post = $id;
 
 		if ( ! is_object( $id ) ) {
@@ -228,15 +264,23 @@ class Pods_Helpers extends
 		if ( $this->object_type == $post->object_type ) {
 			pods_transient_clear( 'pods_object_helpers' );
 		}
+
 	}
 
 	/**
 	 * Change post title placeholder text
 	 *
 	 * @since 2.0
+	 *
+	 * @param $text
+	 * @param $post
+	 *
+	 * @return string|void
 	 */
 	public function set_title_text( $text, $post ) {
+
 		return __( 'Enter helper name here', 'pods' );
+
 	}
 
 	/**
@@ -245,6 +289,7 @@ class Pods_Helpers extends
 	 * @since 2.0
 	 */
 	public function edit_page_form() {
+
 		global $post_type;
 
 		if ( $this->object_type != $post_type ) {
@@ -252,6 +297,7 @@ class Pods_Helpers extends
 		}
 
 		add_filter( 'enter_title_here', array( $this, 'set_title_text' ), 10, 2 );
+
 	}
 
 	/**
@@ -260,12 +306,13 @@ class Pods_Helpers extends
 	 * @since 2.0
 	 */
 	public function add_meta_boxes() {
+
 		$pod = array(
 			'name' => $this->object_type,
 			'type' => 'post_type'
 		);
 
-		if ( isset( Pods_Meta::$post_types[ $pod['name'] ] ) ) {
+		if ( isset( Pods_Meta::$post_types[ $pod[ 'name' ] ] ) ) {
 			return;
 		}
 
@@ -292,6 +339,7 @@ class Pods_Helpers extends
 		);
 
 		pods_group_add( $pod, __( 'Helper', 'pods' ), $fields, 'normal', 'high' );
+
 	}
 
 	/**
@@ -305,6 +353,7 @@ class Pods_Helpers extends
 	 * @return array|bool|int|mixed|null|string|void
 	 */
 	public function get_meta( $_null, $post_ID = null, $meta_key = null, $single = false ) {
+
 		if ( 'code' == $meta_key ) {
 			$post = get_post( $post_ID );
 
@@ -314,19 +363,21 @@ class Pods_Helpers extends
 		}
 
 		return $_null;
+
 	}
 
 	/**
 	 * Save the fields
 	 *
 	 * @param        $_null
-	 * @param int    $post_ID
+	 * @param int $post_ID
 	 * @param string $meta_key
 	 * @param string $meta_value
 	 *
 	 * @return bool|int|null
 	 */
 	public function save_meta( $_null, $post_ID = null, $meta_key = null, $meta_value = null ) {
+
 		if ( 'code' == $meta_key ) {
 			$post = get_post( $post_ID );
 
@@ -357,6 +408,7 @@ class Pods_Helpers extends
 		}
 
 		return $_null;
+
 	}
 
 	/**
@@ -369,12 +421,13 @@ class Pods_Helpers extends
 	 * $params['name'] string Field name
 	 *
 	 * @param array $params An associative array of parameters
-	 * @param null  $obj
+	 * @param null $obj
 	 *
 	 * @return mixed Anything returned by the helper
 	 * @since 2.0
 	 */
 	public static function helper( $params, $obj = null ) {
+
 		/**
 		 * @var $obj Pods
 		 */
@@ -413,7 +466,7 @@ class Pods_Helpers extends
 			$params->value = null;
 		}
 
-		if ( true === $params->deprecated && is_array( $params->value ) && ! empty( $params->value ) && ! isset( $params->value[0] ) ) {
+		if ( true === $params->deprecated && is_array( $params->value ) && ! empty( $params->value ) && ! isset( $params->value[ 0 ] ) ) {
 			$params->value = array( $params->value );
 		}
 
@@ -425,13 +478,13 @@ class Pods_Helpers extends
 
 		ob_start();
 
-		if ( ! empty( $helper ) && ! empty( $helper['code'] ) ) {
-			$code = $helper['code'];
+		if ( ! empty( $helper ) && ! empty( $helper[ 'code' ] ) ) {
+			$code = $helper[ 'code' ];
 
 			$code = str_replace( '$this->', '$obj->', $code );
 
 			$value =& $params->value;
-			$name  =& $params->name;
+			$name =& $params->name;
 
 			$_safe_params = $params;
 
@@ -449,8 +502,10 @@ class Pods_Helpers extends
 		$out = ob_get_clean();
 
 		$out = apply_filters( 'pods_helpers_post_helper', $out, $params, $helper );
-		$out = apply_filters( 'pods_helpers_post_helper_' . $helper['slug'], $out, $params, $helper );
+		$out = apply_filters( 'pods_helpers_post_helper_' . $helper[ 'slug' ], $out, $params, $helper );
 
 		return $out;
+
 	}
+
 }

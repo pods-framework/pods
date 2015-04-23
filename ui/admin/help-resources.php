bd36b52660547e5c15d145df5dd320133e82b208
Nikhil Vimal <techvoltz@gmail.com>
2015-04-23 06:15:19 -0500

    Update help-resources.php

--- ui/admin/help-resources.php
+++ ui/admin/help-resources.php
@@ -2,13 +2,13 @@
 
 <p><?php _e('There are many resources available to help you learn to use Pods on our site,', 'pods'); ?><a href="http://pods.io/" target="_blank">Pods.io</a>:</p>
 <ul class="ul-disc">
-	<li><?php _e('To learn more about using Pods, see the', 'pods'); ?><a href="http://pods.io/docs/" target="_blank">documentation</a>, or our <a href="http://pods.io/tutorials/" target="_blank">tutorials section</a>.
+	<li><?php _e('To learn more about using Pods, see the', 'pods'); ?><a href="http://pods.io/docs/" target="_blank">documentation</a>, or our <a href="http://pods.io/tutorials/" target="_blank">tutorials section</a>.</li>
 
-	<li>To get help with a specific issue, you can ask in our <a href="http://pods.io/forums/" target="_blank">support forums</a>, or in <a href="http://pods.io/forums/chat/" target="_blank">our chat</a>.
+	<li><?php _e('To get help with a specific issue, you can ask in our <a href="http://pods.io/forums/" target="_blank">support forums</a>, or in <a href="http://pods.io/forums/chat/" target="_blank">our chat</a>.', 'pods'); ?></li>
 
-	<li>To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.</li>
+	<li><?php _e('To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank">GitHub</a>.', 'pods');  ?></li>
 
-	<li><?php _e("Pods is open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?"); ?></li>
+	<li><?php _e("Pods is open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?", 'pods'); ?></li>
 </ul>
 
 <hr />

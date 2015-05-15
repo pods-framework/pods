8e70d8261236c2c408f765078d24a4c7eb8a36a1
Andy Fragen <andy@thefragens.com>
2015-05-14 23:32:42 -0700

    remove vestiges of old updater

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 Plugin Name: Pods - Custom Content Types and Fields
 Plugin URI: http://pods.io/
 Description: Pods is a framework for creating, managing, and deploying customized content types and fields
-Version: 2.5.2 Beta 1
+Version: 2.5.2.1
 Author: Pods Framework Team
 Author URI: http://pods.io/about/
 Text Domain: pods
@@ -39,18 +39,10 @@ if ( defined( 'PODS_VERSION' ) || defined( 'PODS_DIR' ) ) {
 }
 else {
     // Current version
-    define( 'PODS_VERSION', '2.5.2-b-1' );
+    define( 'PODS_VERSION', '2.5.2.1' );
 
     // Version tracking between DB updates themselves
     define( 'PODS_DB_VERSION', '2.3.5' );
-
-    if ( !defined( 'PODS_GITHUB_UPDATE' ) ) {
-        define( 'PODS_GITHUB_UPDATE', false );
-	}
-
-    if ( !defined( 'PODS_GITHUB_BRANCH' ) ) {
-        define( 'PODS_GITHUB_BRANCH', '2.x' );
-	}
 
     if ( !defined( 'PODS_WP_VERSION_MINIMUM' ) ) {
         define( 'PODS_WP_VERSION_MINIMUM', '3.8' );
@@ -80,54 +72,6 @@ else {
 
         if ( !defined( 'PODS_MEDIA' ) || PODS_MEDIA )
             require_once( PODS_DIR . 'includes/media.php' );
-
-        // @todo Allow user to opt-in to future betas easily
-        if ( PODS_GITHUB_UPDATE ) {
-            $update = admin_url( 'update.php' );
-            $update = str_replace( get_site_url(), '', $update );
-
-            $update_network = network_admin_url( 'update.php' );
-            $update_network = str_replace( get_site_url(), '', $update_network );
-
-            if ( is_admin() &&
-                 ( isset( $_GET[ 'pods_force_refresh' ] ) ||
-                   ( 'update-selected' == pods_var( 'action' ) &&
-                     ( false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update ) ||
-                       false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update_network ) ) ) ) ) {
-
-                // Configuration
-                $user = 'pods-framework';
-                $repo = 'pods';
-                $branch = PODS_GITHUB_BRANCH;
-
-                // GitHub Plugin Updater
-                // https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
-                require_once( PODS_DIR . 'includes/updater.php' );
-
-                $version = PODS_VERSION;
-
-                if ( isset( $_GET[ 'pods_force_refresh' ] ) )
-                    $version = '0.1';
-
-                if ( 'update-selected' == pods_var( 'action' ) && ( false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update ) || false !== strpos( $_SERVER[ 'REQUEST_URI' ], $update_network ) ) )
-                    $version = '0.1';
-
-                $config = array(
-                    'slug' => PODS_SLUG, // this is the slug of your plugin
-                    'proper_folder_name' => dirname( PODS_SLUG ), // this is the name of the folder your plugin lives in
-                    'api_url' => 'https://api.github.com/repos/' . $user . '/' . $repo, // the github API url of your github repo
-                    'raw_url' => 'https://raw.github.com/' . $user . '/' . $repo . '/' . $branch, // the github raw url of your github repo
-                    'github_url' => 'https://github.com/' . $user . '/' . $repo, // the github url of your github repo
-                    'zip_url' => 'https://github.com/' . $user . '/' . $repo . '/zipball/' . $branch, // the zip url of the github repo
-                    'sslverify' => false, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-                    'requires' => '3.8', // which version of WordPress does your plugin require?
-                    'tested' => '4.1', // which version of WordPress is your plugin tested up to?
-                    'version' => $version
-                );
-
-                new WPGitHubUpdater( $config );
-            }
-        }
 
         if ( !defined( 'SHORTINIT' ) || !SHORTINIT ) {
 	        if ( pods_allow_deprecated() ) {

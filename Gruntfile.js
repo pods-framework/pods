a4bab5e81945d5c840535e58ad5eca2523692fa5
Scott Kingsley Clark <lol@scottkclark.com>
2015-05-28 17:39:53 -0500

    More Gruntfile.js format cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,188 +1,213 @@
 module.exports = function ( grunt ) {
 
-    // load all grunt tasks in package.json matching the `grunt-*` pattern
-    require( 'load-grunt-tasks' )( grunt );
+	// load all grunt tasks in package.json matching the `grunt-*` pattern
+	require( 'load-grunt-tasks' )( grunt );
 
-    grunt.initConfig( {
+	grunt.initConfig( {
 		pkg : grunt.file.readJSON( 'package.json' ),
 
 		githooks : {
 			all : {
-			  'pre-commit' : 'default'
+				'pre-commit' : 'default'
 			}
 		},
 
 		sprite : {
 			all : {
-			  'src'       : 'sources/images/sprites/*.png',
-			  'destImg'   : 'ui/images/sprites.png',
-			  'destCSS'   : 'sources/sass/partials/_sprites.scss',
-			  'imgPath'   : 'ui/images/sprites.png',
-			  'algorithm' : 'binary-tree'
+				'src'       : 'sources/images/sprites/*.png',
+				'destImg'   : 'ui/images/sprites.png',
+				'destCSS'   : 'sources/sass/partials/_sprites.scss',
+				'imgPath'   : 'ui/images/sprites.png',
+				'algorithm' : 'binary-tree'
 			}
 		},
 
 		csscomb : {
 			dist : {
-			  files : [
-				  {
-					  expand : true, cwd : 'sources/css/', src : ['*.css'], dest : 'sources/css/'
-				  }
-			  ]
+				files : [
+					{
+						expand : true,
+						cwd : 'sources/css/',
+						src : ['*.css'],
+						dest : 'sources/css/'
+					}
+				]
 			}
 		},
 
 		sass : {
 			dist : {
-			  options  : {
-				  style : 'expanded', lineNumbers : true
-			  }, files : [
-				  {
-					  expand : true,
-					  cwd    : 'sources/sass/',
-					  src    : ['*.scss'],
-					  dest   : 'sources/css/',
-					  ext    : '.css'
-				  }
-			  ]
+				options  : {
+					style : 'expanded',
+					lineNumbers : true
+				},
+				files : [
+					{
+						expand : true,
+						cwd    : 'sources/sass/',
+						src    : ['*.scss'],
+						dest   : 'sources/css/',
+						ext    : '.css'
+					}
+				]
 			}
 		},
 
 		cssmin : {
 			minify : {
-			  expand : true, cwd : 'sources/css/', src : '*.css', dest : 'ui/css/', ext : '.css'
+				expand : true,
+				cwd : 'sources/css/',
+				src : '*.css',
+				dest : 'ui/css/',
+				ext : '.css'
 			}
 		},
 
 		uglify : {
 			build : {
-			  options  : {
-				  mangle : false
-			  }, files : [
-				  {
-					  expand : true, cwd : 'sources/js/', src : '*.js', dest : 'ui/js/', ext : '.js'
-				  }
-			  ]
+				options  : {
+					mangle : false
+				},
+				files : [
+					{
+						expand : true,
+						cwd : 'sources/js/',
+						src : '*.js',
+						dest : 'ui/js/',
+						ext : '.js'
+					}
+				]
 			}
 		},
 
 		imagemin : {
 			dynamic : {
-			  files : [
-				  {
-					  expand : true,
-					  cwd    : 'sources/images/',
-					  src    : ['**/*.{png,jpg,gif}'],
-					  dest   : 'ui/images/'
-				  }
-			  ]
+				files : [
+					{
+						expand : true,
+						cwd    : 'sources/images/',
+						src    : ['**/*.{png,jpg,gif}'],
+						dest   : 'ui/images/'
+					}
+				]
 			}
 		},
 
 		watch : {
 			scripts : {
-			  files : ['sources/js/*.js'], tasks : ['javascript'], options : {
-				  spawn : false
-			  }
+				files : ['sources/js/*.js'],
+				tasks : ['javascript'],
+				options : {
+					spawn : false
+				}
 			},
 
 			css : {
-			  files : ['sources/sass/*.scss'], tasks : ['styles'], options : {
-				  spawn : false, livereload : true
-			  }
+				files : ['sources/sass/*.scss'],
+				tasks : ['styles'],
+				options : {
+					spawn : false,
+					livereload : true
+				}
 			},
 
 			sprite : {
-			  files   : ['ui/images/sprites/*.png'],
-			  tasks   : [
-				  'sprite',
-				  'styles'
-			  ],
-			  options : {
-				  spawn : false, livereload : true
-			  }
+				files   : ['ui/images/sprites/*.png'],
+				tasks   : [
+					'sprite',
+					'styles'
+				],
+				options : {
+					spawn : false,
+					livereload : true
+				}
 			}
 		},
 
 		shell : {
 			grunt : {
-			  command : ''
+				command : ''
 			}
 		},
 
 		clean : {
-			js : ['ui/js/*.js'], css : ['ui/css/*.css']
+			js : ['ui/js/*.js'],
+			css : ['ui/css/*.css']
 		},
 
 		makepot : {
 			pods : {
-			  options : {
-				  cwd         : '/',
-				  domainPath  : '/languages/',
-				  potFilename : 'pods.pot',
-				  type        : 'wp-plugin'
-			  }
+				options : {
+					cwd         : '/',
+					domainPath  : '/languages/',
+					potFilename : 'pods.pot',
+					type        : 'wp-plugin'
+				}
 			}
 		},
 
 		addtextdomain : {
 			pods : {
-			  options   : {
-				  textdomain : 'pods'
-			  }, target : {
-				  files : {
-					  src : ['*.php']
-				  }
-			  }
+				options   : {
+					textdomain : 'pods'
+				},
+				target : {
+					files : {
+						src : ['*.php']
+					}
+				}
 			}
 		},
 
 		update_submodules : {
-			default                 : {
-			  options : {
-				  // default command line parameters will be used: --init --recursive
-			  }
-			}, withCustomParameters : {
-			  options : {
-				  params : '--force' // specifies your own command-line parameters
-			  }
+			default              : {
+				options : {
+					// default command line parameters will be used: --init --recursive
+				}
+			},
+			withCustomParameters : {
+				options : {
+					params : '--force' // specifies your own command-line parameters
+				}
 			}
 		},
 
 		phpcs : {
 			application : {
-			  dir : [
-				  '**/*.php',
-				  '!**/node_modules/**'
-			  ]
-			}, options  : {
-			  bin : '~/phpcs/scripts/phpcs', standard : 'WordPress'
+				dir : [
+					'**/*.php',
+					'!**/node_modules/**'
+				]
+			},
+			options     : {
+				bin : '~/phpcs/scripts/phpcs',
+				standard : 'WordPress'
 			}
 		}
 
 	} );
 
-    grunt.registerTask( 'styles', [
-        'sass',
-        'csscomb',
-        'cssmin'
-    ] );
+	grunt.registerTask( 'styles', [
+		'sass',
+		'csscomb',
+		'cssmin'
+	] );
 
-    grunt.registerTask( 'javascript', ['uglify'] );
+	grunt.registerTask( 'javascript', ['uglify'] );
 
-    grunt.registerTask( 'imageminnewer', ['newer:imagemin'] );
+	grunt.registerTask( 'imageminnewer', ['newer:imagemin'] );
 
-    grunt.registerTask( 'i18n', [
-        'makepot',
-        'glotpress_download'
-    ] );
+	grunt.registerTask( 'i18n', [
+		'makepot',
+		'glotpress_download'
+	] );
 
-    grunt.registerTask( 'default', [
-        'sprite',
-        'styles',
-        'javascript',
-        'imageminnewer',
-        'i18n'
-    ] );
+	grunt.registerTask( 'default', [
+		'sprite',
+		'styles',
+		'javascript',
+		'imageminnewer',
+		'i18n'
+	] );
 
 };

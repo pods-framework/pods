c6ee8458e9407e1e760a8f6de6f7ed3cd8b69394
pglewis <pglewis13@gmail.com>
2015-12-20 02:00:41 -0500

    Flexible relationships: mop up on the modal Javascript code

--- ui/js/jquery.pods.modal.js
+++ ui/js/jquery.pods.modal.js
@@ -1,55 +1,53 @@
 ( function ( $ ) {
 
-	/**
-	 * Close the modal on form submit, if one is open
-	 */
-	var pods_modal_submit = function( e ) {
+	var methods = {
 
-		// @todo: good chance we're in a race condition in some cases
-		// This will trigger the unload event for the modal, which in turn
-		// will make an ajax request for updated markup.  We have no
-		// guarantee that what we're saving from the modal will complete
-		// before our ajax update in all cases.  What we need are events on
-		// completion we could attach to.
+		/**
+		 * Close the modal on form submits, if one is open
+		 */
+		update_submitted: function ( e ) {
 
-		var $modal_target = parent.window.jQuery( '.pods-modal.showing-modal' );
-		if ( $modal_target.length > 0 ) {
-			$modal_target.modal( 'hide' );
-		}
-	};
+			// @todo: good chance we're in a race condition in some cases
+			// This will trigger the unload event for the modal, which in turn
+			// will make an ajax request for updated markup.  We have no
+			// guarantee that what we're saving from the modal will complete
+			// before our ajax update in all cases.  What we need are events on
+			// completion we could attach to.
 
-	$( '#addtag #submit' ).on( 'click', pods_modal_submit );
-	$( 'form#post, form#createuser' ).on( 'submit', pods_modal_submit );
-	$( document ).on( 'pods_submit_success', pods_modal_submit );
+			var $modal_target = parent.window.jQuery( '.pods-modal.showing-modal' );
+			if ( $modal_target.length > 0 ) {
+				$modal_target.modal( 'hide' );
+			}
+		},
 
-	/**
-	 * Modal display and ajax updates
-	 */
-		// @todo: hard-coded class selector
-	$( document ).on( 'click', '.pods-related-edit', function( e ) {
+		/**
+		 *
+		 */
+		related_edit_click: function ( e ) {
 
-		var $add_new_button = $( this );
+			var $add_new_button = $( this );
 
-		var pod_id = $add_new_button.data( 'pod-id' );
-		var field_id = $add_new_button.data( 'field-id' );
-		var item_id = $add_new_button.data( 'item-id' );
+			var pod_id = $add_new_button.data( 'pod-id' );
+			var field_id = $add_new_button.data( 'field-id' );
+			var item_id = $add_new_button.data( 'item-id' );
 
-		e.preventDefault();
+			e.preventDefault();
 
-		// Add a class and setup a handler function for when the popup unloads
-		$add_new_button.addClass( 'showing-modal' ).on( 'hidden.r.modal', function () {
-			$( this ).removeClass( 'showing-modal' );
+			// Add a class and setup a handler function for when the popup unloads
+			$add_new_button.addClass( 'showing-modal' ).on( 'hidden.r.modal', function () {
+				$( this ).removeClass( 'showing-modal' );
 
-			var data = {
-				'action'  : 'pods_relationship_popup', // @todo: hardcoded constant
-				'pod_id'  : pod_id,
-				'field_id': field_id,
-				'item_id' : item_id
-			};
+				var data = {
+					'action'  : 'pods_relationship_popup', // @todo: hardcoded constant
+					'pod_id'  : pod_id,
+					'field_id': field_id,
+					'item_id' : item_id
+				};
 
-			// @todo: check failure as well?
-			$.post( ajaxurl, data, response_success( $add_new_button ) );
-		} );
+				// @todo: check failure as well?
+				$.post( ajaxurl, data, methods.response_success( $add_new_button ) );
+			} );
+		},
 
 		/**
 		 * Return the callback function that handles a successful response from the ajax post call
@@ -57,7 +55,7 @@
 		 * @returns {Function}
 		 * @param $add_new_button
 		 */
-		var response_success = function( $add_new_button ) {
+		response_success: function( $add_new_button ) {
 
 			// We return a function to be used as the callback, this allows us to expose the target element as a passed param
 			return function( response ) {
@@ -73,8 +71,16 @@
 					$modal_target.modal( $.getDataOptions( $modal_target, 'modal' ) );
 				}
 			};
-		};
+		}
 
-	} );
+	};
+
+	// Listeners for modal form submits
+	$( '#addtag #submit' ).on( 'click', methods.update_submitted );
+	$( 'form#post, form#createuser' ).on( 'submit', methods.update_submitted );
+	$( document ).on( 'pods_submit_success', methods.update_submitted );
+
+	// Add new button click events
+	$( document ).on( 'click', '.pods-related-edit', methods.related_edit_click );
 
 } )( jQuery );

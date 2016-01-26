4d3a9b02cbcc0826111928a458f55b5eca81b591
pglewis <pglewis13@gmail.com>
2016-01-25 19:27:30 -0500

    Enqueue missing `pods-attach` styles (new fields were only working if something else had enqueued the styles already)

--- ui/fields/file-upload/file-upload.php
+++ ui/fields/file-upload/file-upload.php
@@ -4,6 +4,8 @@
  * @var $options         array
  * @var $field_type      string
  */
+wp_enqueue_style( 'pods-attach' );
+
 wp_enqueue_script( 'jquery-ui-core' );
 wp_enqueue_script( 'jquery-ui-sortable' );
 

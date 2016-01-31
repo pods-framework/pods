ec586c8f5911265af625c24acfa7464587f6d982
pglewis <pglewis13@gmail.com>
2016-01-30 19:27:41 -0500

    Application object isn't doing anything yet, just strip it down

--- ui/js/pods-ui.js
+++ ui/js/pods-ui.js
@@ -2,18 +2,6 @@
 var pods_ui = (function () {
 	'use strict';
 
-	var PodsUIAPP = Mn.Application.extend( {
-
-		models: {},
-
-		views: {},
-
-		collections: {},
-
-		behaviors: {}
-
-	} );
-
-	return new PodsUIAPP();
+	return new Mn.Application.extend( {} );
 
 }());

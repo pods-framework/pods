f3823ad0ad64047d0fbfbc7893d09015f9a3605b
pglewis <pglewis13@gmail.com>
2016-02-15 01:12:05 -0500

    `pods-ui-ready` clean up

--- ui/js/pods-ui-ready.js
+++ ui/js/pods-ui-ready.js
@@ -1,71 +1,78 @@
 /*global jQuery, _, Backbone, Mn */
+const $ = jQuery;
+import { FileUpload } from '../fields/file-upload/src/file-upload';
+
 const app = {
 	fields: {}
 };
 export default app;
 
-import { FileUpload } from '../fields/file-upload/src/file-upload';
-
 // @todo: just here for testing the file upload queue, long term solution needed to expose things
 import * as Queue from '../fields/file-upload/src/views/file-upload-queue';
 app.Queue = Queue;
 
+/**
+ * This is the workhorse that currently kicks everything off
+ */
 jQuery( function ( $ ) {
-
-	const PODS_UI_FIELDS = '.pods-ui-field';
-	const SCRIPT_TARGETS = 'script.data';
-
-	/**
-	 * @param container
-	 * @param data
-	 */
-	const field_factory = function ( container, data ) {
-		var field_control;
-
-		switch ( data.field_type ) {
-
-			case 'file-upload':
-				field_control = new FileUpload( {
-					el        : container,
-					field_meta: data[ 'field_meta' ],
-					model_data: data[ 'model_data' ]
-				} );
-				break;
-		}
-
-		return field_control;
-	};
-
-	/**
-	 *
-	 */
-	$.fn.pods_ui_field_init = function () {
-
-		return this.each( function () {
-			var data = {};
-			var field_id;
-			var defaults = {
-				field_type: 'hidden'
-			};
-
-			// Combine data from all in-line data scripts in the container
-			$( this ).find( SCRIPT_TARGETS ).each( function () {
-					var this_data = $.parseJSON( $( this ).html() );
-					$.extend( data, this_data );
-					$( this ).remove();
-				}
-			);
-
-			// Merge inline data with the defaults and startup the new control
-			data = $.extend( defaults, data );
-			field_id = data[ 'field_meta' ][ 'field_attributes' ][ 'id' ];
-			app.fields[ field_id ] = field_factory( this, data );
-			app.fields[ field_id ].render();
-		} );
-
-	};
-
-	// Go
-	$( PODS_UI_FIELDS ).pods_ui_field_init();
-
+	$( '.pods-ui-field' ).pods_ui_field_init();
 } );
+
+/**
+ * Custom jQuery plugin to handle Pods Fields
+ */
+jQuery.fn.pods_ui_field_init = function () {
+
+	return this.each( function () {
+		let data = {}, field_id, field;
+		let defaults = {
+			field_type: 'hidden'
+		};
+
+		// Combine data from all in-line data scripts in the container
+		$( this ).find( 'script.data' ).each( function () {
+				var this_data = $.parseJSON( $( this ).html() );
+				$.extend( data, this_data );
+				$( this ).remove();
+			}
+		);
+
+		// Merge inline data with the defaults and startup the new control
+		data = $.extend( defaults, data );
+		field = field_factory( data.field_type );
+
+		if ( field !== undefined ) {
+			field_id = data.field_meta[ 'field_attributes' ].id;
+
+			app.fields[ field_id ] = new field( {
+				el        : this,
+				field_meta: data[ 'field_meta' ],
+				model_data: data[ 'model_data' ]
+			} );
+			app.fields[ field_id ].render();
+		}
+	} );
+};
+
+/**
+ * @param {string} field_type
+ */
+const field_factory = function ( field_type ) {
+	let field_control;
+
+	switch ( field_type ) {
+		case 'file-upload':
+			field_control = FileUpload;
+			break;
+
+		case 'pick':
+			// field_control = Pick
+			break;
+
+		default:
+			//field_control = Hidden;
+			break;
+	}
+
+	return field_control;
+};

f93c526fd3cbb7171905e8fa820787d7bf345aea
pglewis <pglewis13@gmail.com>
2016-02-17 02:42:06 -0500

    Initial pick field data skeleton

---
+++ ui/fields/pick/pick.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @var $form_field_type string
+ * @var $options         array
+ * @var $field_type      string
+ */
+wp_enqueue_script( 'jquery-ui-core' );
+wp_enqueue_script( 'jquery-ui-sortable' );
+
+wp_enqueue_script( 'backbone' );
+wp_enqueue_script( 'marionette', PODS_URL . 'ui/js/marionette/backbone.marionette.js', array( 'backbone' ), '2.4.4', true );
+
+wp_enqueue_script( 'backbone.babysitter', PODS_URL . 'ui/js/marionette/backbone.babysitter.min.js', array( 'backbone' ), '0.1.10', true );
+wp_enqueue_script( 'backbone.radio', PODS_URL . 'ui/js/marionette/backbone.radio.min.js', array( 'backbone' ), '1.0.2', true );
+wp_enqueue_script( 'marionette.radio.shim', PODS_URL . 'ui/js/marionette/marionette.radio.shim.js', array(
+	'marionette',
+	'backbone.radio'
+), '1.0.2', true );
+
+wp_enqueue_script( 'pods-ui-ready', PODS_URL . 'ui/js/pods-ui-ready.min.js', array(), PODS_VERSION, true );
+
+$data = (array) pods_v( 'data', $options, array(), null, true );
+unset ( $options[ 'data' ] );
+$model_data = array();
+foreach ( $data as $id => $name ) {
+	$model_data[] = array(
+		'id'   => $id,
+		'name' => $name
+	);
+}
+
+$attributes = PodsForm::merge_attributes( array(), $name, $form_field_type, $options );
+$attributes = array_map( 'esc_attr', $attributes );
+$field_meta = array(
+	'field_attributes' => array(
+		'id'         => $attributes[ 'id' ],
+		'class'      => $attributes[ 'class' ],
+		'name'       => $attributes[ 'name' ],
+		'name_clean' => $attributes[ 'data-name-clean' ]
+	),
+	'field_options'    => $options
+);
+
+include_once PODS_DIR . 'ui/fields/pick/src/templates/relationship-tpl.php';
+include_once PODS_DIR . 'classes/PodsFieldData.php';
+$field_data = new PodsUIFieldData( $field_type, array( 'model_data' => $model_data, 'field_meta' => $field_meta ) );
+?>
+<div<?php PodsForm::attributes( array('class' => $attributes[ 'class' ], 'id'    => $attributes[ 'id' ] ), $name, $form_field_type, $options ); ?>>
+	<?php $field_data->emit_script(); ?>
+</div>

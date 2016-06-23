0d254e8b430cb22f6ca0c944299397b9d197890a
Scott Kingsley Clark <lol@scottkclark.com>
2016-06-22 20:55:50 -0500

    Add 2.7 compatible changes for file uploader tile view

--- ui/fields-mv/_src/file-upload/views/file-upload-list.js
+++ ui/fields-mv/_src/file-upload/views/file-upload-list.js
@@ -40,10 +40,16 @@ export const FileUploadList = PodsFieldListView.extend( {
 		// http://stackoverflow.com/questions/1735372/jquery-sortable-list-scroll-bar-jumps-up-when-sorting/4187833#4187833
 
 		if ( 1 != fieldOptions[ 'file_limit' ] ) {
+			var sort_axis = 'y';
+
+			if ( 'tiles' == fieldOptions[ 'file_field_template' ] ) {
+				sort_axis = '';
+			}
+
 			// init sortable
 			this.$el.sortable( {
 				containment      : 'parent',
-				axis             : 'y',
+				axis             : sort_axis,
 				scrollSensitivity: 40,
 				tolerance        : 'pointer',
 				opacity          : 0.6

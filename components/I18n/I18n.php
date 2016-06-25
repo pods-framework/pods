a43fae6660748e20ea42b200203d8b345df475de
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-06-25 22:38:08 +0200

    Page to enable/disable languages + toggle visibility of i18n inputs

--- components/I18n/I18n.php
+++ components/I18n/I18n.php
@@ -2,7 +2,7 @@
 /**
  * Name: Multilingual Pods labels
  *
- * Menu Name: Multilingual Pods labels
+ * Menu Name: Translate Pods
  *
  * Description: Allow Pod name, labels and description to be translated
  *
@@ -21,10 +21,13 @@ if ( class_exists( 'Pods_I18n' ) )
 
 class Pods_I18n extends PodsComponent {
 
+	public $settings = array();
 	public $locale = null;
 	public $languages = array();
-	public $language_translations = array();
+	public $languages_available = array();
+	public $languages_translated = array();
 	public $cur_pod = null;
+	public $option_key = 'pods_component_i18n_settings';
 
 	public $translatable_fields = array(
 		'label',
@@ -46,52 +49,99 @@ class Pods_I18n extends PodsComponent {
 	 */
 	public function __construct () {
 
-		$this->locale = get_locale();
+		//add_action( 'plugins_loaded', array( $this, 'init' ) );
+		$this->init();
 
-		if ( is_admin() && ( isset( $_GET['page'] ) && ( $_GET['page'] == 'pods' /*|| $_GET['page'] == 'pods-add-new'*/ ) ) ) {
-
-			$pod = null;
-			// Get the pod if available
-			if ( isset( $_GET['id'] ) && is_numeric( $_GET['id'] ) ) {
-				$pod = pods_api()->load_pod( array( 'id' => $_GET['id'] ) );
-				// Append options to root array for pods_v function to work
-				foreach ( $pod[ 'options' ] as $_option => $_value ) {
-					$pod[ $_option ] = $_value;
-				}
-			}
-			$this->cur_pod = $pod;
-
-			$this->languages = get_available_languages();
-
-			/**
-			 * format: array( language, version, updated, english_name, native_name, package, iso, strings )
-			 */
-			require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
-			$this->language_translations = wp_get_available_translations();
-
-			add_filter( 'pods_form_ui_field_text', array( $this, 'add_i18n_inputs' ), 10, 6 );
-		
-		} elseif ( is_admin() ) {
-			/*foreach ( pods_form()->field_types() as $type => $data ) {
-				//add_filter( 'pods_form_ui_field_' . $type . '_data', array( $this, 'field_options_i18n' ), 10, 3 );
-				//add_filter( 'pods_form_ui_field_' . $type . '_merge_attributes', array( $this, 'merge_attributes_fields_i18n' ), 10, 3 );
-			}*/
-
-			// Default filters for all fields
-			add_filter( 'pods_form_ui_label_text', array( $this, 'fields_ui_label_text_i18n' ), 10, 4 );
-			add_filter( 'pods_form_ui_comment_text', array( $this, 'fields_ui_comment_text_i18n' ), 10, 3 );
-
-			// Field specific
-			add_filter( 'pods_field_pick_data', array( $this, 'field_pick_data_i18n' ), 10, 6 );
+		// Do save action here because otherwise the loading of post_types get done first and labels aren't translated
+		if (   pods_is_admin( array( 'pods_i18n_activate_lanuages' ) ) 
+			&& isset( $_POST['_nonce_i18n'] ) 
+			&& wp_verify_nonce( $_POST['_nonce_i18n'], 'pods_i18n_activate_lanuages' ) 
+		) {
+			$this->save();
 		}
 
-		// Object filters
-		add_filter( 'pods_register_post_type', array( $this, 'pods_register_object_i18n' ), 10, 2 );
-		add_filter( 'pods_register_taxonomy', array( $this, 'pods_register_object_i18n' ), 10, 2 );
+	}
 
-		add_filter( 'pods_form_ui_field_date_args', array( $this, 'field_date_args_i18n' ), 10, 6 );
-		add_filter( 'pods_form_ui_field_datetime_args', array( $this, 'field_date_args_i18n' ), 10, 6 );
+	public function init() {
+		$this->settings = get_option( $this->option_key, array() );
 
+		// Polylang
+		if ( function_exists( 'PLL' ) && file_exists( plugin_dir_path( __FILE__ ) . '/I18n-polylang.php' ) ) {
+			include_once( plugin_dir_path( __FILE__ ) . '/I18n-polylang.php' );
+		}
+		// WPML
+		// Polylang has WPML compat functions with the same names so check constant ICL_SITEPRESS_VERSION for WPML
+		if ( defined('ICL_SITEPRESS_VERSION') && file_exists( plugin_dir_path( __FILE__ ) . '/I18n-wpml.php' ) ) {
+			include_once( plugin_dir_path( __FILE__ ) . '/I18n-wpml.php' );
+		}
+
+		if ( ! empty( $this->settings['enabled_languages'] ) ) {		
+
+			$this->languages = $this->settings['enabled_languages'];
+			$this->locale = get_locale();
+
+			if ( is_admin() ) {
+				
+				if ( isset( $_GET['page'] ) && ( $_GET['page'] == 'pods' /*|| $_GET['page'] == 'pods-add-new'*/ ) ) {
+
+					$this->admin_assets();
+
+					$pod = null;
+					// Get the pod if available
+					if ( isset( $_GET['id'] ) && is_numeric( $_GET['id'] ) ) {
+						$pod = pods_api()->load_pod( array( 'id' => $_GET['id'] ) );
+						// Append options to root array for pods_v function to work
+						foreach ( $pod[ 'options' ] as $_option => $_value ) {
+							$pod[ $_option ] = $_value;
+						}
+					}
+					$this->cur_pod = $pod;
+
+
+					add_filter( 'pods_form_ui_field_text', array( $this, 'add_i18n_inputs' ), 10, 6 );
+
+				}
+
+				/*foreach ( pods_form()->field_types() as $type => $data ) {
+					//add_filter( 'pods_form_ui_field_' . $type . '_data', array( $this, 'field_options_i18n' ), 10, 3 );
+					//add_filter( 'pods_form_ui_field_' . $type . '_merge_attributes', array( $this, 'merge_attributes_fields_i18n' ), 10, 3 );
+				}*/
+
+				// Default filters for all fields
+				add_filter( 'pods_form_ui_label_text', array( $this, 'fields_ui_label_text_i18n' ), 10, 4 );
+				add_filter( 'pods_form_ui_comment_text', array( $this, 'fields_ui_comment_text_i18n' ), 10, 3 );
+
+				// Field specific
+				add_filter( 'pods_field_pick_data', array( $this, 'field_pick_data_i18n' ), 10, 6 );
+			}
+
+			// Object filters
+			add_filter( 'pods_register_post_type', array( $this, 'pods_register_object_i18n' ), 10, 2 );
+			add_filter( 'pods_register_taxonomy', array( $this, 'pods_register_object_i18n' ), 10, 2 );
+
+			add_filter( 'pods_form_ui_field_date_args', array( $this, 'field_date_args_i18n' ), 10, 6 );
+			add_filter( 'pods_form_ui_field_datetime_args', array( $this, 'field_date_args_i18n' ), 10, 6 );
+
+		}
+	}
+
+	public function admin_assets() {
+		wp_enqueue_script( 'pods-admin-i18n', PODS_URL . 'components/I18n/pods-admin-i18n.js', array( 'jquery' ), '1.0', true );
+		$localize_script = array(
+			'__add_translation' => __( 'Add translation', 'pods' ),
+			'__toggle_translations' => __( 'Toggle translations', 'pods' ),
+			'__select' => __( 'Select', 'pods' ),
+		);
+		foreach ( $this->languages as $lang => $lang_data ) {
+			$lang_label = $this->create_lang_label( $lang_data );
+			if ( ! empty( $lang_label ) ) {
+				$lang_label = $lang . ' ('. $lang_label .')';
+			} else {
+				$lang_label = $lang;
+			}
+			$localize_script[ $lang ] = $lang_label;
+		}
+		wp_localize_script( 'pods-admin-i18n', 'pods_admin_i18n_strings', $localize_script );
 	}
 
 	/**
@@ -138,11 +188,15 @@ class Pods_I18n extends PodsComponent {
 			$pod = true;
 		}
 
-		foreach ( $this->languages as $lang ) {
+		$output .= '<br clear="both" />';
+		foreach ( $this->languages as $lang => $lang_data ) {
 			// Our own shiny label with language information
-			$lang_label = '<code style="font-size: 1em;">' . $lang . '</code>';
-			if ( array_key_exists( $lang, $this->language_translations ) ) {
-				$lang_label = $lang_label . ' ('. $this->language_translations[ $lang ]['english_name'] .'/'. $this->language_translations[ $lang ]['native_name'] .')';
+			$lang_code = '<code style="font-size: 1em;">' . $lang . '</code>';
+			$lang_label = $this->create_lang_label( $lang_data );
+			if ( ! empty( $lang_label ) ) {
+				$lang_label = $lang_code . ' ('. $lang_label .')';
+			} else {
+				$lang_label = $lang_code;
 			}
 			$lang_label = '<small>' . $lang_label . '</small>';
 
@@ -155,9 +209,10 @@ class Pods_I18n extends PodsComponent {
 			}
 
 			// Add the translation fields
-			$output .= '<br clear="both" />';
+			$output .= '<div class="pods-i18n-input pods-i18n-input-'.$lang.'" data-locale="'.$lang.'">';
 			$output .= PodsForm::label( $field_name, $lang_label, __( 'help', 'pods' ) );
 			$output .= PodsForm::field( $field_name, pods_v( $field_name, $pod ), 'text', null, $pod );
+			$output .= '</div>';
 		}
 		return $output;
 	}
@@ -203,7 +258,11 @@ class Pods_I18n extends PodsComponent {
 	public function fields_ui_label_text_i18n( $label, $name, $help, $options ) {
 		$locale = $this->locale;
 		// Validate field by checking if the default label is set and is the same as the label send with this function
-		if ( isset( $options['label'] ) && $options['label'] == $label && ! empty( $options[ 'label_' . $locale ] ) ) {
+		if (   isset( $options['label'] ) 
+			&& array_key_exists( $locale, $this->languages )
+			&& $options['label'] == $label 
+			&& ! empty( $options[ 'label_' . $locale ] ) 
+		) {
 			return $options[ 'label_' . $locale ];
 		}
 		return $label;
@@ -223,7 +282,11 @@ class Pods_I18n extends PodsComponent {
 	public function fields_ui_comment_text_i18n( $message, $name, $options ) {
 		$locale = $this->locale;
 		// Validate field by checking if the default description is set and is the same as the message send with this function
-		if ( isset( $options['description'] ) && $options['description'] == $message && ! empty( $options[ 'description_' . $locale ] ) ) {
+		if (   isset( $options['description'] ) 
+			&& array_key_exists( $locale, $this->languages )
+			&& $options['description'] == $message 
+			&& ! empty( $options[ 'description_' . $locale ] ) 
+		) {
 			return $options[ 'description_' . $locale ];
 		}
 		return $message;
@@ -246,7 +309,7 @@ class Pods_I18n extends PodsComponent {
 	public function field_pick_data_i18n( $data, $name, $value, $options, $pod, $id ) {
 		if ( isset( $data[''] ) && isset( $options['pick_select_text'] ) ) {
 			$locale = $this->locale;
-			if ( isset( $options['pick_select_text_' . $locale ] ) ) {
+			if ( isset( $options['pick_select_text_' . $locale ] ) && array_key_exists( $locale, $this->languages ) ) {
 				$data[''] = $options['pick_select_text_' . $locale ];
 			}
 		}
@@ -331,6 +394,10 @@ class Pods_I18n extends PodsComponent {
 
 		$locale = $this->locale;
 
+		if ( ! array_key_exists( $locale, $this->languages ) ) {
+			return $options;
+		}
+
 		$temp_options = $options;
 
 		// Load the pod
@@ -380,6 +447,40 @@ class Pods_I18n extends PodsComponent {
 		return $options;
 	}
 
+	public function save() {
+
+		$this->languages_available = get_available_languages();
+		$this->admin_assets();
+
+		/**
+		 * format: array( language, version, updated, english_name, native_name, package, iso, strings )
+		 */
+		require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
+		$this->languages_translated = wp_get_available_translations();
+
+		$new_languages = array();
+		if ( isset( $_POST['pods_i18n_enabled_languages'] ) && is_array( $_POST['pods_i18n_enabled_languages'] ) ) {
+			foreach ( $_POST['pods_i18n_enabled_languages'] as $lang ) {
+				if ( in_array( strip_tags( $lang ), $this->languages_available ) ) {
+					$new_languages[$lang] = array();
+					if ( isset( $this->languages_translated[$lang]['language'] ) ) {
+						$new_languages[$lang]['language'] = $this->languages_translated[$lang]['language'];
+					}
+					if ( isset( $this->languages_translated[$lang]['english_name'] ) ) {
+						$new_languages[$lang]['english_name'] = $this->languages_translated[$lang]['english_name'];
+					}
+					if ( isset( $this->languages_translated[$lang]['native_name'] ) ) {
+						$new_languages[$lang]['native_name'] = $this->languages_translated[$lang]['native_name'];
+					}
+				}
+			}
+		}
+		$this->settings['enabled_languages'] = $new_languages;
+		update_option( $this->option_key, $this->settings );
+		$this->languages = $new_languages;
+
+	}
+
 	/**
 	 * Build admin area
 	 *
@@ -389,65 +490,106 @@ class Pods_I18n extends PodsComponent {
 	 * @return void
 	 * @since 0.1
 	 */
-	/*
 	public function admin ( $options, $component ) {
 
-		//wp_get_installed_translations()
-		$api = pods_api();
+		$this->languages_available = get_available_languages();
+		$this->admin_assets();
 
-		$pods = $api->load_pods();
-		//$pod_templates = $api->load_templates();
-		//$pod_pages = $api->load_pages();
-		//$pod_helpers = $api->load_helpers();
+		/**
+		 * format: array( language, version, updated, english_name, native_name, package, iso, strings )
+		 */
+		require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
+		$this->languages_translated = wp_get_available_translations();
 
-		//echo '<pre>';print_r($pods);echo'</pre>';
+		if (   pods_is_admin( array( 'pods_i18n_activate_lanuages' ) ) 
+			&& isset( $_POST['_nonce_i18n'] ) 
+			&& wp_verify_nonce( $_POST['_nonce_i18n'], 'pods_i18n_activate_lanuages' ) 
+		) {
+			$new_languages = array();
+			if ( isset( $_POST['pods_i18n_enabled_languages'] ) && is_array( $_POST['pods_i18n_enabled_languages'] ) ) {
+				foreach ( $_POST['pods_i18n_enabled_languages'] as $lang ) {
+					if ( in_array( strip_tags( $lang ), $this->languages_available ) ) {
+						$new_languages[$lang] = array();
+						if ( isset( $this->languages_translated[$lang]['language'] ) ) {
+							$new_languages[$lang]['language'] = $this->languages_translated[$lang]['language'];
+						}
+						if ( isset( $this->languages_translated[$lang]['english_name'] ) ) {
+							$new_languages[$lang]['english_name'] = $this->languages_translated[$lang]['english_name'];
+						}
+						if ( isset( $this->languages_translated[$lang]['native_name'] ) ) {
+							$new_languages[$lang]['native_name'] = $this->languages_translated[$lang]['native_name'];
+						}
+					}
+				}
+			}
+			$this->settings['enabled_languages'] = $new_languages;
+			update_option( $this->option_key, $this->settings );
+			$this->languages = $new_languages;
+		}
 
-		$data = array();
+		// en_US is always installed
+		$data = array( 'en_US' => array(
+			'id' => 'en_US',
+			'locale' => 'en_US',
+			'lang' => 'English',
+			'lang_native' => 'English',
+			'enabled' => 'Default',
+		) );
 
-		foreach ( $pods as $key => $pod ) {
-			$fields = array();
-			$total_labels = count( $this->get_pod_labels( $pod ) );
-			$total_field_labels = 0;
-			foreach ( $pod['fields'] as $field ) {
-				$fields[] = $field['label'] . ' <code>'.$field['name'].'</code>';
-				$total_field_labels += count( $this->get_field_labels( $field ) );
+		foreach ( $this->languages_available as $lang ) {
+			$lang_label = $this->create_lang_label( $this->languages_translated[ $lang ] );
+			if ( ! empty( $lang_label ) ) {
+				$lang_label = $lang . ' ('. $lang_label .')';
+			} else {
+				$lang_label = $lang;
 			}
 
-			$data[ $key ] = array(
-				'id' => $pod['id'],
-				'pod' => $pod['label'],
-				'total_labels' => $total_labels,
-				'fields' => implode( '<br />', $fields ),
-				'total_field_labels' => $total_field_labels,
+			if ( array_key_exists( $lang, $this->languages ) ) {
+				$checked = 'checked="checked"';
+			} else {
+				$checked = '';
+			}
+			$enabled = '<input type="checkbox" name="pods_i18n_enabled_languages['.$lang.']" value="'.$lang.'" '.$checked.'/>';
+
+			$data[ $lang ] = array(
+				'id' => $lang,
+				'locale' => $lang,
+				'lang' => $this->languages_translated[ $lang ]['english_name'],
+				'lang_native' => $this->languages_translated[ $lang ]['native_name'],
+				'enabled' => $enabled,
 			);
 		}
 
 		$ui = array(
 			'component' => $component,
-			'data' => $data,
+			/*'data' => $data,
 			'total' => count( $data ),
-			'total_found' => count( $data ),
-			'items' => 'Multilingual Pods labels',
-			'item' => 'Multilingual Pods labels',
+			'total_found' => count( $data ),*/
+			'items' => 'Languages',
+			'item' => 'Language',
 			'fields' => array(
 				'manage' => array(
-					'pod' => array( 'label' => __( 'Pod', 'pods' ) ),
-					'total_labels' => array( 'label' => __( 'Total labels', 'pods' ) ),
-					'fields' => array(
+					'enabled' => array( 
+						'label' => __( 'Active', 'pods' ),
+						'type' => 'text',
+					),
+					'locale' => array( 'label' => __( 'Locale', 'pods' ) ),
+					'lang' => array( 'label' => __( 'Language', 'pods' ) ),
+					'lang_native' => array( 'label' => __( 'Native name', 'pods' ) ),
+					/*'fields' => array(
 						'label' => __( 'Fields', 'pods' ),
 						'type' => 'text',
 						'options' => array(
 							'text_allow_html' => 1,
 							'text_allowed_html_tags' => 'br code'
 						)
-					),
-					'total_field_labels' => array( 'label' => __( 'Total field labels', 'pods' ) ),
+					),*/
 				)
 			),
-			'actions_disabled' => array( 'add', 'delete', 'duplicate', 'view', 'export' ),
+			'actions_disabled' => array( 'edit', 'add', 'delete', 'duplicate', 'view', 'export' ),
 			'actions_custom' => array(
 				//'add' => array( $this, 'admin_add' ),
-				'edit' => array( $this, 'admin_edit' ),
+				//'edit' => array( $this, 'admin_edit' ),
 				//'delete' => array( $this, 'admin_delete' )
 			),
 			'search' => false,
@@ -456,69 +598,51 @@ class Pods_I18n extends PodsComponent {
 			'pagination' => false
 		);
 
-		if ( !pods_is_admin( array( 'pods_i18n_labels_edit' ) ) )
-			$ui[ 'actions_disabled' ][] = 'edit';
+		$data = apply_filters( 'pods_component_i18n_admin_data', $data );
+		$ui['fields'] = apply_filters( 'pods_component_i18n_admin_ui_fields', $ui['fields'], $data );
+
+		$ui['data'] = $data;
+		$ui['total'] = count( $data );
+		$ui['total_found'] = count( $data );
+
+		/*if ( !pods_is_admin( array( 'pods_i18n_activate_lanuages' ) ) )
+			$ui[ 'actions_disabled' ][] = 'edit';*/
 
 		pods_ui( $ui );
+
+		echo '<div id="pods_i18n_settings_save">';
+		echo '<input type="hidden" id="nonce_i18n" name="_nonce_i18n" value="' . wp_create_nonce('pods_i18n_activate_lanuages') . '" />';
+		submit_button();
+		echo '</div>';
 	}
 
-	public function admin_edit() {
-		
-	}
+	public function create_lang_label( $lang_data ) {
 
-	public function get_pod_labels( $pod ) {
+		$english_name = '';
+		$native_name = '';
 
-		$labels = array();
-
-		if ( isset( $pod['label'] ) ) {
-			$labels['label'] = $pod['label'];
+		if ( isset( $lang_data['english_name'] ) ) {
+			$english_name = $lang_data['english_name'];
 		}
-		// Add option labels
-		if ( isset( $pod['options'] ) && is_array( $pod['options'] ) ) {
-			foreach( $pod['options'] as $key => $value ) {
-				if ( strpos( $key, 'label' ) !== false ) {
-					$labels[ $key ] = $value;
-				}
+		if ( isset( $lang_data['native_name'] ) ) {
+			$native_name = $lang_data['native_name'];
+		}
+
+		if ( ! empty( $native_name ) && ! empty( $english_name ) ) {
+			if ( $native_name == $english_name ) {
+				return $english_name;
+			} else {
+				return $english_name . ' / ' . $native_name;
+			}
+		} else {
+			if ( ! empty( $english_name ) ) {
+				return $english_name;
+			}
+			if ( ! empty( $native_name ) ) {
+				return $native_name;
 			}
 		}
-		if ( isset( $pod['description'] ) ) {
-			$labels['description'] = $pod['description'];
-		}
-
-		return $labels;
-
+		return '';
 	}
 
-	public function get_field_labels( $pod ) {
-
-		$labels = array();
-		$field_option_labels = array(
-			'file_add_button',
-			'file_modal_title',
-			'file_modal_add_button',
-			'placeholder',
-		);
-
-		if ( isset( $pod['label'] ) ) {
-			$labels['label'] = $pod['label'];
-		}
-		// Add option labels
-		if ( isset( $pod['options'] ) && is_array( $pod['options'] ) ) {
-			foreach( $pod['options'] as $key => $value ) {
-				if ( strpos( $key, 'label' ) !== false ) {
-					$labels[ $key ] = $value;
-				}
-				if ( in_array( $key, $field_option_labels ) ) {
-					$labels[ $key ] = $value;
-				}
-			}
-		}
-		if ( isset( $pod['description'] ) ) {
-			$labels['description'] = $pod['description'];
-		}
-
-		return $labels;
-
-	}
-	*/
 }
---
+++ components/I18n/pods-admin-i18n.js
@@ -0,0 +1,68 @@
+( function ( $ ) {
+
+	if ( $('#pods_i18n_settings_save').length ) {
+		$(document).on('click', '#pods_i18n_settings_save', function() {
+			$('#nonce_i18n').appendTo('.pods-admin form#posts-filter');
+			$('.pods-admin form#posts-filter').attr('method', 'post').submit();
+		});
+	}
+
+	if ( $('.pods-i18n-input').length ) {
+		//pods_edit_i18n.init();
+		$('.pods-admin .postbox-container #submitdiv').after('<div id="toggle_i18n" class="postbox"><div class="inside"><p><button class="button-secondary">'+pods_admin_i18n_strings.__toggle_translations+'</button></p></div></div>');
+		$(document).on('click', '.pods-admin .postbox-container #toggle_i18n', function(e) {
+			e.preventDefault();
+			if ( $('.pods-i18n-input').first().css('display') == 'none' ) {
+				$('.pods-i18n-input').each( function() { $(this).show(); });
+			} else {
+				$('.pods-i18n-input').each( function() { $(this).hide(); });
+			}
+			return false;
+		});
+	}
+	
+	/*pods_edit_i18n = {
+		available_languages: {},
+		locale_container_selector: '.pods-i18n-input',
+		init: function() {
+			var locale_container_selector = pods_edit_i18n.locale_container_selector;
+			// Hide empty translations
+			$( locale_container_selector ).each( function() {
+
+				if ( $(this).attr('data-locale') != '' && typeof pods_edit_i18n.available_languages['__'+$(this).attr('data-locale')] == 'undefined' ) {
+					pods_edit_i18n.available_languages[$(this).attr('data-locale')] = $(this).attr('data-locale');
+				}
+
+				if ( $('.pods-form-ui-field', this).val() == '' ) {
+					$(this).hide();
+				}
+
+				if ( $(this).is(':last-child') ) {
+					$(this).after( pods_edit_i18n.add_new_link() );
+				}
+			} );
+		},
+		add_new_link: function( $languages ) {
+
+			$html = '<div class="pods-i18n-add">';
+
+			$html += '<label></label>';
+			$html += '<select class="select-translation">';
+			$html += '<option>- '+pods_admin_i18n_strings.__select+' -</option>';
+			for ( var lang in pods_edit_i18n.available_languages ) {
+				var label = pods_edit_i18n.available_languages[lang];
+				if ( typeof pods_admin_i18n_strings[lang] != 'undefined' ) {
+					label = pods_admin_i18n_strings[lang];
+				}
+				$html += '<option value="'+pods_edit_i18n.available_languages[lang]+'">'+label+'</option>';
+			}
+			$html += '</select>';
+			$html += '<a href="" class="add-translation">'+pods_admin_i18n_strings.__add_translation+'</a>';
+
+			$html += '</div>';
+
+			return $html;
+		}
+	};*/
+
+} )( jQuery );

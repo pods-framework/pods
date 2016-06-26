a94be8e96b5eb623000c450edd781a5a5b40a7d8
pglewis <pglewis13@gmail.com>
2016-06-26 00:53:04 -0400

    Yank old testing code

--- ui/fields-mv/_src/pods-fields-ready.js
+++ ui/fields-mv/_src/pods-fields-ready.js
@@ -1,12 +1,10 @@
 /*global jQuery, _, Backbone, Mn */
 import { podsFieldsInit } from './pods-fields-init'; // jQuery plugin
 import * as fieldClasses from './pods-fields-manifest'; // All fields
-import * as Queue from './file-upload/views/file-upload-queue'; // @todo: just here for testing the file upload queue
 
 const PodsUI = {
 	fieldClasses: fieldClasses,
-	fields      : {},
-	Queue       : Queue
+	fields      : {}
 };
 export default PodsUI;
 

fe147462358ae769a4b953987010363441c57b8e
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-06-26 02:08:42 +0200

    Indentation

--- ui/fields/oembed.php
+++ ui/fields/oembed.php
@@ -1,19 +1,19 @@
 <?php
-    $attributes = array();
-    $attributes[ 'type' ] = 'text';
-    $attributes[ 'value' ] = $value;
-    $attributes[ 'tabindex' ] = 2;
-    $attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
+	$attributes = array();
+	$attributes[ 'type' ] = 'text';
+	$attributes[ 'value' ] = $value;
+	$attributes[ 'tabindex' ] = 2;
+	$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
 
-    if ( pods_var( 'readonly', $options, false ) ) {
-        $attributes[ 'readonly' ] = 'READONLY';
+	if ( pods_var( 'readonly', $options, false ) ) {
+		$attributes[ 'readonly' ] = 'READONLY';
 
-        $attributes[ 'class' ] .= ' pods-form-ui-read-only';
-    }
-    
+		$attributes[ 'class' ] .= ' pods-form-ui-read-only';
+	}
+
     $show_preview = (int) pods_v( $form_field_type . '_show_preview', $options, 0 );
 ?>
-    <input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
+	<input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
 
 <?php 
 if ( 1 == $show_preview ) {

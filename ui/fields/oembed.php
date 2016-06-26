c0bee5f549829fa08bf05fed331dad550e205e5e
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-06-26 02:13:35 +0200

    Indentation

--- ui/fields/oembed.php
+++ ui/fields/oembed.php
@@ -11,7 +11,7 @@
 		$attributes[ 'class' ] .= ' pods-form-ui-read-only';
 	}
 
-    $show_preview = (int) pods_v( $form_field_type . '_show_preview', $options, 0 );
+	$show_preview = (int) pods_v( $form_field_type . '_show_preview', $options, 0 );
 ?>
 	<input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
 
@@ -22,12 +22,11 @@ if ( 1 == $show_preview ) {
 		<?php _e( 'Preview', 'pods' );?>
 	</p>
 	<input type="hidden" id="<?php echo esc_js( pods_js_name( $attributes[ 'id' ] ) ); ?>_preview_nonce" name="_nonce_pods_oembed" value="<?php echo wp_create_nonce( 'pods_field_embed_preview' ); ?>" />
-    <div class="pods-oembed-preview">
-        <?php echo PodsForm::field_method( $form_field_type, 'display', $value, $name, $options ); ?>
-    </div>
+	<div class="pods-oembed-preview">
+		<?php echo PodsForm::field_method( $form_field_type, 'display', $value, $name, $options ); ?>
+	</div>
 	<script type="text/javascript">
 		jQuery( function( $ ){
-//https://www.youtube.com/watch?v=bYEE2i3nPOM
 			var element_<?php echo esc_js( pods_js_name( $attributes[ 'id' ] ) ); ?> = $( '#<?php echo esc_js( $css_id ); ?>' ),
 				timeout_<?php echo esc_js( pods_js_name( $attributes[ 'id' ] ) ); ?> = false,
 				pods_ajaxurl = ajaxurl + '?pods_ajax=1';
@@ -40,15 +39,15 @@ if ( 1 == $show_preview ) {
 						'_nonce_pods_oembed': $(this).parent().find('#<?php echo esc_js( pods_js_name( $attributes[ 'id' ] ) ); ?>_preview_nonce').val(),
 						'pods_field_embed_value': value
 					}
-                    $.ajax({
-                        type : 'POST',
-                        url : pods_ajaxurl,
-                        cache : false,
-                        data : postdata,
-                        success : function ( response ) {
-                        	console.log(response);
-                        }
-                	});
+					$.ajax({
+						type : 'POST',
+						url : pods_ajaxurl,
+						cache : false,
+						data : postdata,
+						success : function ( response ) {
+							console.log(response);
+						}
+					});
 				}, 500);
 			});
 

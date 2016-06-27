9365eb9655c9208b98f93bfaaaa63fbeb2157f35
pglewis <pglewis13@gmail.com>
2016-06-27 00:22:12 -0400

    Remove unnecesasry import from the rollup dev build config file

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,6 +1,5 @@
 import string from 'rollup-plugin-string';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 
 export default {
 	entry     : 'ui/fields-mv/_src/pods-fields-ready.js',

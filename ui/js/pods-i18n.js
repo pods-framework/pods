53e09d5c3bc992260a8b25e8e10a7e2865b63c71
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2016-07-01 12:00:41 +0200

    Convert to js object

--- ui/js/pods-i18n.js
+++ ui/js/pods-i18n.js
@@ -1,25 +1,31 @@
-function pods__( str ) {
 
-	if ( typeof pods_localized_strings != 'undefined' ) {
+PodsI18n = {
+	__: function( str ) {
+		return this.translate_string( str );
+	},
+	_e: function( str ) {
+		print( this.translate_string( str ) );
+	},
+	translate_string: function( str ) {
+		if ( typeof pods_localized_strings != 'undefined' ) {
 
-		/**
-		 * Converts string into reference object variable
-		 * Uses the same logic as PHP to create the same references
-		 *
-		 * 1. Remove capitals
-		 * 2. Remove all punctuation etc.
-		 * 3. Trim
-		 * 4. Convert whitespaces to underscores
-		 */
-		var ref = '__' + str.toLowerCase().replace( /[^a-z ]+/g, ' ' ).trim().replace( /\s{2,}/g, "_" ).replace( / /g, '_' );
-
-		if ( typeof pods_localized_strings[ ref ] != 'undefined' ) {
-			return pods_localized_strings[ ref ];
+			/**
+			 * Converts string into reference object variable
+			 * Uses the same logic as PHP to create the same references
+			 * 
+			 * 1. Remove capitals
+			 * 2. Remove all punctuation etc.
+			 * 3. Trim
+			 * 4. Convert whitespaces to underscores
+			 */
+			var ref = '__' + str.toLowerCase().replace(/[^a-z ]+/g, ' ').trim().replace(/\s{2,}/g, "_").replace(/ /g,'_');
+			
+			if ( typeof pods_localized_strings[ ref ] != 'undefined' ) {
+				return pods_localized_strings[ ref ];
+			} else if ( pods_localized_strings.debug == true ) {
+				console.log( 'Pods__: String not found "' + str + '" (reference used: "' + ref + '")');
+			}
 		}
-		else if ( pods_localized_strings.debug == true ) {
-			console.log( 'Pods__: String not found "' + str + '" (reference used: "' + ref + '")' );
-		}
+		return str;
 	}
-
-	return str;
 }

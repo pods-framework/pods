11025cf575424ec760b5a5f7800915d72b0a0174
pglewis <pglewis13@gmail.com>
2016-07-01 16:12:29 -0400

    Whitespace, reformatting, linting comment for the global

--- ui/js/pods-i18n.js
+++ ui/js/pods-i18n.js
@@ -1,12 +1,15 @@
-
-PodsI18n = {
-	__: function( str ) {
+/*global pods_localized_strings */
+'use strict';
+var PodsI18n = {
+	__: function ( str ) {
 		return this.translate_string( str );
 	},
-	_e: function( str ) {
+
+	_e: function ( str ) {
 		print( this.translate_string( str ) );
 	},
-	translate_string: function( str ) {
+
+	translate_string: function ( str ) {
 		if ( typeof pods_localized_strings != 'undefined' ) {
 
 			/**
@@ -14,13 +17,14 @@ PodsI18n = {
 			 * Uses the same logic as PHP to create the same references
 			 */
 			var ref = '__' + str;
-			
+
 			if ( typeof pods_localized_strings[ ref ] != 'undefined' ) {
 				return pods_localized_strings[ ref ];
-			} else if ( pods_localized_strings.debug == true ) {
-				console.log( 'Pods__: String not found "' + str + '" (reference used: "' + ref + '")');
+			}
+			else if ( pods_localized_strings.debug == true ) {
+				console.log( 'Pods__: String not found "' + str + '" (reference used: "' + ref + '")' );
 			}
 		}
 		return str;
 	}
-}
+};

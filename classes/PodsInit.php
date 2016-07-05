b74ff7895ece9abdbe5793ab4b6e7091398f01c1
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-07-05 11:50:03 +0200

    Remove old localize_assets function

--- classes/PodsInit.php
+++ classes/PodsInit.php
@@ -330,31 +330,6 @@ class PodsInit {
 			true
 		);
 		wp_register_style( 'pods-flex', PODS_URL . 'ui/css/pods-flex.css', array(), PODS_VERSION );
-
-		$this->localize_assets();
-	}
-
-	/**
-	 * Localize assets
-	 */
-	public function localize_assets() {
-		wp_localize_script('pods', 'pods_localized_strings', array(
-			'__is_required' => __( '%s is required.', 'pods' ),
-			'__add' => __( 'Add', 'pods' ),
-			'__add_new' => __( 'Add New', 'pods' ),
-			'__add_new_record' => __( 'Add New Record', 'pods' ),
-			'__added' => __( 'Added!', 'pods' ),
-			'__added_choose_another_or_close_this_box' => __( 'Added! Choose another or <a href="#">close this box</a>', 'pods' ),
-			'__copy' => __( 'Copy', 'pods' ),
-			'__reorder' => __( 'Reorder', 'pods' ),
-			'__remove' => __( 'Remove', 'pods' ),
-			'__download' => __( 'Download', 'pods' ),
-			'__view' => __( 'View', 'pods' ),
-			'__edit' => __( 'Edit', 'pods' ),
-			'__navigating_away_from_this_page_will_discard_any_changes_you_have_made' => __( 'Navigating away from this page will discard any changes you have made.', 'pods' ),
-			'__unable_to_process_request_please_try_again' => __( 'Unable to process request, please try again.', 'pods' ),
-			'__there_was_an_issue_with_the_file_upload_please_try_again' => __( 'There was an issue with the file upload, please try again.', 'pods' ),
-		));
 	}
 
 	/**

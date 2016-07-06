01cfef546039044c57858fd72b561f99e82f18aa
pglewis <pglewis13@gmail.com>
2016-07-06 16:20:02 -0400

    Default globals we need to make available as they would be in a WordPress environment

---
+++ tests-js/fields-mv/_setup.js
@@ -0,0 +1,6 @@
+global.document = require( 'jsdom' ).jsdom( '<html></html>' );
+global.window = document.defaultView;
+global.jQuery = require( 'jquery' );
+global._ = require( 'underscore' );
+global.Backbone = require( 'backbone' );
+global.Mn = require( 'backbone.marionette' );

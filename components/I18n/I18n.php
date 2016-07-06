c993e798f2b84ac45a22f44bb68222183dbdd916
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-07-06 22:45:09 +0200

    Change metabox actions
    Compat with https://github.com/pods-framework/pods/pull/3637
    (commit: #129c4f2)

--- components/I18n/I18n.php
+++ components/I18n/I18n.php
@@ -116,7 +116,7 @@ class Pods_Component_I18n extends PodsComponent {
 						//add_filter( 'pods_admin_setup_edit_options', array( $this, 'pod_options' ), 12, 2 );
 
 						//Add options metabox to the pod edit screens
-						add_action( 'add_meta_boxes', array( $this, 'admin_meta_box' ), 10 );
+						add_action( 'pods_add_meta_boxes', array( $this, 'admin_meta_box' ), 10 );
 
 						//Add the i18n input fields based on existing fields
 						add_filter( 'pods_form_ui_field_text', array( $this, 'add_i18n_inputs' ), 10, 6 );
@@ -604,7 +604,7 @@ class Pods_Component_I18n extends PodsComponent {
 		echo '<input type="hidden" id="nonce_i18n" name="_nonce_i18n" value="' . wp_create_nonce('pods_i18n_activate_lanuages') . '" />';
 		submit_button();
 		echo '</div>';
-		
+
 		echo '</div>';
 	}
 
@@ -659,7 +659,6 @@ class Pods_Component_I18n extends PodsComponent {
 	 * @since 0.1
 	 */
 	public function admin_meta_box() {
-		add_meta_box( 'pods_i18n', __( 'Translation options', 'pods' ), array( $this, 'meta_box' ), 'pods_edit', 'side', 'default' );
 	}
 
 	/**

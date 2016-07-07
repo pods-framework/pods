953db9141edac4c93b0b3715f97cc54cdb7c8449
James Golovich <james@gnuinter.net>
2016-07-07 13:13:02 -0700

    Fix broken tests

--- tests/includes/testcase.php
+++ tests/includes/testcase.php
@@ -652,6 +652,9 @@ class Pods_UnitTestCase extends \WP_UnitTestCase {
 					}
 					else {
 						$v = sprintf( $v, wp_generate_password( 4, false ) );
+						if ( 'permalink' === $k ) {
+							$v = strtolower( $v );
+						}
 					}
 
 					$item_data[ 'data' ][ $k ] = $v;
@@ -755,6 +758,9 @@ class Pods_UnitTestCase extends \WP_UnitTestCase {
 								}
 								else {
 									$v = sprintf( $v, wp_generate_password( 4, false ) );
+									if ( 'permalink' === $k ) {
+										$v = strtolower( $v );
+									}
 								}
 
 								$pod_item_data[ 'data' ][ $k ] = $v;

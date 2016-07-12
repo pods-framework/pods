1ff9abed006368d5f6b37532f3a78a7db307df94
pglewis <pglewis13@gmail.com>
2016-07-11 23:35:39 -0400

    Set data default properties to ensure they exist; use dot notation

--- ui/fields-mv/_src/pods-mv-fields-init.js
+++ ui/fields-mv/_src/pods-mv-fields-init.js
@@ -1,8 +1,8 @@
 /*global jQuery, _, Backbone, Mn */
 import * as fieldClasses from './pods-mv-fields-manifest';
-import { PodsFieldModel } from './core/pods-field-model';
-import { FileUploadCollection } from './file-upload/models/file-upload-model';
-import { RelationshipCollection } from './pick/models/relationship-model';
+import {PodsFieldModel} from './core/pods-field-model';
+import {FileUploadCollection} from './file-upload/models/file-upload-model';
+import {RelationshipCollection} from './pick/models/relationship-model';
 
 /**
  * @param {string} fieldType
@@ -33,7 +33,16 @@ const fieldFactory = function ( fieldType ) {
 export const podsMVFieldsInit = function ( fields ) {
 
 	return this.each( function () {
-		let data = {}, fieldModel, fieldId, fieldControl, field;
+		let data, fieldModel, fieldId, fieldControl, field;
+
+		data = {
+			field_type: '',
+			field_meta: {
+				field_attributes: {},
+				field_options   : {}
+			},
+			model_data: {}
+		};
 
 		// Combine data from all in-line data scripts in the container
 		jQuery( this ).find( 'script.pods-mv-field-data' ).each( function () {
@@ -44,22 +53,22 @@ export const podsMVFieldsInit = function ( fields ) {
 		);
 
 		// Ignore anything that doesn't have the field type set
-		if ( data[ 'field_type' ] !== undefined ) {
+		if ( data.field_type !== undefined ) {
 
-			fieldControl = fieldFactory( data[ 'field_type' ] );
+			fieldControl = fieldFactory( data.field_type );
 			if ( fieldControl.control !== undefined ) {
 				fieldModel = new PodsFieldModel( {
-					type      : data[ 'field_type' ],
-					attributes: data[ 'field_meta' ][ 'field_attributes' ],
-					options   : data[ 'field_meta' ][ 'field_options' ]
+					type      : data.field_type,
+					attributes: data.field_meta.field_attributes,
+					options   : data.field_meta.field_options
 				} );
 
-				fieldId = data[ 'field_meta' ][ 'field_attributes' ].id;
+				fieldId = data.field_meta.field_attributes.id;
 
 				field = new fieldControl.control( {
 					el        : this,
 					model     : fieldModel,
-					collection: new fieldControl.collection( data[ 'model_data' ] )
+					collection: new fieldControl.collection( data.model_data )
 				} );
 				field.render();
 				fields[ fieldId ] = field;

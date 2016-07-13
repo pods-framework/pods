2a31774b2efca8297b2b700c9fde56b619d4e7f3
pglewis <pglewis13@gmail.com>
2016-07-13 05:36:47 -0400

    Add babel-root-import to simplify import paths

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,15 +2,18 @@ import string from 'rollup-plugin-string';
 import babel from 'rollup-plugin-babel';
 
 export default {
-	entry     : 'ui/fields-mv/_src/pods-mv-fields.js',
-	dest      : 'ui/fields-mv/js/pods-mv-fields.min.js',
-	format    : 'iife',
-	moduleName: 'PodsMVFields',
-	plugins   : [
-		string( { extensions: [ '.html' ] } ),
+	"entry"     : 'ui/fields-mv/_src/pods-mv-fields.js',
+	"dest"      : 'ui/fields-mv/js/pods-mv-fields.min.js',
+	"format"    : 'iife',
+	"moduleName": 'PodsMVFields',
+	"plugins"   : [
+		string( { "extensions": [ '.html' ] } ),
 		babel( {
-			babelrc: false, // Ignore the .babelrc file which is there for mocha tests
-			presets: [ 'es2015-rollup' ]
+			"babelrc": false, // Ignore the .babelrc file which is there for mocha tests
+			"presets": [ 'es2015-rollup' ],
+			"plugins": [
+				"babel-root-import"
+			]
 		} )
 	]
 };
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -3,15 +3,18 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-	entry     : 'ui/fields-mv/_src/pods-mv-fields.js',
-	dest      : 'ui/fields-mv/js/pods-mv-fields.min.js',
-	format    : 'iife',
-	moduleName: 'PodsMVFields',
-	plugins   : [
-		string( { extensions: [ '.html' ] } ),
+	"entry"     : 'ui/fields-mv/_src/pods-mv-fields.js',
+	"dest"      : 'ui/fields-mv/js/pods-mv-fields.min.js',
+	"format"    : 'iife',
+	"moduleName": 'PodsMVFields',
+	"plugins"   : [
+		string( { "extensions": [ '.html' ] } ),
 		babel( {
-			babelrc: false, // Ignore the .babelrc file which is there for mocha tests
-			presets: [ 'es2015-rollup' ]
+			"babelrc": false, // Ignore the .babelrc file which is there for mocha tests
+			"presets": [ 'es2015-rollup' ],
+			"plugins": [
+				"babel-root-import"
+			]
 		} ),
 		uglify()
 	]

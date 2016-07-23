3293d4fcfc9a31bff28c7f7bebeaf1d1875fc83d
pglewis <pglewis13@gmail.com>
2016-07-23 02:13:59 -0400

    A couple of basic tests of the main LayoutView for the Pick field

---
+++ tests/js/fields-mv/pick/pick-spec.js
@@ -0,0 +1,53 @@
+/*global assert */
+import {PodsFieldModel} from '~/ui/fields-mv/_src/core/pods-field-model';
+import {Pick} from '~/ui/fields-mv/_src/pick/pick';
+
+describe( 'Pick field', function () {
+	let field, $el;
+	let fieldModel = new PodsFieldModel();
+
+	/**
+	 * Setup a target div for the Pick field
+	 */
+	beforeEach( function () {
+		jQuery( document.body ).append( '<div id="target">' );
+		$el = jQuery( '#target' );
+	} );
+
+	/**
+	 * Post test clean-up, after every it()
+	 */
+	afterEach( function () {
+		jQuery( document.body ).empty();
+
+		if ( field instanceof Backbone.View ) {
+			field.destroy();
+		}
+	} );
+
+	/**
+	 *
+	 */
+	it( 'Does not throw an exception with defaults', function () {
+		field = new Pick( {
+			el   : $el,
+			model: fieldModel
+		} );
+
+		assert.doesNotThrow( field.render );
+	} );
+
+	/**
+	 *
+	 */
+	it( 'Throws an exception on invalid view name', function () {
+		fieldModel.set( 'options', { view_name: 'not a view' } );
+
+		field = new Pick( {
+			el   : $el,
+			model: fieldModel
+		} );
+		assert.throws( field.render, Error );
+	} );
+
+} );

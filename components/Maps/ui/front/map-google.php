7670735d292f79c68778df8ce6b7445815dca575
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-12-08 17:17:09 +0100

    Rename map.php to map-google.php

--- components/Maps/ui/front/map-google.php
+++ components/Maps/ui/front/map-google.php
@@ -1,33 +1,61 @@
 <?php
 wp_enqueue_script( 'googlemaps' );
-wp_enqueue_style( 'pods-component-address-maps' );
+wp_enqueue_script( 'pods-maps' );
 
 $attributes = array();
 $attributes = PodsForm::merge_attributes( $attributes, $name, '', $options );
 
-$address = '';
-if ( isset( $value['address'] ) ) {
-	$address = implode( ' ', $value['address'] );
+$map_options = array();
+if ( ! empty( $options[ $type . '_map_zoom' ] ) ) {
+	$map_options['zoom'] = (int) $options[ $type . '_map_zoom' ];
+} else {
+	$map_options['zoom'] = (int) Pods_Component_Maps::$options['map_zoom'];
 }
+if ( ! empty( $options[ $type . '_map_type' ] ) ) {
+	$map_options['type'] = $options[ $type . '_map_type' ];
+} else {
+	$map_options['type'] = Pods_Component_Maps::$options['map_type'];
+}
+if ( ! empty( $options[ $type . '_map_marker' ] ) ) {
+	$map_options['marker'] = $options[ $type . '_map_marker' ];
+} else {
+	$map_options['marker'] = Pods_Component_Maps::$options['map_marker'];
+}
+
+if ( ! isset( $address_html ) ) {
+	$format = PodsForm::field_method( 'address', 'default_display_format' );
+	if ( $options['address_display_type'] == 'custom' ) {
+		$format = $options['address_display_type_custom'];
+	}
+	$address_html = PodsForm::field_method( 'address', 'format_to_html', $format, $value, $options );
+}
+$value['address_html'] = $address_html;
+
 ?>
-<div id="<?php echo $attributes['id'] . '-map-canvas' ?>" class="pods-address-maps-map-canvas"></div>
+<div id="<?php echo $attributes['id'] . '-map-canvas' ?>" class="pods-address-maps-map-canvas" data-value='<?php echo json_encode( $value ) ?>'></div>
 
 <script type="text/javascript">
-	document.addEventListener( "DOMContentLoaded", function() { 
+	document.addEventListener( "DOMContentLoaded", function() {
 		jQuery( document ).ready( function ( $ ) {
-			var mapDiv = document.getElementById( '<?php echo $attributes['id'] . '-map-canvas'?>' );
+			var mapDiv = document.getElementById( '<?php echo $attributes['id'] . '-map-canvas' ?>' );
+			var value = $( '#<?php echo $attributes['id'] . '-map-canvas' ?>' ).attr('data-value');
 			var address = '<?php echo $address; ?>';
 
 			var map = null;
 			var geocoder = null;
 			var marker = null;
+			var mapOptions = {
+				center: null, // default (Chicago)
+				marker: '<?php echo $map_options['marker'] ?>',
+				zoom: <?php echo $map_options['zoom'] ?>,
+				type: '<?php echo $map_options['type'] ?>'
+			};
 
 			//------------------------------------------------------------------------
 			// Initialze the map
 			//
 			//(function () {
 
-				var zoom = 10;
 				var center = new google.maps.LatLng( 40.026, -82.936 );
 
 				map = new google.maps.Map( mapDiv, {

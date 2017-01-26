dacc0eb9d9b0d5ac0d0c7309ac400e2a5c575b03
Scott Kingsley Clark <lol@scottkclark.com>
2017-01-26 02:31:23 -0600

    Move rollup into _src folder for DFV

--- ui/js/pods-dfv/_src/rollup.config.prod.js
+++ ui/js/pods-dfv/_src/rollup.config.prod.js
@@ -2,10 +2,10 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-	"entry"     : 'ui/fields-mv/_src/pods-mv-fields.js',
-	"dest"      : 'ui/fields-mv/js/pods-mv-fields.min.js',
+	"entry"     : 'ui/js/pods-dfv/_src/pods-dfv.js',
+	"dest"      : 'ui/js/pods-dfv/pods-dfv.min.js',
 	"format"    : 'iife',
-	"moduleName": 'PodsMVFields',
+	"moduleName": 'PodsDFV',
 	"plugins"   : [
 		babel( {
 			"babelrc": false, // Ignore the .babelrc file which is there for mocha tests

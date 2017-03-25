bcb62eec93683e63967134e64ee5c04ca2814762
Scott Kingsley Clark <lol@scottkclark.com>
2017-03-25 00:00:50 -0500

    Avatar field using DFV based on File field class

--- classes/fields/avatar.php
+++ classes/fields/avatar.php
@@ -1,8 +1,10 @@
 <?php
+require_once( PODS_DIR . 'classes/fields/file.php' );
+
 /**
  * @package Pods\Fields
  */
-class PodsField_Avatar extends PodsField {
+class PodsField_Avatar extends PodsField_File {
 
     /**
      * Field Type Group
@@ -37,9 +39,7 @@ class PodsField_Avatar extends PodsField {
     public static $pod_types = array( 'user' );
 
     /**
-     * Do things like register/enqueue scripts and stylesheets
-     *
-     * @since 2.0
+	 * {@inheritdoc}
      */
     public function __construct () {
         self::$label = __( 'Avatar', 'pods' );
@@ -52,240 +52,37 @@ class PodsField_Avatar extends PodsField {
      *
      * @since 2.0
      */
-    public function options () {
-        $sizes = get_intermediate_image_sizes();
+	public function options() {
 
-        $image_sizes = array();
+		$options = parent::options();
 
-        foreach ( $sizes as $size ) {
-            $image_sizes[ $size ] = ucwords( str_replace( '-', ' ', $size ) );
-        }
+		unset( $options[ self::$type . '_type' ] );
+		unset( $options[ self::$type . '_allowed_extensions' ] );
+		unset( $options[ self::$type . '_field_template' ] );
+		unset( $options[ self::$type . '_wp_gallery_output' ] );
+		unset( $options[ self::$type . '_wp_gallery_link' ] );
+		unset( $options[ self::$type . '_wp_gallery_columns' ] );
+		unset( $options[ self::$type . '_wp_gallery_random_sort' ] );
+		unset( $options[ self::$type . '_wp_gallery_size' ] );
 
-        $options = array(
-            self::$type . '_uploader' => array(
-                'label' => __( 'Avatar Uploader', 'pods' ),
-                'default' => 'attachment',
-                'type' => 'pick',
-                'data' => apply_filters(
-                    'pods_form_ui_field_avatar_uploader_options',
-                    array(
-                        'attachment' => __( 'Attachments (WP Media Library)', 'pods' ),
-                        'plupload' => __( 'Plupload', 'pods' )
-                    )
-                ),
-                'dependency' => true
-            ),
-            self::$type . '_attachment_tab' => array(
-                'label' => __( 'Attachments Default Tab', 'pods' ),
-                'depends-on' => array( self::$type . '_uploader' => 'attachment' ),
-                'default' => 'upload',
-                'type' => 'pick',
-                'data' => array(
-                    // keys MUST match WP's router names
-                    'upload' => __( 'Upload File', 'pods' ),
-                    'browse' => __( 'Media Library', 'pods' )
-                )
-            ),
-            self::$type . '_restrict_filesize' => array(
-                'label' => __( 'Restrict File Size', 'pods' ),
-                'depends-on' => array( self::$type . '_uploader' => 'plupload' ),
-                'default' => '10MB',
-                'type' => 'text'
-            ),
-            self::$type . '_add_button' => array(
-                'label' => __( 'Add Button Text', 'pods' ),
-                'default' => __( 'Add File', 'pods' ),
-                'type' => 'text'
-            ),
-            self::$type . '_modal_title' => array(
-                'label' => __( 'Modal Title', 'pods' ),
-                'depends-on' => array( self::$type . '_uploader' => 'attachment' ),
-                'default' => __( 'Attach a file', 'pods' ),
-                'type' => 'text'
-            ),
-            self::$type . '_modal_add_button' => array(
-                'label' => __( 'Modal Add Button Text', 'pods' ),
-                'depends-on' => array( self::$type . '_uploader' => 'attachment' ),
-                'default' => __( 'Add File', 'pods' ),
-                'type' => 'text'
-            )
-        );
+		return $options;
 
-        if ( !pods_version_check( 'wp', '3.5' ) ) {
-            unset( $options[ self::$type . '_modal_title' ] );
-            unset( $options[ self::$type . '_modal_add_button' ] );
+	}
 
-            $options[ self::$type . '_attachment_tab' ][ 'default' ] = 'type';
-            $options[ self::$type . '_attachment_tab' ][ 'data' ] = array(
-                'type' => __( 'Upload File', 'pods' ),
-                'library' => __( 'Media Library', 'pods' )
-            );
-        }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function input( $name, $value = null, $options = null, $pod = null, $id = null ) {
 
-        return $options;
-    }
+		$options = (array) $options;
 
-    /**
-     * Define the current field's schema for DB table storage
-     *
-     * @param array $options
-     *
-     * @return array
-     * @since 2.0
-     */
-    public function schema ( $options = null ) {
-        $schema = false;
+		$options[ self::$type . '_type' ]              = 'images';
+		$options[ self::$type . '_field_template' ]    = 'rows';
+		$options[ self::$type . '_wp_gallery_output' ] = 0;
 
-        return $schema;
-    }
+		parent::input( $name, $value, $options, $pod, $id );
 
-    /**
-     * Change the way the value of the field is displayed with Pods::get
-     *
-     * @param mixed $value
-     * @param string $name
-     * @param array $options
-     * @param array $pod
-     * @param int $id
-     *
-     * @return mixed|null
-     * @since 2.0
-     */
-    public function display ( $value = null, $name = null, $options = null, $pod = null, $id = null ) {
-        return $value;
-    }
-
-    /**
-     * Customize output of the form field
-     *
-     * @param string $name
-     * @param mixed $value
-     * @param array $options
-     * @param array $pod
-     * @param int $id
-     *
-     * @since 2.0
-     */
-    public function input ( $name, $value = null, $options = null, $pod = null, $id = null ) {
-        $options = (array) $options;
-        $form_field_type = PodsForm::$field_type;
-
-        if ( !is_admin() ) {
-            include_once( ABSPATH . '/wp-admin/includes/template.php' );
-
-            if ( is_multisite() )
-                include_once( ABSPATH . '/wp-admin/includes/ms.php' );
-        }
-
-        if ( ( ( defined( 'PODS_DISABLE_FILE_UPLOAD' ) && true === PODS_DISABLE_FILE_UPLOAD )
-               || ( defined( 'PODS_UPLOAD_REQUIRE_LOGIN' ) && is_bool( PODS_UPLOAD_REQUIRE_LOGIN ) && true === PODS_UPLOAD_REQUIRE_LOGIN && !is_user_logged_in() )
-               || ( defined( 'PODS_UPLOAD_REQUIRE_LOGIN' ) && !is_bool( PODS_UPLOAD_REQUIRE_LOGIN ) && ( !is_user_logged_in() || !current_user_can( PODS_UPLOAD_REQUIRE_LOGIN ) ) ) )
-             && ( ( defined( 'PODS_DISABLE_FILE_BROWSER' ) && true === PODS_DISABLE_FILE_BROWSER )
-                  || ( defined( 'PODS_FILES_REQUIRE_LOGIN' ) && is_bool( PODS_FILES_REQUIRE_LOGIN ) && true === PODS_FILES_REQUIRE_LOGIN && !is_user_logged_in() )
-                  || ( defined( 'PODS_FILES_REQUIRE_LOGIN' ) && !is_bool( PODS_FILES_REQUIRE_LOGIN ) && ( !is_user_logged_in() || !current_user_can( PODS_FILES_REQUIRE_LOGIN ) ) ) )
-        ) {
-            ?>
-        <p>You do not have access to upload / browse files. Contact your website admin to resolve.</p>
-        <?php
-            return;
-        }
-
-        if ( 'plupload' == pods_v( self::$type . '_uploader', $options ) )
-            $field_type = 'plupload';
-        elseif ( 'attachment' == pods_v( self::$type . '_uploader', $options ) ) {
-            if ( !pods_version_check( 'wp', '3.5' ) || !is_admin() ) // @todo test frontend media modal
-                $field_type = 'attachment';
-            else
-                $field_type = 'media';
-        }
-        else {
-            // Support custom File Uploader integration
-            do_action( 'pods_form_ui_field_avatar_uploader_' . pods_v( self::$type . '_uploader', $options ), $name, $value, $options, $pod, $id );
-            do_action( 'pods_form_ui_field_avatar_uploader', pods_v( self::$type . '_uploader', $options ), $name, $value, $options, $pod, $id );
-            return;
-        }
-
-        pods_view( PODS_DIR . 'ui/fields/' . $field_type . '.php', compact( array_keys( get_defined_vars() ) ) );
-    }
-
-    /**
-     * Build regex necessary for JS validation
-     *
-     * @param mixed $value
-     * @param string $name
-     * @param array $options
-     * @param string $pod
-     * @param int $id
-     *
-     * @return bool
-     * @since 2.0
-     */
-    public function regex ( $value = null, $name = null, $options = null, $pod = null, $id = null ) {
-        return false;
-    }
-
-    /**
-     * Validate a value before it's saved
-     *
-     * @param mixed $value
-     * @param string $name
-     * @param array $options
-     * @param array $fields
-     * @param array $pod
-     * @param int $id
-     * @param null $params
-     *
-     * @return bool
-     * @since 2.0
-     */
-    public function validate ( $value, $name = null, $options = null, $fields = null, $pod = null, $id = null, $params = null ) {
-        // check file size
-        // check file extensions
-        return true;
-    }
-
-    /**
-     * Change the value or perform actions after validation but before saving to the DB
-     *
-     * @param mixed $value
-     * @param int $id
-     * @param string $name
-     * @param array $options
-     * @param array $fields
-     * @param array $pod
-     * @param object $params
-     *
-     * @return mixed
-     * @since 2.0
-     */
-    public function pre_save ( $value, $id = null, $name = null, $options = null, $fields = null, $pod = null, $params = null ) {
-        return $value;
-    }
-
-    /**
-     * Customize the Pods UI manage table column output
-     *
-     * @param int $id
-     * @param mixed $value
-     * @param string $name
-     * @param array $options
-     * @param array $fields
-     * @param array $pod
-     *
-     * @return mixed|void
-     * @since 2.0
-     */
-    public function ui ( $id, $value, $name = null, $options = null, $fields = null, $pod = null ) {
-        if ( empty( $value ) )
-            return;
-
-        if ( !empty( $value ) && isset( $value[ 'ID' ] ) )
-            $value = array( $value );
-
-        foreach ( $value as $v ) {
-            echo pods_image( $v, 'thumbnail' );
-        }
-    }
+	}
 
     /**
      * Take over the avatar served from WordPress

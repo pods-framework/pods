5883d20d831d350228be8af8ac3deffcd73aa337
Scott Kingsley Clark <lol@scottkclark.com>
2017-04-06 00:35:21 -0500

    Clean up field class formatting

--- classes/fields/avatar.php
+++ classes/fields/avatar.php
@@ -6,52 +6,49 @@ require_once( PODS_DIR . 'classes/fields/file.php' );
  */
 class PodsField_Avatar extends PodsField_File {
 
-    /**
-     * Field Type Group
-     *
-     * @var string
-     * @since 2.0
-     */
-    public static $group = 'Relationships / Media';
+	/**
+	 * Field Type Group
+	 *
+	 * @var string
+	 * @since 2.0
+	 */
+	public static $group = 'Relationships / Media';
 
-    /**
-     * Field Type Identifier
-     *
-     * @var string
-     * @since 2.0
-     */
-    public static $type = 'avatar';
+	/**
+	 * Field Type Identifier
+	 *
+	 * @var string
+	 * @since 2.0
+	 */
+	public static $type = 'avatar';
 
-    /**
-     * Field Type Label
-     *
-     * @var string
-     * @since 2.0
-     */
-    public static $label = 'Avatar';
+	/**
+	 * Field Type Label
+	 *
+	 * @var string
+	 * @since 2.0
+	 */
+	public static $label = 'Avatar';
 
-    /**
-     * Pod Types supported on (true for all, false for none, or give array of specific types supported)
-     *
-     * @var array|bool
-     * @since 2.1
-     */
-    public static $pod_types = array( 'user' );
+	/**
+	 * Pod Types supported on (true for all, false for none, or give array of specific types supported)
+	 *
+	 * @var array|bool
+	 * @since 2.1
+	 */
+	public static $pod_types = array( 'user' );
 
-    /**
+	/**
 	 * {@inheritdoc}
-     */
-    public function __construct () {
-        self::$label = __( 'Avatar', 'pods' );
-    }
+	 */
+	public function __construct() {
 
-    /**
-     * Add options and set defaults to
-     *
-     * @param array $options
-     *
-     * @since 2.0
-     */
+		self::$label = __( 'Avatar', 'pods' );
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
 	public function options() {
 
 		$options = parent::options();
@@ -84,91 +81,98 @@ class PodsField_Avatar extends PodsField_File {
 
 	}
 
-    /**
-     * Take over the avatar served from WordPress
-     *
-     * @param string $avatar Default Avatar Image output from WordPress
-     * @param int|string|object $id_or_email A user ID, email address, or comment object
-     * @param int $size Size of the avatar image
-     * @param string $default URL to a default image to use if no avatar is available
-     * @param string $alt Alternate text to use in image tag. Defaults to blank
-     * @return string <img> tag for the user's avatar
-     */
-    public function get_avatar ( $avatar, $id_or_email, $size, $default = '', $alt ='' ) {
-        // Don't replace for the Avatars section of the Discussion settings page
-        if ( is_admin() ) {
-            $current_screen = get_current_screen();
-            if ( ! is_null( $current_screen ) && 'options-discussion' == $current_screen->id && 32 == $size ) {
-                return $avatar;
-            }
-        }
+	/**
+	 * Take over the avatar served from WordPress
+	 *
+	 * @param string            $avatar      Default Avatar Image output from WordPress.
+	 * @param int|string|object $id_or_email A user ID, email address, or comment object.
+	 * @param int               $size        Size of the avatar image.
+	 * @param string            $default     URL to a default image to use if no avatar is available.
+	 * @param string            $alt         Alternate text to use in image tag. Defaults to blank.
+	 *
+	 * @return string <img> tag for the user's avatar
+	 */
+	public function get_avatar( $avatar, $id_or_email, $size, $default = '', $alt = '' ) {
 
-        $_user_ID = 0;
+		// Don't replace for the Avatars section of the Discussion settings page.
+		if ( is_admin() ) {
+			$current_screen = get_current_screen();
 
-        if ( is_numeric( $id_or_email ) && 0 < $id_or_email )
-            $_user_ID = (int) $id_or_email;
-        elseif ( is_object( $id_or_email ) && isset( $id_or_email->user_id ) && 0 < $id_or_email->user_id )
-            $_user_ID = (int) $id_or_email->user_id;
-        elseif ( is_object( $id_or_email ) && isset( $id_or_email->ID ) && isset( $id_or_email->user_login ) && 0 < $id_or_email->ID )
-            $_user_ID = (int) $id_or_email->ID;
-        elseif ( !is_object( $id_or_email ) && false !== strpos( $id_or_email, '@' ) ) {
-            $_user = get_user_by( 'email', $id_or_email );
+			if ( ! is_null( $current_screen ) && 'options-discussion' === $current_screen->id && 32 === $size ) {
+				return $avatar;
+			}
+		}
 
-            if ( !empty( $_user ) )
-                $_user_ID = (int) $_user->ID;
-        }
+		$user_id = 0;
 
-		// Include PodsMeta if not already included
+		if ( is_numeric( $id_or_email ) && 0 < $id_or_email ) {
+			$user_id = (int) $id_or_email;
+		} elseif ( is_object( $id_or_email ) && isset( $id_or_email->user_id ) && 0 < $id_or_email->user_id ) {
+			$user_id = (int) $id_or_email->user_id;
+		} elseif ( is_object( $id_or_email ) && isset( $id_or_email->ID ) && isset( $id_or_email->user_login ) && 0 < $id_or_email->ID ) {
+			$user_id = (int) $id_or_email->ID;
+		} elseif ( ! is_object( $id_or_email ) && false !== strpos( $id_or_email, '@' ) ) {
+			$_user = get_user_by( 'email', $id_or_email );
+
+			if ( ! empty( $_user ) ) {
+				$user_id = (int) $_user->ID;
+			}
+		}
+
+		// Include PodsMeta if not already included.
 		pods_meta();
 
-        if ( 0 < $_user_ID && !empty( PodsMeta::$user ) ) {
-            $avatar_cached = pods_cache_get( $_user_ID . '-' . $size, 'pods_avatars' );
+		if ( 0 < $user_id && ! empty( PodsMeta::$user ) ) {
+			$avatar_cached = pods_cache_get( $user_id . '-' . $size, 'pods_avatars' );
 
-            if ( !empty( $avatar_cached ) )
-                $avatar = $avatar_cached;
-            else {
-                $avatar_field = pods_transient_get( 'pods_avatar_field' );
+			if ( ! empty( $avatar_cached ) ) {
+				$avatar = $avatar_cached;
+			} else {
+				$avatar_field = pods_transient_get( 'pods_avatar_field' );
 
-                $user = current( PodsMeta::$user );
+				$user = current( PodsMeta::$user );
 
-                if ( empty( $avatar_field ) ) {
-                    foreach ( $user[ 'fields' ] as $field ) {
-                        if ( 'avatar' == $field[ 'type' ] ) {
-                            $avatar_field = $field[ 'name' ];
+				if ( empty( $avatar_field ) ) {
+					foreach ( $user['fields'] as $field ) {
+						if ( 'avatar' === $field['type'] ) {
+							$avatar_field = $field['name'];
 
-                            pods_transient_set( 'pods_avatar_field', $avatar_field );
+							pods_transient_set( 'pods_avatar_field', $avatar_field );
 
-                            break;
-                        }
-                    }
-                }
-                elseif ( !isset( $user[ 'fields' ][ $avatar_field ] ) )
-                    $avatar_field = false;
+							break;
+						}
+					}
+				} elseif ( ! isset( $user['fields'][ $avatar_field ] ) ) {
+					$avatar_field = false;
+				}
 
-                if ( !empty( $avatar_field ) ) {
-                    $user_avatar = get_user_meta( $_user_ID, $avatar_field . '.ID', true );
+				if ( ! empty( $avatar_field ) ) {
+					$user_avatar = get_user_meta( $user_id, $avatar_field . '.ID', true );
 
-                    if ( !empty( $user_avatar ) ) {
-                        $attributes = array(
-                            'alt' => '',
-                            'class' => 'avatar avatar-' . $size . ' photo'
-                        );
+					if ( ! empty( $user_avatar ) ) {
+						$attributes = array(
+							'alt'   => '',
+							'class' => 'avatar avatar-' . $size . ' photo',
+						);
 
-                        if ( !empty( $alt ) )
-                            $attributes[ 'alt' ] = $alt;
+						if ( ! empty( $alt ) ) {
+							$attributes['alt'] = $alt;
+						}
 
-                        $user_avatar = pods_image( $user_avatar, array( $size, $size ), 0, $attributes );
+						$user_avatar = pods_image( $user_avatar, array( $size, $size ), 0, $attributes );
 
-                        if ( !empty( $user_avatar ) ) {
-                            $avatar = $user_avatar;
+						if ( ! empty( $user_avatar ) ) {
+							$avatar = $user_avatar;
 
-                            pods_cache_set( $_user_ID . '-' . $size, $avatar, 'pods_avatars' );
-                        }
-                    }
-                }
-            }
-        }
+							pods_cache_set( $user_id . '-' . $size, $avatar, 'pods_avatars' );
+						}
+					}
+				}
+			}
+		}
 
-        return $avatar;
-    }
+		return $avatar;
+
+	}
+
 }

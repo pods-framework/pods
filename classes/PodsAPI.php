bf38a9b01fd70fa674d9cf8c15d9528d2e62cc02
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-05-16 01:28:59 +0200

    Spaces as indentation to match surrounding code
    sigh...

--- classes/PodsAPI.php
+++ classes/PodsAPI.php
@@ -8322,9 +8322,9 @@ class PodsAPI {
         $id = $this->save_pod_item( $params );
 
         // Always return $id for AJAX requests.
-	    if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-		    return $id;
-	    }
+        if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+            return $id;
+        }
 
         if ( 0 < $id && ! empty( $thank_you ) ) {
             $thank_you = str_replace( 'X_ID_X', $id, $thank_you );

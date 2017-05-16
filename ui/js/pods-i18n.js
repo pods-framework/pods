93c39f43b92f8d426a54897a316cbf8e57fda350
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-05-16 07:47:39 +0200

    Support number formats (%d and %#$d) + JSdoc

--- ui/js/pods-i18n.js
+++ ui/js/pods-i18n.js
@@ -3,7 +3,9 @@
 var PodsI18n = (function () {
 
 	/**
-	 * Only visible to the closure, not exposed externally
+	 * Only visible to the closure, not exposed externally.
+	 * @param {string} str
+	 * @returns {string}
 	 */
 	var translateString = function ( str ) {
 		var translated = str, ref;
@@ -28,6 +30,53 @@ var PodsI18n = (function () {
 	};
 
 	/**
+	 * Supports %s and %d formats.
+	 * @param {string} str
+	 * @param {array} args
+	 * @returns {string}
+	 */
+	var sprintf = function ( str, args ) {
+		var i, s, d;
+		if ( 1 < args.length ) {
+			var multi_s = ( -1 !== str.indexOf( '%1$s' ) );
+			var multi_d = ( -1 !== str.indexOf( '%1$d' ) );
+			for ( i=0, s=1, d=1; i < args.length; i++ ) {
+				// Only support numbers other than strings
+				if ( 'string' !== typeof args[ i ] ) {
+					if ( multi_d ) {
+						str = str.replace( "%" + d + "$d", args[ i ] );
+						d++;
+					} else {
+						str = str.replace( "%d", args[ i ] );
+					}
+				} else {
+					if ( multi_s ) {
+						str = str.replace( "%" + s + "$s", args[ i ] );
+						s++;
+					} else {
+						str = str.replace( "%s", args[ i ] );
+					}
+				}
+			}
+		} else {
+			if ( -1 === str.indexOf( '%1$s' ) && -1 === str.indexOf( '%1$d' ) ) {
+				if ( 'string' !== typeof args[0] ) {
+					str = str.replace( "%d", args[0] );
+				} else {
+					str = str.replace( "%s", args[0] );
+				}
+			} else {
+				if ( 'string' !== typeof args[0] ) {
+					str = str.replace( "%1$d", args[0] );
+				} else {
+					str = str.replace( "%1$s", args[0] );
+				}
+			}
+		}
+		return str;
+	};
+
+	/**
 	 * The returned object, this is what we'll expose to the outside world
 	 */
 	return {
@@ -48,19 +97,7 @@ var PodsI18n = (function () {
 			if ( ! args.length ) {
 				return str;
 			}
-			var i, c;
-			if ( 1 < args.length ) {
-				for ( i=0, c=1; i < args.length; i++, c++ ) {
-					str = str.replace( "%" + c + "$s", args[ i ] );
-				}
-			} else {
-				if ( -1 === str.indexOf( '%1$s' ) ) {
-					str = str.replace( "%s", args[0] );
-				} else {
-					str = str.replace( "%1$s", args[0] );
-				}
-			}
-			return str;
+			return sprintf( str, args );
 		}
 	};
 

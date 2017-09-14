ad6c6d2761f74849b51a19f59a21e8c238bb1e32
Scott Kingsley Clark <lol@scottkclark.com>
2017-09-14 01:12:39 -0500

    Fix pod object notice in PodsUI (part 2)

--- classes/PodsUI.php
+++ classes/PodsUI.php
@@ -2303,7 +2303,7 @@ class PodsUI {
         if ( $params->full )
             $find_params[ 'limit' ] = -1;
 
-        $find_params = apply_filters('pods_ui_get_params', $find_params, ( is_object( $this->pod ) ? $this->pod->pod_data : null ), $this);
+        $find_params = apply_filters('pods_ui_get_params', $find_params, ( is_object( $this->pod ) ? $this->pod->pod : null ), $this);
 
 		/**
 		 * Filter Pods::find() parameters to make it more easily extended by plugins and developers.

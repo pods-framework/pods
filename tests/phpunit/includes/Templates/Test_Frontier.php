62167642634285a800098091208d6eb8e24bd207
James Golovich <james@gnuinter.net>
2017-10-07 13:20:31 -0700

    Add a test to prove that the issue isn't exactly associated with templates
    This is a generic issue that happens with post types that aren't associated with Pods

--- tests/phpunit/includes/Templates/Test_Frontier.php
+++ tests/phpunit/includes/Templates/Test_Frontier.php
@@ -19,4 +19,24 @@ class Frontier extends \Pods_Unit_Tests\Pods_UnitTestCase {
 		$id = wp_insert_post( $post );
 		$this->assertGreaterThan( 0, $id );
 	}
+
+	/**
+	 * This test isn't specifically about the templates, but it's connected with the issue.
+	 * Create an arbitrary post type and insert a post to check to make sure save_meta isn't failing
+	 * @group pods-issue-4500
+	 */
+	public function test_create_post_type() {
+		$post_type = 'new_post_type';
+		$args = array();
+		register_post_type( $post_type, $args );
+
+		$post = array(
+			'post_type' => $post_type,
+			'post_status' => 'publish',
+			'post_content' => 'All of the true things I am about to tell you are shameless lies.'
+		);
+		$id = wp_insert_post( $post );
+		$this->assertGreaterThan( 0, $id );
+	}
+
 }

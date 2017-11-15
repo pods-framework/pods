b3b86bfdbde3fcff6b33853f9090d0da2dcf1919
Scott Kingsley Clark <lol@scottkclark.com>
2017-11-15 16:52:42 -0600

    PhpStorm format tweaks for ui php files

--- ui/fields/currency.php
+++ ui/fields/currency.php
@@ -3,52 +3,57 @@ $field_number = PodsForm::field_loader( 'currency' );
 
 $value = $field_number->format( $value, $name, $options, $pod, $id );
 
-$attributes = array();
-$attributes[ 'type' ] = 'text';
-$attributes[ 'value' ] = $value;
-$attributes[ 'tabindex' ] = 2;
-$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
+$attributes             = array();
+$attributes['type']     = 'text';
+$attributes['value']    = $value;
+$attributes['tabindex'] = 2;
+$attributes             = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
 
 global $wp_locale;
 
 if ( '9999.99' == pods_var( 'currency_format', $options ) ) {
-    $thousands = '';
-    $dot = '.';
-}
-elseif ( '9999,99' == pods_var( 'currency_format', $options ) ) {
-    $thousands = '';
-    $dot = ',';
-}
-elseif ( '9.999,99' == pods_var( 'currency_format', $options ) ) {
-    $thousands = '.';
-    $dot = ',';
-}
-else {
-    $thousands = $wp_locale->number_format[ 'thousands_sep' ];
-    $dot = $wp_locale->number_format[ 'decimal_point' ];
+	$thousands = '';
+	$dot       = '.';
+} elseif ( '9999,99' == pods_var( 'currency_format', $options ) ) {
+	$thousands = '';
+	$dot       = ',';
+} elseif ( '9.999,99' == pods_var( 'currency_format', $options ) ) {
+	$thousands = '.';
+	$dot       = ',';
+} else {
+	$thousands = $wp_locale->number_format['thousands_sep'];
+	$dot       = $wp_locale->number_format['decimal_point'];
 }
 
 $currency = 'usd';
 
-if ( isset( PodsField_Currency::$currencies[ pods_var( 'currency_format_sign', $options, -1 ) ] ) )
-    $currency = pods_var( 'currency_format_sign', $options );
+if ( isset( PodsField_Currency::$currencies[ pods_var( 'currency_format_sign', $options, - 1 ) ] ) ) {
+	$currency = pods_var( 'currency_format_sign', $options );
+}
 
 $currency_sign = PodsField_Currency::$currencies[ $currency ]['sign'];
 ?>
 <input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?>/>
 <script>
-    jQuery( function ( $ ) {
-        $( 'input#<?php echo esc_js( $attributes[ 'id' ] ); ?>' ).on( 'blur', function () {
-            if ( !/^[0-9\<?php
-            echo esc_js( implode( '\\', array_filter( array( $dot, $thousands ) ) ) );
-            ?>]$/.test( $( this ).val() ) ) {
-                var newval = $( this )
-                    .val()
-                    .replace( /[^0-9-\\<?php echo esc_js( $currency_sign ); ?>\<?php
-                              echo esc_js( implode( '\\', array_filter( array( $dot, $thousands ) ) ) );
-                              ?>]/g, '' );
-                $( this ).val( newval );
-            }
-        } );
-    } );
+	jQuery( function ( $ ) {
+		$( 'input#<?php echo esc_js( $attributes['id'] ); ?>' ).on( 'blur', function () {
+			if ( !/^[0 - 9\<?php
+			echo esc_js( implode( '\\', array_filter( array( $dot, $thousands ) ) ) );
+			?>]
+			$ /
+		.
+			test( $( this ).val() )
+		)
+			{
+				var newval = $( this )
+								 .val()
+								 .replace( / [ ^ 0 - 9 -\\<?php echo esc_js( $currency_sign ); ?>\<?php
+				echo esc_js( implode( '\\', array_filter( array( $dot, $thousands ) ) ) );
+				?>]/
+				g, ''
+			)
+				;$( this ).val( newval );
+			}
+		} );
+	} );
 </script>
--- ui/fields/website.php
+++ ui/fields/website.php
@@ -3,14 +3,15 @@ $attributes = array();
 
 $type = 'text';
 
-if ( 1 == pods_var( 'website_html5', $options ) )
-    $type = 'url';
+if ( 1 == pods_var( 'website_html5', $options ) ) {
+	$type = 'url';
+}
 
-$attributes[ 'type' ] = $type;
-$attributes[ 'value' ] = $value;
-$attributes[ 'tabindex' ] = 2;
-$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
+$attributes['type']     = $type;
+$attributes['value']    = $value;
+$attributes['tabindex'] = 2;
+$attributes             = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
 ?>
-<input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
+	<input<?php PodsForm::attributes( $attributes, $name, $form_field_type, $options ); ?> />
 <?php
 PodsForm::regex( $form_field_type, $options );

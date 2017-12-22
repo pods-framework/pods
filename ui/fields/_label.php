72568fd7a5642d3276ffed43a5eea5555697b5c4
David Hernando Davalillo <davidhernando@Davids-MacBook-Pro.local>
2017-12-22 17:17:28 +0100

    check for the localized string 'help' before inserting help tooltip

--- ui/fields/_label.php
+++ ui/fields/_label.php
@@ -5,7 +5,7 @@
     if ( 1 == pods_var( 'required', $options, pods_var( 'options', $options, $options ) ) )
         echo ' <abbr title="required" class="required">*</abbr>';
 
-    if ( 0 == pods_var( 'grouped', $options, 0, null, true ) && !empty( $help ) && 'help' != $help )
+    if ( 0 == pods_var( 'grouped', $options, 0, null, true ) && !empty( $help ) && __( 'help', 'pods' ) != $help )
         pods_help( $help );
     ?>
 </label>

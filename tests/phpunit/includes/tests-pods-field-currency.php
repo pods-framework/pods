c448cea9eb3e739e409276c5be7b9cb7ef9abded
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-04-11 10:09:34 +0200

    Test different format (comma for decimals)

--- tests/phpunit/includes/tests-pods-field-currency.php
+++ tests/phpunit/includes/tests-pods-field-currency.php
@@ -62,6 +62,33 @@ class Test_PodsField_Currency extends Pods_UnitTestCase {
 	}
 
 	/**
+	 * @dataProvider formatDefaultsDecimalCommaProvider
+	 */
+	public function test_format_defaults_decimal_comma( $value, $expected ) {
+		$options = $this->defaultOptions;
+		$options[ 'currency_format' ] = '9.999,99';
+
+		$this->assertEquals( $expected, $this->field->format( $value, null, $options ) );
+	}
+
+	public function formatDefaultsDecimalCommaProvider() {
+
+		return array(
+			array( "-1", "-1,00" ),
+			array( "0", "0,00" ),
+			array( "1", "1,00" ),
+			array( "1.", "1,00" ),
+			array( "1.5", "1,50" ),
+			array( "10", "10,00" ),
+			array( "10.01", "10,01" ),
+			array( "100", "100,00" ),
+			array( "1000", "1.000,00" ),
+			array( "10000", "10.000,00" ),
+			array( "1000000", "1.000.000,00" ),
+		);
+	}
+
+	/**
 	 * @dataProvider formatDecimalDashProvider
 	 */
 	public function test_format_decimal_dash( $value, $expected ) {

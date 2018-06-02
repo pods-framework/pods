aa1e6d89c88b7240ba0593f22f5a28b18f3e2437
pglewis <pglewis13@gmail.com>
2018-06-02 00:16:33 -0400

    Wordpress will already call `esc_textarea()` if rich edit is off, so don't double-escape when the visual editor is disabled

--- components/Templates/includes/element-view_template.php
+++ components/Templates/includes/element-view_template.php
@@ -6,7 +6,12 @@
 <div class="pods-compat-container">
 	<textarea id="content" name="content">
 		<?php if ( isset( $content ) ) {
-			echo esc_textarea( $content );
+			// WordPress will already call esc_textarea() if richedit is off, don't escape twice (see #3462)
+			if ( ! user_can_richedit() ) {
+				echo $content;
+			} else {
+				echo esc_textarea( $content );
+			}
 		} ?>
 	</textarea>
 </div>

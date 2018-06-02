88d65a2d3c6a9501a006e5002abf17224e010ed2
pglewis <pglewis13@gmail.com>
2018-06-02 02:23:24 -0400

    phpcs fixes

--- components/Templates/includes/element-view_template.php
+++ components/Templates/includes/element-view_template.php
@@ -1,17 +1,22 @@
 <?php
 /**
  * Frontier Template code editor metabox
+ *
+ * @package Pods_templates
  */
+
 ?>
 <div class="pods-compat-container">
 	<textarea id="content" name="content">
-		<?php if ( isset( $content ) ) {
+		<?php
+		if ( isset( $content ) ) {
 			// WordPress will already call esc_textarea() if richedit is off, don't escape twice (see #3462)
 			if ( ! user_can_richedit() ) {
-				echo $content;
+				echo $content; // phpcs:ignore
 			} else {
 				echo esc_textarea( $content );
 			}
-		} ?>
+		}
+		?>
 	</textarea>
 </div>

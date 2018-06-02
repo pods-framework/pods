a54284583e96bbdc8c95c14d2cdbe587c083b378
pglewis <pglewis13@gmail.com>
2018-06-02 14:17:03 -0400

    Namespace the phpcs reports and add them to .gitignore

--- bin/phpcs-audit.sh
+++ bin/phpcs-audit.sh
@@ -24,5 +24,5 @@ echo Running PHPCS reports
 for type in ${types[@]}
 do
   echo Running PHPCS report: ${type}
-  ./vendor/bin/phpcs -v --report="${type}" --report-file="report-${type}${file_affix}.txt" ${file_path}
+  ./vendor/bin/phpcs -v --report="${type}" --report-file="phpcs-report-${type}${file_affix}.txt" ${file_path}
 done;

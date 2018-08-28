344db728ec784f873bf40d583ece784140b80fe9
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-08-28 14:47:34 +0200

    Also trigger change on .pods-form-front

--- ui/js/jquery.pods.js
+++ ui/js/jquery.pods.js
@@ -1261,7 +1261,7 @@
                 $( '.pods-dependency .pods-depends-on, .pods-dependency .pods-excludes-on, .pods-dependency .pods-wildcard-on' ).hide();
 
                 // Handle dependent toggle
-                $( '.pods-admin' ).on( 'change', '.pods-dependent-toggle[data-name-clean]', function ( e ) {
+                $( '.pods-admin, .pods-form-front' ).on( 'change', '.pods-dependent-toggle[data-name-clean]', function ( e ) {
                     var selectionTypeRegex = /pick-format-type$/g;
                     var elementId = $( this ).attr( 'id' );
                     var selectionType, selectionFormatId;

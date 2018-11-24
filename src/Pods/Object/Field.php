6215817469f5c0cf552665758233a84dde23c3fb
Scott Kingsley Clark <lol@scottkclark.com>
2018-11-24 13:00:04 -0600

    Add get_table_info logic for relationship fields

--- src/Pods/Object/Field.php
+++ src/Pods/Object/Field.php
@@ -16,16 +16,29 @@ class Pods_Object_Field extends Pods_Object {
 	 * {@inheritdoc}
 	 */
 	public function get_table_info() {
-		return array();
-
-		// @todo Handle logic to get table info for related object.
 		if ( array() === $this->_table_info ) {
 			return array();
 		}
 
+		$type = $this->get_type();
+
+		$simple_tableless_objects = PodsForm::simple_tableless_objects();
+
+		$related_type = $this->get_arg( $type . '_object' );
+
+		if ( empty( $related_type ) || in_array( $related_type, $simple_tableless_objects, true ) ) {
+			return array();
+		}
+
+		$related_name = $this->get_arg( $type . '_val' );
+
+		if ( '__current__' === $related_name ) {
+			$related_name = $this->get_name();
+		}
+
 		$api = pods_api();
 
-		$table_info = $api->get_table_info( $this->get_type(), $this->get_name() );
+		$table_info = $api->get_table_info( $related_type, $related_name );
 
 		if ( ! $table_info ) {
 			$table_info = array();

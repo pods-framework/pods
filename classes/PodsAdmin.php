91afe8856ed339e597e877a3afe75b2c67a6f7ac
Benjamin Bradley <benjamin@wpstudio.com>
2018-12-05 16:25:08 -0500

    Added new post_type labels for WP 5.0
    Added new post_type labels for WP5.0 - item_published, item_published_privately, item_reverted_to_draft, item_scheduled, item_updated.

--- classes/PodsAdmin.php
+++ classes/PodsAdmin.php
@@ -1516,6 +1516,41 @@ class PodsAdmin {
 					'default'     => '',
 					'object_type' => array( 'taxonomy' ),
 				),
+				'label_item_published'		=> array(
+					'label'		=> __('<span class="pods-slugged" data-sluggable="label_singular">Item</span> Published.', 'pods' ),
+					'help'		=> __( 'help', 'pods' ),
+					'type' 		=> 'text',
+					'default'	=> '',
+					'object_type'	=> array('post_type'),
+				),
+				'label_item_published_privately'	=> array(
+					'label'		=> __('<span class="pods-slugged" data-sluggable="label_singular">Item</span> published privately.', 'pods' ),
+					'help'		=> __('help', 'pods' ),
+					'type'		=> 'text',
+					'default'	=> '',
+					'object_type'	=> array('post_type'),
+				),
+				'label_item_reverted_to_draft'		=> array(
+					'label'		=> __('<span class="pods-slugged" data-sluggable="label_singular">Item</span> reverted to draft.', 'pods' ),
+					'help'		=> __('help', 'pods'),
+					'type'		=> 'text',
+					'default'	=> ''
+					'object_type'	=> array('post_type'),
+				),
+				'label_item_scheduled' 			=> array(
+					'label'		=> __('<span class="pods-slugged" data-sluggable="label_singular">Item</span> scheduled.', 'pods' ),
+					'help'		=> __('help', 'pods'),
+					'type'		=> 'text',
+					'default'	=> '',
+					'object_type'	=> array('post_type'),
+				),
+				'label_item_updated'			=> array(
+					'label'		=> __('<span class="pods-slugged" data-sluggable="label_singular">Item</span> updated.', 'pods' ),
+					'help' 		=> __('help', 'pods'),
+					'type'		=> 'text',
+					'default'	=> '',
+					'object_type'	=> array('post_type'),
+				),
 			);
 
 			$options['labels'] = array();

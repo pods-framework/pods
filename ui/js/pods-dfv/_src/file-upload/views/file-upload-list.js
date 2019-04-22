26cd7dc3b2c1c88be59609d75d08a39500e31bae
pglewis <pglewis13@gmail.com>
2019-04-22 13:34:19 -0400

    Use `Marionette.noConflict()` to keep a private copy of Marionette

--- ui/js/pods-dfv/_src/file-upload/views/file-upload-list.js
+++ ui/js/pods-dfv/_src/file-upload/views/file-upload-list.js
@@ -1,4 +1,4 @@
-/*global jQuery, _, Backbone, Marionette, wp */
+/*global jQuery, _, Backbone, PodsMn, wp */
 import template from 'pods-dfv/_src/file-upload/views/file-upload-item.html';
 
 import { PodsFieldListView, PodsFieldView } from 'pods-dfv/_src/core/pods-field-views';

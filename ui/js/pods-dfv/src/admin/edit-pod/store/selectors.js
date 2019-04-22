fe6da52c3b8db820b82edb02db0262182cf57178
pglewis <pglewis13@gmail.com>
2019-04-22 16:49:50 -0400

    Pod edit, selectors: remove defaulting sub objects to an empty object for now

--- ui/js/pods-dfv/src/admin/edit-pod/store/selectors.js
+++ ui/js/pods-dfv/src/admin/edit-pod/store/selectors.js
@@ -5,21 +5,17 @@ export const getState = state => state;
 
 // UI
 export const getActiveTab = state => {
-	state.ui = state.ui || {};
 	return state.ui.activeTab;
 };
 export const getSaveStatus = state => {
-	state.ui = state.ui || {};
 	return state.ui.saveStatus;
 };
 export const isSaving = state => {
-	state.ui = state.ui || {};
 	return ( state.ui.saveStatus === uiConstants.saveStatuses.SAVING );
 };
 
 // Pod meta
 export const getPodName = state => {
-	state.podMeta = state.podMeta || {};
 	return state.podMeta.name;
 };
 export const getPodMetaValue = ( state, key ) => {
@@ -33,8 +29,6 @@ export const getFields = state => state.fields;
 export const getLabels = state => state.labels;
 
 export const getLabelValue = ( state, labelName ) => {
-	state.labels = state.labels || {};
-
 	for ( let i = 0; i < state.labels.length; i++ ) {
 		if ( state.labels[ i ].name === labelName ) {
 			return state.labels[ i ].value;

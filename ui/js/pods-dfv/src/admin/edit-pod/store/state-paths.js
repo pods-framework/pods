62d0098481b6fb8fc25313dcba09c2ce01d05a15
pglewis <pglewis13@gmail.com>
2019-05-01 04:24:58 -0400

    State paths: rearrange and document some of the key structures

--- ui/js/pods-dfv/src/admin/edit-pod/store/state-paths.js
+++ ui/js/pods-dfv/src/admin/edit-pod/store/state-paths.js
@@ -14,20 +14,31 @@ export const createStatePath = path => {
 	};
 };
 
-export const UI = createStatePath( 'ui' );
 export const POD_META = createStatePath( 'podMeta' );
-export const OPTIONS = createStatePath( 'options' );
-export const FIELDS = createStatePath( 'fields' );
-
 export const POD_NAME = createStatePath( `${POD_META.path}.name` );
 export const POD_ID = createStatePath( `${POD_META.path}.id` );
 
+export const FIELDS = createStatePath( 'fields' );
+
+export const UI = createStatePath( 'ui' );
 export const ACTIVE_TAB = createStatePath( `${UI.path}.activeTab` );
 export const SAVE_STATUS = createStatePath( `${UI.path}.saveStatus` );
-
 export const TABS = createStatePath( `${UI.path}.tabs` );
+
+// Ordered list of tab names as an array: [ 'tab1name', 'tab2name', ... ]
 export const TABS_LIST = createStatePath( `${TABS.path}.orderedList` );
+
+// Tab objects keyed by tab name:
+// { tab1name: {tab object}, tab2name: {tab object}, ...}
 export const TABS_BY_NAME = createStatePath( `${TABS.path}.byName` );
 
-// Loose strings
+// Pod option list keyed by option name:
+// { can_export: {object}, show_ui: {object}, etc }
+export const OPTIONS = createStatePath( 'options' );
+
+// Ordered list of option names for this tab as an array:
+// [ 'option1name', 'options2name', ...]
+// Stored in the tab objects in TABS_BY_NAME
 export const TAB_OPTIONS_LIST = 'optionList';
+
+

ee57baa19f2fc9c7ff1222e45fc887119547198f
pglewis <pglewis13@gmail.com>
2019-05-02 01:40:40 -0400

    Edit Pod: re-organize selectors

--- ui/js/pods-dfv/src/admin/edit-pod/store/selectors.js
+++ ui/js/pods-dfv/src/admin/edit-pod/store/selectors.js
@@ -4,20 +4,16 @@ import { uiConstants } from './constants';
 // Everything
 export const getState = state => state;
 
-/**
- * UI
- */
+//-- Pod meta
+export const getPodName = state => {
+	return paths.POD_NAME.getFrom( state );
+};
 
-//-- Tabs and options
-export const getOption = ( state, optionName ) =>
-	paths.OPTIONS.getFrom( state )[ optionName ];
+export const getPodMetaValue = ( state, key ) => {
+	return paths.POD_META.getFrom( state )[ key ];
+};
 
-export const getOptionItemValue = ( state, optionName, itemName ) =>
-	paths.OPTIONS.getFrom( state )[ optionName ][ itemName ];
-
-export const getOptionValue = ( state, optionName ) =>
-	paths.OPTIONS.getFrom( state )[ optionName ].value;
-
+//-- Tabs
 export const getActiveTab = state => paths.ACTIVE_TAB.getFrom( state );
 
 export const getTab = ( state, tabName ) =>
@@ -44,18 +40,19 @@ export const getSaveStatus = state => paths.SAVE_STATUS.getFrom( state );
 export const isSaving = state =>
 	paths.SAVE_STATUS.getFrom( state ) === uiConstants.saveStatuses.SAVING;
 
-/**
- * Pod meta
- */
-export const getPodName = state => {
-	return paths.POD_NAME.getFrom( state );
-};
+//-- Options
+export const getOption = ( state, optionName ) =>
+	paths.OPTIONS.getFrom( state )[ optionName ];
 
-export const getPodMetaValue = ( state, key ) => {
-	return paths.POD_META.getFrom( state )[ key ];
-};
+export const getOptionItemValue = ( state, optionName, itemName ) =>
+	paths.OPTIONS.getFrom( state )[ optionName ][ itemName ];
 
-/**
- * Fields
- */
+export const getOptionValue = ( state, optionName ) =>
+	paths.OPTIONS.getFrom( state )[ optionName ].value;
+
+//-- Groups
+export const getGroup = ( state, groupName ) =>
+	paths.GROUPS.getFrom( state )[ groupName ];
+
+//-- Fields
 export const getFields = state => paths.FIELDS.getFrom( state );

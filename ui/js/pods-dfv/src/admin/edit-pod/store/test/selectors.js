91d3a07bad929f7836b330d1d21f1a8b47e3b8b3
pglewis <pglewis13@gmail.com>
2019-05-16 15:40:45 -0400

    Extract ui selector tests to their own test file

--- ui/js/pods-dfv/src/admin/edit-pod/store/test/selectors.js
+++ ui/js/pods-dfv/src/admin/edit-pod/store/test/selectors.js
@@ -1,20 +1,11 @@
 import deepFreeze from 'deep-freeze';
-import { merge } from 'lodash';
 
 import * as paths from '../state-paths';
-import { uiConstants } from '../constants';
 import {
 	getState,
 	getOption,
 	getOptionItemValue,
 	getOptionValue,
-	getActiveTab,
-	getTabList,
-	getTab,
-	getTabs,
-	getTabOptions,
-	getSaveStatus,
-	isSaving,
 	getPodName,
 	getPodMetaValue,
 	getFields,
@@ -39,141 +30,6 @@ describe( 'selectors', () => {
 
 			expect( result ).toBeDefined();
 			expect( result ).toEqual( state );
-		} );
-	} );
-
-	describe( 'ui', () => {
-		describe( 'tabs', () => {
-			describe( 'getActiveTab()', () => {
-				const { tabNames } = uiConstants;
-
-				it( 'Should return the active tab', () => {
-					const state = deepFreeze(
-						paths.ACTIVE_TAB.createTree( tabNames.LABELS )
-					);
-					const result = getActiveTab( state );
-					const expected = paths.ACTIVE_TAB.getFrom( state );
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( expected );
-				} );
-			} );
-
-			describe( 'getTabList()', () => {
-				it( 'Should return the ordered tab list', () => {
-					const orderedList = [ 'foo', 'bar', 'baz' ];
-					const state = deepFreeze(
-						paths.TAB_LIST.createTree( orderedList )
-					);
-					const result = getTabList( state );
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( orderedList );
-				} );
-			} );
-
-			// These are being reused below
-			const testTabs = {
-				foo: {
-					name: 'foo',
-					[paths.TAB_OPTION_LIST]: [ 'foo-option2', 'foo-option1' ]
-				},
-				bar: {
-					name: 'bar',
-					[paths.TAB_OPTION_LIST]: [ 'bar-option2', 'bar-option1' ]
-				}
-			};
-			describe( 'getTab()', () => {
-				const state = deepFreeze(
-					paths.TABS_BY_NAME.createTree( testTabs )
-				);
-
-				it( 'Should return the specified tab', () => {
-					const result = getTab( state, 'bar' );
-					const expected = testTabs.bar;
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( expected );
-				} );
-
-				it( 'Should return undefined for a non-existent tab name', () => {
-					const result = getTab( state, 'not a tab' );
-					expect( result ).toBeUndefined();
-				} );
-			} );
-
-			describe( 'getTabs()', () => {
-				it( 'Should return the ordered tabs', () => {
-					const state = deepFreeze( merge(
-						paths.TABS_BY_NAME.createTree( testTabs ),
-						paths.TAB_LIST.createTree( [ 'bar', 'foo' ] )
-					) );
-					const result = getTabs( state );
-					const expected = [ testTabs.bar, testTabs.foo ];
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( expected );
-				} );
-			} );
-
-			describe( 'getTabOptions()', () => {
-				it( 'Should get the ordered options for the specified tab', () => {
-					const options = {
-						'foo-option1': { name: 'foo-option1', value: 'foo1 value' },
-						'foo-option2': { name: 'foo-option2', value: 'foo2 value' },
-						'bar-option1': { name: 'bar-option1', value: 'bar1 value' },
-						'bar-option2': { name: 'bar-option2', value: 'bar2 value' },
-					};
-					const state = deepFreeze( merge(
-						paths.TABS_BY_NAME.createTree( testTabs ),
-						paths.OPTIONS.createTree( options )
-					) );
-
-					const result = getTabOptions( state, 'bar' );
-					const expected = [
-						{ name: 'bar-option2', value: 'bar2 value' },
-						{ name: 'bar-option1', value: 'bar1 value' },
-					];
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( expected );
-				} );
-			} );
-		} );
-
-		describe( 'save status', () => {
-			describe( 'getSaveStatus()', () => {
-				const { saveStatuses } = uiConstants;
-
-				it( 'Should return the save status', () => {
-					const saveStatus = saveStatuses.SAVE_SUCCESS;
-					const state = deepFreeze(
-						paths.SAVE_STATUS.createTree( saveStatus )
-					);
-					const result = getSaveStatus( state );
-
-					expect( result ).toBeDefined();
-					expect( result ).toEqual( saveStatus );
-				} );
-			} );
-
-			describe( 'isSaving()', () => {
-				const { saveStatuses } = uiConstants;
-
-				it( 'Should return true when saving', () => {
-					const state = deepFreeze(
-						paths.SAVE_STATUS.createTree( saveStatuses.SAVING )
-					);
-					expect( isSaving( state ) ).toBe( true );
-				} );
-
-				it( 'Should return false when not saving', () => {
-					const state = deepFreeze(
-						paths.SAVE_STATUS.createTree( saveStatuses.SAVE_SUCCESS )
-					);
-					expect( isSaving( state ) ).toBe( false );
-				} );
-			} );
 		} );
 	} );
 

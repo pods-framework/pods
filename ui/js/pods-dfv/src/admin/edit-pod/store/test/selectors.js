cc4707c6dd393f0ae5cf44729521b22ce3be7068
pglewis <pglewis13@gmail.com>
2019-05-17 12:31:24 -0400

    Selector tests: extract podMeta tests to their own test file

--- ui/js/pods-dfv/src/admin/edit-pod/store/test/selectors.js
+++ ui/js/pods-dfv/src/admin/edit-pod/store/test/selectors.js
@@ -3,8 +3,6 @@ import deepFreeze from 'deep-freeze';
 import * as paths from '../state-paths';
 import {
 	getState,
-	getPodName,
-	getPodMetaValue,
 	getFields,
 } from '../selectors';
 
@@ -27,35 +25,6 @@ describe( 'selectors', () => {
 
 			expect( result ).toBeDefined();
 			expect( result ).toEqual( state );
-		} );
-	} );
-
-	describe( 'podMeta', () => {
-		describe( 'getPodName()', () => {
-			it( 'Should return the Pod name', () => {
-				const state = deepFreeze(
-					paths.POD_META.createTree( { name: 'plugh' } )
-				);
-				const result = getPodName( state );
-				const expected = paths.POD_NAME.getFrom( state );
-
-				expect( result ).toBeDefined();
-				expect( result ).toEqual( expected );
-			} );
-		} );
-
-		describe( 'getPodMetaValue()', () => {
-			it( 'Should return the meta value', () => {
-				const key = 'foo';
-				const expected = 'bar';
-				const state = deepFreeze(
-					paths.POD_META.createTree( { [ key ]: expected } )
-				);
-				const result = getPodMetaValue( state, key );
-
-				expect( result ).toBeDefined();
-				expect( result ).toEqual( expected );
-			} );
 		} );
 	} );
 

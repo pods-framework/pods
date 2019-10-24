e3285e44e1a22261775ca95670a84e33924eeecb
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2019-10-24 17:48:08 +0200

    Use Pods->fetch() instead of creating a new instance

--- classes/PodsUI.php
+++ classes/PodsUI.php
@@ -4931,7 +4931,7 @@ class PodsUI {
 
 					$pod = $this->pod;
 					if ( ! $pod->id() && $row ) {
-						$pod = pods( $this->pod->pod, $row );
+						$pod = $pod->fetch( $row );
 					}
 
 					// Check if the current user is the author of this item.

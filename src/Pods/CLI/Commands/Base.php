7c8aa633d5fbc0b05130fc480c82dfb0d70b953a
Scott Kingsley Clark <lol@scottkclark.com>
2019-12-28 13:43:06 -0600

    More slug handling, fix params sending for REST API, add arg validation, add command synopsis responses

--- src/Pods/CLI/Commands/Base.php
+++ src/Pods/CLI/Commands/Base.php
@@ -86,7 +86,7 @@ abstract class Base {
 
 			WP_CLI::add_command( $command, [
 				$this,
-				'get',
+				'get_by_slug',
 			], $this->build_command_args( 'get', $this->endpoint_single_slug ) );
 		}
 
@@ -104,7 +104,7 @@ abstract class Base {
 
 			WP_CLI::add_command( $command, [
 				$this,
-				'update',
+				'update_by_slug',
 			], $this->build_command_args( 'update', $this->endpoint_single_slug ) );
 		}
 
@@ -122,7 +122,7 @@ abstract class Base {
 
 			WP_CLI::add_command( $command, [
 				$this,
-				'delete',
+				'delete_by_slug',
 			], $this->build_command_args( 'delete', $this->endpoint_single_slug ) );
 		}
 	}
@@ -136,7 +136,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function list_items( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'get', $this->endpoint_archive );
+		return $this->run_endpoint_method( $args, $assoc_args, 'get', $this->endpoint_archive );
 	}
 
 	/**
@@ -148,7 +148,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function add( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'create', $this->endpoint_archive );
+		return $this->run_endpoint_method( $args, $assoc_args, 'create', $this->endpoint_archive );
 	}
 
 	/**
@@ -160,7 +160,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function get( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'get', $this->endpoint_single );
+		return $this->run_endpoint_method( $args, $assoc_args, 'get', $this->endpoint_single );
 	}
 
 	/**
@@ -172,7 +172,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function get_by_slug( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'get', $this->endpoint_single_slug );
+		return $this->run_endpoint_method( $args, $assoc_args, 'get', $this->endpoint_single_slug );
 	}
 
 	/**
@@ -184,7 +184,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function update( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'update', $this->endpoint_single );
+		return $this->run_endpoint_method( $args, $assoc_args, 'update', $this->endpoint_single );
 	}
 
 	/**
@@ -196,7 +196,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function update_by_slug( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'update', $this->endpoint_single_slug );
+		return $this->run_endpoint_method( $args, $assoc_args, 'update', $this->endpoint_single_slug );
 	}
 
 	/**
@@ -208,7 +208,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function delete( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'delete', $this->endpoint_single );
+		return $this->run_endpoint_method( $args, $assoc_args, 'delete', $this->endpoint_single );
 	}
 
 	/**
@@ -220,7 +220,7 @@ abstract class Base {
 	 * @param array $assoc_args List of associative arguments.
 	 */
 	public function delete_by_slug( array $args, array $assoc_args ) {
-		return $this->run_endpoint_method( $assoc_args, 'delete', $this->endpoint_single_slug );
+		return $this->run_endpoint_method( $args, $assoc_args, 'delete', $this->endpoint_single_slug );
 	}
 
 	/**
@@ -228,28 +228,55 @@ abstract class Base {
 	 *
 	 * @since 2.8
 	 *
+	 * @param array         $args       List of positional arguments.
 	 * @param array         $assoc_args List of associative arguments.
 	 * @param string        $method     Method name.
 	 * @param Base_Endpoint $endpoint   Endpoint object.
 	 */
-	public function run_endpoint_method( array $assoc_args, $method, Base_Endpoint $endpoint ) {
+	public function run_endpoint_method( array $args, array $assoc_args, $method, Base_Endpoint $endpoint ) {
 		if ( ! method_exists( $endpoint, $method ) ) {
 			return;
 		}
 
 		$assoc_args = $this->json_or_args( $assoc_args );
+		$assoc_args = $this->validate_args( $args, $assoc_args, $method, $endpoint );
 
-		$valid = $this->validate_args( $assoc_args );
-
-		if ( is_wp_error( $valid ) ) {
-			return $this->output_error_response( $valid );
+		if ( is_wp_error( $assoc_args ) ) {
+			return $this->output_error_response( $assoc_args );
 		}
 
 		$attributes = [
 			'args' => $assoc_args,
 		];
 
-		$request = new WP_REST_Request( '', '', $attributes );
+		$method_mapping = [
+			'list'   => 'GET',
+			'add'    => 'POST',
+			'get'    => 'GET',
+			'update' => 'POST',
+			'delete' => 'DELETE',
+		];
+
+		$rest_method = 'GET';
+
+		if ( isset( $method_mapping[ $method ] ) ) {
+			$rest_method = $method_mapping[ $method ];
+		}
+
+		$route = $endpoint->get_route();
+
+		// Add numeric args.
+		if ( ! empty( $args ) ) {
+			$route = sprintf( $route, ...$args );
+		}
+
+		$request = new WP_REST_Request( $rest_method, '/' . rest_get_url_prefix() . $route, $attributes );
+
+		if ( 'POST' === $rest_method ) {
+			$request->set_body_params( $assoc_args );
+		} else {
+			$request->set_query_params( $assoc_args );
+		}
 
 		$response = $endpoint->$method( $request );
 
@@ -257,9 +284,9 @@ abstract class Base {
 			return $this->output_error_response( $response );
 		}
 
-		// @todo Output response data in a better way.
+		// @todo Output response data in a better way maybe.
 
-		var_dump( $response );
+		WP_CLI::line( wp_json_encode( $response ) );
 
 		WP_CLI::success( __( 'Command successful', 'pods' ) );
 	}
@@ -288,22 +315,134 @@ abstract class Base {
 	 *
 	 * @since 2.8
 	 *
-	 * @param array  $assoc_args List of associative arguments.
-	 * @param string $command    Command name.
+	 * @param array         $args       List of positional arguments.
+	 * @param array         $assoc_args List of associative arguments.
+	 * @param string        $method     Method name.
+	 * @param Base_Endpoint $endpoint   Endpoint object.
 	 *
-	 * @return true|WP_Error Whether the args validated or the WP_Error object with what failed.
+	 * @return array|WP_Error The associative args that validated or the WP_Error object with what failed.
 	 */
-	public function validate_args( array $assoc_args, $command = null ) {
-		// @todo Get list of args and determine what's really required.
+	public function validate_args( array $args, array $assoc_args, $method, Base_Endpoint $endpoint ) {
+		$methdod_mapping = [
+			'get'    => 'READ_args',
+			'create' => 'CREATE_args',
+			'update' => 'EDIT_args',
+			'delete' => 'DELETE_args',
+		];
 
-		$runner    = WP_CLI::get_runner();
-		$arguments = $runner->arguments;
-
-		if ( null === $command && ! empty( $arguments ) ) {
-			$command = end( $arguments );
+		if ( ! isset( $methdod_mapping[ $method ] ) ) {
+			return $assoc_args;
 		}
 
-		return true;
+		$method = $methdod_mapping[ $method ];
+
+		if ( ! method_exists( $endpoint, $method ) ) {
+			return $assoc_args;
+		}
+
+		$rest_args = $endpoint->$method();
+
+		if ( empty( $rest_args ) ) {
+			return $assoc_args;
+		}
+
+		foreach ( $rest_args as $param => $arg ) {
+			// Handle path args.
+			if ( isset( $arg['in'] ) && 'path' === $arg['in'] ) {
+				if ( empty( $args ) ) {
+					if ( empty( $arg['required'] ) ) {
+						continue;
+					}
+
+					// @todo Fix messaging.
+					return new WP_Error( 'no', 'Missing positional argument' );
+				}
+
+				$value = current( $args );
+
+				$value = $this->validate_arg( $value, $arg, $param );
+
+				if ( is_wp_error( $value ) ) {
+					return $value;
+				}
+
+				$assoc_args[ $param ] = $value;
+
+				continue;
+			}
+
+			// Handle normal args.
+			$value = null;
+
+			if ( isset( $assoc_args[ $param ] ) ) {
+				$value = $assoc_args[ $param ];
+			}
+
+			$value = $this->validate_arg( $value, $arg, $param );
+
+			if ( is_wp_error( $value ) ) {
+				return $value;
+			}
+
+			if ( null !== $value ) {
+				$assoc_args[ $param ] = $value;
+			}
+		}
+
+		return $assoc_args;
+	}
+
+	/**
+	 * Determine whether the arg validates.
+	 *
+	 * @since 2.8
+	 *
+	 * @param mixed  $value CLI value provided.
+	 * @param array  $arg   REST API argument options.
+	 * @param string $param Parameter name.
+	 *
+	 * @return mixed|WP_Error The argument value or the WP_Error object with what failed to validate.
+	 */
+	public function validate_arg( $value, array $arg, $param ) {
+		$is_required = ! empty( $arg['required'] );
+		$is_null     = null === $value;
+
+		if ( $is_null ) {
+			if ( ! $is_required ) {
+				return $value;
+			}
+
+			// @todo Fix messaging.
+			return new WP_Error( 'no', 'Argument is required' );
+		}
+
+		if ( 'integer' === $arg['type'] ) {
+			$value = (int) $value;
+		}
+
+		if ( isset( $arg['validate_callback'] ) && is_callable( $arg['validate_callback'] ) ) {
+			$valid = call_user_func( $arg['validate_callback'], $value );
+
+			if ( ! $valid ) {
+				return '';
+			}
+
+			if ( is_wp_error( $valid ) ) {
+				return $valid;
+			}
+		}
+
+		$valid = rest_validate_value_from_schema( $value, $arg, $param );
+
+		if ( ! $valid ) {
+			return '';
+		}
+
+		if ( is_wp_error( $valid ) ) {
+			return $valid;
+		}
+
+		return $value;
 	}
 
 	/**
@@ -341,9 +480,36 @@ abstract class Base {
 			return [];
 		}
 
-		$cli_args = [];
+		$cli_args = [
+			'synopsis' => [],
+		];
 
-		// @todo Map the REST args to CLI args formatting.
+		foreach ( $rest_args as $param => $arg ) {
+			$cli_arg = [
+				'type' => 'assoc',
+				'name' => $param,
+				'optional' => empty( $arg['required'] ),
+			];
+
+			if ( ! empty( $arg['description'] ) ) {
+				$cli_arg['description'] = $arg['description'];
+			}
+
+			if ( ! empty( $arg['default'] ) ) {
+				$cli_arg['default'] = $arg['default'];
+			}
+
+			if ( ! empty( $arg['enum'] ) ) {
+				$cli_arg['options'] = $arg['enum'];
+			}
+
+			// Handle path args.
+			if ( isset( $arg['in'] ) && 'path' === $arg['in'] ) {
+				$cli_arg['type'] = 'positional';
+			}
+
+			$cli_args['synopsis'][] = $cli_arg;
+		}
 
 		return $cli_args;
 	}
@@ -354,6 +520,8 @@ abstract class Base {
 	 * @since 2.8
 	 *
 	 * @param WP_Error $error The error object.
+	 *
+	 * @throws WP_CLI\ExitException
 	 */
 	public function output_error_response( WP_Error $error ) {
 		$error_message = sprintf( '%1$s [%2$s]', $error->get_error_message(), $error->get_error_code() );

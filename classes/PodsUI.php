a2b5f11fdb41390fcccf1b6eb191588962058b00
Nic Ford <nicdford@gmail.com>
2020-01-08 20:41:46 -0800

    Copy updates

--- classes/PodsUI.php
+++ classes/PodsUI.php
@@ -3210,8 +3210,8 @@ class PodsUI {
 				<img class="pods-admin_friends-callout_logo" src="https://32z2oi1mh41t3vukd84dqy4f-wpengine.netdna-ssl.com/files/2018/04/friends-of-pods-logo.svg" width="100px" alt="Friends of Pods Logo">
 			</div>
 			<div class="pods-admin_friends-callout_content-container">
-				<h2 class="pods-admin_friends-callout_headline">We need <span class="pods-admin_friends-you">YOU</span> in 2020!</h2>
-				<p class="pods-admin_friends-callout_text">Things they are a changin' and we want you to be a part of it! Our goal is to be fully funded by users like you. Help us reach 300 recurring donors in 2020.</p>
+				<h2 class="pods-admin_friends-callout_headline">We need <span class="pods-admin_friends-you">YOU</span> in 2020</h2>
+				<p class="pods-admin_friends-callout_text">Things they are a changin' and we want you to be a part of it! Our goal is to be fully funded by users like you. Help us reach 200 recurring donors in 2020.</p>
 				<div class="pods-admin_friends-callout_button-group">
 					<a href="https://friends.pods.io/" class="pods-admin_friends-callout_button">Learn More</a>
 					<a href="https://pods.io/friends-of-pods/membership-levels/" class="pods-admin_friends-callout_button--join">Join Now</a>

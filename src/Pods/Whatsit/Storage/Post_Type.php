4d4a92d5bc0e6c4462f725fdd3ac43563a63520d
Scott Kingsley Clark <lol@scottkclark.com>
2020-03-20 23:32:46 -0500

    Allow forcing refresh

--- src/Pods/Whatsit/Storage/Post_Type.php
+++ src/Pods/Whatsit/Storage/Post_Type.php
@@ -484,11 +484,12 @@ class Post_Type extends Collection {
 	/**
 	 * Setup object from a Post ID or Post object.
 	 *
-	 * @param \WP_Post|array|int $post Post object or ID of the object.
+	 * @param \WP_Post|array|int $post          Post object or ID of the object.
+	 * @param bool               $force_refresh Whether to force the refresh of the object.
 	 *
 	 * @return Whatsit|null
 	 */
-	public function to_object( $post ) {
+	public function to_object( $post, $force_refresh = false ) {
 		if ( null !== $post && ! $post instanceof \WP_Post ) {
 			$post = get_post( $post );
 		}
@@ -507,7 +508,11 @@ class Post_Type extends Collection {
 		$object = $object_collection->get_object( $post->ID );
 
 		if ( $object instanceof Whatsit && $post->post_type === '_pods_' . $object->get_object_type() ) {
-			return $object;
+			if ( ! $force_refresh ) {
+				return $object;
+			}
+
+			$object_collection->unregister_object( $object );
 		}
 
 		$args = array();

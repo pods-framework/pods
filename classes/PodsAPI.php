b982b500b28a56c898f8b846aaa2fbd4575ba243
Scott Kingsley Clark <lol@scottkclark.com>
2020-05-14 20:47:04 -0500

    Update classes/PodsAPI.php

--- classes/PodsAPI.php
+++ classes/PodsAPI.php
@@ -5028,6 +5028,7 @@ class PodsAPI {
 								$related_item_data = $this->do_hook( 'export_pod_item_level', $related_item, $related_pod->pod, $related_pod->id(), $related_pod, $related_fields, $depth, $flatten, ( $current_depth + 1 ), $params );
 
 								if ( function_exists( 'wp_is_json_request' ) && wp_is_json_request() ) {
+									// Don't pass IDs as keys for REST API context to ensure arrays of data are returned.
 									$related_data[] = $related_item_data;
 								} else {
 									$related_data[ $related_id ] = $related_item_data;

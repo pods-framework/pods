9286a8fe36a81c8b01e7b7ed0a41409bf7e49cb3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-05-18 18:51:19 +0200

    Also test array return type
    This is where the actual bug occured. This test will make sure get_metadata handlers are functioning properly with $single param.

--- tests/phpunit/includes/Meta/Test_Metadata.php
+++ tests/phpunit/includes/Meta/Test_Metadata.php
@@ -213,6 +213,101 @@ class Test_Metadata extends \Pods_Unit_Tests\Pods_UnitTestCase
 
 	public function test_get_metadata() {
 
+		foreach ( self::$obj_ids as $type => $ids ) {
+
+			$id_key   = self::get_id_key( $type );
+			$name     = self::$pod_names[ $type ];
+			$get_meta = 'get_' . $name;
+
+			foreach ( $ids as $key => $id ) {
+
+				$single        = call_user_func( $get_meta, $id, 'related_single', false );
+				$single_single = call_user_func( $get_meta, $id, 'related_single', true );
+				$multi         = call_user_func( $get_meta, $id, 'related_multi', false );
+				$multi_single  = call_user_func( $get_meta, $id, 'related_multi', true );
+
+				// Pods returns object arrays by default. Fetch the ID's of these objects.
+				$single        = self::convert_output_type_ids( $single, $id_key );
+				$multi         = self::convert_output_type_ids( $multi, $id_key );
+				$single_single = isset( $single_single[ $id_key ] ) ? $single_single[ $id_key ] : $single_single;
+				$multi_single  = isset( $multi_single[ $id_key ] ) ? $multi_single[ $id_key ] : $multi_single;
+
+				// Add a bit of context when a assertion has failed.
+				$message = "Method: `{$get_meta}()`:";
+
+				switch ( $key ) {
+					case 0:
+
+						$images = self::get_images();
+
+						// Single param false
+						$value = call_user_func( $get_meta, $id, 'text', false );
+						$this->assertEquals( array( 'text' ), $value, $message );
+
+						$value = call_user_func( $get_meta, $id, 'images', false );
+						$value = self::convert_output_type_ids( $value, 'ID' );
+						$this->assertEquals( $images, $value, $message );
+
+						$this->assertEquals( array(), $single, $message );
+						$this->assertEquals( array(), $multi, $message );
+
+						// Single param true
+						$value = call_user_func( $get_meta, $id, 'text', true );
+						$this->assertEquals( 'text', $value, $message );
+
+						$value = call_user_func( $get_meta, $id, 'images', true );
+						$value = isset( $value[ 'ID' ] ) ? $value[ 'ID' ] : $value;
+						$this->assertEquals( $images[0], $value, $message );
+
+						$this->assertEquals( '', $single_single, $message );
+						$this->assertEquals( '', $multi_single, $message );
+
+						break;
+					case 1:
+						// Single related to: 2
+						// Multi related to: 2
+
+						$single_rel = self::$obj_ids[ $type ][ 2 ];
+						$multi_rel  = self::$obj_ids[ $type ][ 2 ];
+
+						// Single param false
+						$this->assertEquals( array( $single_rel ), $single, $message );
+						$this->assertEquals( array( $multi_rel ), $multi, $message );
+
+						// Single param true
+						$this->assertEquals( $single_rel, $single_single, $message );
+						$this->assertEquals( $multi_rel, $multi_single, $message );
+
+						break;
+					case 2:
+						// Single related to: 1
+						// Multi related to: 0 and 1
+
+						$single_rel = self::$obj_ids[ $type ][ 1 ];
+						$multi_rel  = array(
+							self::$obj_ids[ $type ][ 0 ],
+							self::$obj_ids[ $type ][ 1 ]
+						);
+
+						// Single param false
+						$this->assertEquals( array( $single_rel ), $single, $message );
+						$this->assertEquals( $multi_rel, $multi, $message );
+
+						// Single param true
+						$this->assertEquals( $single_rel, $single_single, $message );
+						// Even when having multiple values, if single is true then it should return the first value.
+						$this->assertEquals( $multi_rel[0], $multi_single, $message );
+
+						break;
+				}
+
+			}
+
+		}
+	}
+
+	public function test_get_metadata_ids() {
+
 		// Make sure we return ID's.
 		add_filter( 'pods_pods_field_related_output_type', array( $this, 'filter_output_type_ids' ) );
 		//remove_filter( 'get_post_metadata', array( pods_meta(), 'get_post_meta' ), 10 );
@@ -315,6 +410,15 @@ class Test_Metadata extends \Pods_Unit_Tests\Pods_UnitTestCase
 				break;
 		}
 		return $id_key;
+	}
+
+	public static function convert_output_type_ids( $list, $id_key ) {
+		foreach ( $list as $key => $value ) {
+			if ( isset( $value[ $id_key ] ) ) {
+				$list[ $key ] = $value[ $id_key ];
+			}
+		}
+		return $list;
 	}
 
 	public function filter_output_type_ids() {

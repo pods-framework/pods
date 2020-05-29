98dcdfa7d409f2107db10fa4b58a624f12d70acd
Zack Rothauser <zrothauser@gmail.com>
2020-05-29 16:37:03 -0700

    Strip out old modal classes, add new ones

--- ui/js/dfv/src/admin/edit-pod/main-tabs/field-group-settings.js
+++ ui/js/dfv/src/admin/edit-pod/main-tabs/field-group-settings.js
@@ -25,82 +25,78 @@ const FieldGroupSettings = ( {
 
 	return (
 		<Modal
-			className="pods-field-group_settings pods-field-group_settings--visible"
+			className="pods-settings-modal"
 			title={ title }
 			isDismissible={ true }
 			onRequestClose={ cancelEditing }
 		>
-			<Button isPrimary onClick={ () => save( changedOptions ) }>
-				{ __( 'Save Group', 'pods' ) }
-			</Button>
-
-			<Button isSecondary onClick={ cancelEditing }>
-				{ __( 'Cancel', 'pods' ) }
-			</Button>
-
 			{ hasSaveError && (
 				<span className="pod-field-group_settings-error-message">
 					{ __( 'There was an error saving the group, please try again.', 'pods' ) }
 				</span>
 			) }
 
-			<div className="pods-field-group_settings-container">
-				<div className="pods-field-group_settings-options">
-					<div
-						className="pods-field-group_settings-sidebar"
-						role="tablist"
-						aria-label={ __( 'Pods Field Group Settings', 'pods' ) }
-					>
-						{ editGroupSections.map( ( {
-							name: sectionName,
-							label: sectionLabel,
-						} ) => {
-							const isActive = selectedTab === sectionName;
+			<div
+				className="pods-settings-modal__tabs"
+				role="tablist"
+				aria-label={ __( 'Pods Field Group Settings', 'pods' ) }
+			>
+				{ editGroupSections.map( ( {
+					name: sectionName,
+					label: sectionLabel,
+				} ) => {
+					const isActive = selectedTab === sectionName;
 
-							const classes = classNames(
-								'pods-field-group_settings-sidebar-item',
-								{
-									'pods-field-group_settings-sidebar-item--active': isActive,
-								}
-							);
-
-							return (
-								<div
-									className={ classes }
-									aria-controls={ `${ sectionName }-tab` }
-									role="button"
-									tabIndex={ 0 }
-									key={ sectionName }
-									onClick={ () => setSelectedTab( sectionName ) }
-									onKeyPress={ ( event ) => event.keyCode === ENTER_KEY && setSelectedTab( sectionName ) }
-								>
-									{ sectionLabel }
-								</div>
-							);
-						} ) }
-					</div>
-
-					<div
-						className="pods-field-group_settings-main"
-						role="tabpanel"
-						aria-labelledby="main"
-						id="main-tab"
-					>
+					const classes = classNames(
+						'pods-settings-modal__tab-item',
 						{
-							<DynamicTabContent
-								tabOptions={ editGroupSections.find( ( section ) => section.name === selectedTab ).fields }
-								optionValues={ changedOptions }
-								setOptionValue={ ( optionName, value ) => {
-									setChangedOptions( {
-										...changedOptions,
-										[ optionName ]: value,
-									} );
-								} }
-							/>
+							'pods-settings-modal__tab-item--active': isActive,
 						}
-					</div>
-				</div>
+					);
+
+					return (
+						<div
+							className={ classes }
+							aria-controls={ `${ sectionName }-tab` }
+							role="button"
+							tabIndex={ 0 }
+							key={ sectionName }
+							onClick={ () => setSelectedTab( sectionName ) }
+							onKeyPress={ ( event ) => event.keyCode === ENTER_KEY && setSelectedTab( sectionName ) }
+						>
+							{ sectionLabel }
+						</div>
+					);
+				} ) }
 			</div>
+
+			<div
+				className="pods-settings-modal__panel"
+				role="tabpanel"
+				aria-labelledby="main"
+				id="main-tab"
+			>
+				{
+					<DynamicTabContent
+						tabOptions={ editGroupSections.find( ( section ) => section.name === selectedTab ).fields }
+						optionValues={ changedOptions }
+						setOptionValue={ ( optionName, value ) => {
+							setChangedOptions( {
+								...changedOptions,
+								[ optionName ]: value,
+							} );
+						} }
+					/>
+				}
+			</div>
+
+			<Button isSecondary onClick={ cancelEditing }>
+				{ __( 'Cancel', 'pods' ) }
+			</Button>
+
+			<Button isPrimary onClick={ () => save( changedOptions ) }>
+				{ __( 'Save Group', 'pods' ) }
+			</Button>
 		</Modal>
 	);
 };

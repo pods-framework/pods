0489d5848d289ef9e7bc065b760e69666ba908f6
Zack Rothauser <zrothauser@gmail.com>
2020-09-05 19:34:10 -0500

    Add tests for validators

--- ui/js/dfv/src/helpers/validators.js
+++ ui/js/dfv/src/helpers/validators.js
@@ -1,7 +1,5 @@
 import { __, sprintf } from '@wordpress/i18n';
 
-const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 export const requiredValidator = ( fieldLabel ) => ( value ) => {
 	if ( ! value ) {
 		// translators: Field label required message.
@@ -14,24 +12,26 @@ export const requiredValidator = ( fieldLabel ) => ( value ) => {
 export const maxValidator = ( maxValue ) => ( value ) => {
 	if ( parseFloat( value ) > parseFloat( maxValue ) ) {
 		// translators: Exceeds a maximum value.
-		throw sprintf( __( 'Exceeds the maximum value of %s', 'pods' ), maxValue );
+		throw sprintf( __( 'Exceeds the maximum value of %s.', 'pods' ), maxValue );
 	}
 
 	return true;
 };
 
 export const minValidator = ( minValue ) => ( value ) => {
-	if ( parseFloat( value ) < parseFloat( value ) ) {
+	if ( parseFloat( value ) < parseFloat( minValue ) ) {
 		// translators: Below a minimum value.
-		throw sprintf( __( 'Below the minimum value of %s', 'pods' ), minValue );
+		throw sprintf( __( 'Below the minimum value of %s.', 'pods' ), minValue );
 	}
 
 	return true;
 };
 
 export const emailValidator = () => ( value ) => {
+	const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 	if ( ! value || ! value.match( EMAIL_REGEX ) ) {
-		throw __( 'Invalid email address format', 'pods' );
+		throw __( 'Invalid email address format.', 'pods' );
 	}
 
 	return true;

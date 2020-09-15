bf553ef1e40eac68ca445f456d0949bc0b465db6
Zack Rothauser <zrothauser@gmail.com>
2020-09-15 11:49:01 -0500

    Retrieve post media details for the File component

--- ui/js/dfv/src/fields/file/index.js
+++ ui/js/dfv/src/fields/file/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import apiFetch from '@wordpress/api-fetch';
 
 import MarionetteAdapter from 'dfv/src/fields/marionette-adapter';
 import { File as FileView } from './file-upload';
 import { FIELD_PROP_TYPE_SHAPE } from 'dfv/src/config/prop-types';
 
-// @todo this may be an incomplete field component
 // @todo add tests
 const File = ( props ) => {
 	const {
@@ -15,24 +16,54 @@ const File = ( props ) => {
 		setValue,
 	} = props;
 
+	const [ collectionData, setCollectionData ] = useState( [] );
+
 	const setValueFromModels = ( models ) => {
-		// const uploadedFileIDs = models.map( ( model ) => model.id );
-
-		const fileData = models.map( ( model ) => {
-			return {
-				id: model.get( 'id' ),
-				icon: model.get( 'icon' ),
-				name: model.get( 'name' ),
-				edit_link: model.get( 'edit_link' ),
-				link: model.get( 'link' ),
-				download: model.get( 'download' ),
-			};
-		} );
-
-		console.log( 'setting value for File component in setValueFromModels: ', fileData );
-
-		setValue( fileData );
+		if ( Array.isArray( models ) ) {
+			setValue( models.map( ( model ) => model.id ).join( ',' ) );
+		} else {
+			setValue( models.get( 'id' ) );
+		}
 	};
+
+	// The `value` prop will be a comma-separated string of media post IDs,
+	// but we need to pass an array of objects with data about the media
+	// to the Backbone view/model.
+	useEffect( () => {
+		if ( ! value || ! value.length ) {
+			setCollectionData( [] );
+			return;
+		}
+
+		const allMediaIDs = value.split( ',' );
+
+		const getMediaItemData = async ( mediaID ) => {
+			try {
+				const result = await apiFetch( { path: `/wp/v2/media/${ mediaID }` } );
+
+				return {
+					id: mediaID,
+					// eslint-disable-next-line camelcase
+					icon: result?.media_details?.sizes?.thumbnail?.source_url,
+					name: result.title.rendered,
+					edit_link: `/wp-admin/post.php?post=${ mediaID }&action=edit`,
+					link: result.link,
+					download: result.source_url,
+				};
+			} catch ( e ) {
+				return {
+					id: mediaID,
+				};
+			}
+		};
+
+		const getAndSetMediaData = async ( mediaIDs ) => {
+			const results = await Promise.all( mediaIDs.map( getMediaItemData ) );
+			setCollectionData( results );
+		};
+
+		getAndSetMediaData( allMediaIDs );
+	}, [ value ] );
 
 	return (
 		<MarionetteAdapter
@@ -40,7 +71,7 @@ const File = ( props ) => {
 			htmlAttr={ htmlAttr }
 			fieldConfig={ fieldConfig }
 			View={ FileView }
-			value={ value }
+			value={ collectionData }
 			setValue={ setValueFromModels }
 		/>
 	);
@@ -49,19 +80,7 @@ const File = ( props ) => {
 File.propTypes = {
 	fieldConfig: FIELD_PROP_TYPE_SHAPE.isRequired,
 	setValue: PropTypes.func.isRequired,
-	value: PropTypes.arrayOf(
-		PropTypes.shape( {
-			id: PropTypes.oneOfType( [
-				PropTypes.string,
-				PropTypes.number,
-			] ).isRequired,
-			icon: PropTypes.string.isRequired,
-			name: PropTypes.string.isRequired,
-			edit_link: PropTypes.string.isRequired,
-			link: PropTypes.string.isRequired,
-			download: PropTypes.string.isRequired,
-		} )
-	),
+	value: PropTypes.string,
 };
 
 export default File;

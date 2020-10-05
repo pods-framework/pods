3abe242cd47c86f0e53462d36e9f5c8b0d56fdee
Zack Rothauser <zrothauser@gmail.com>
2020-10-05 17:01:57 -0500

    Add better interface for color picker

--- ui/js/dfv/src/fields/color/index.js
+++ ui/js/dfv/src/fields/color/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { ColorPicker } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { ColorIndicator, ColorPicker } from '@wordpress/components';
 
 import { FIELD_PROP_TYPE_SHAPE } from 'dfv/src/config/prop-types';
+
+import './color.scss';
 
 const Color = ( {
 	setValue,
 	value,
 } ) => {
+	const [ isOpen, setIsOpen ] = useState( false );
+
 	return (
-		<ColorPicker
-			color={ value }
-			onChangeComplete={ ( newValue ) => setValue( newValue.hex ) }
-			disableAlpha
-		/>
+		<div>
+			<button
+				onClick={ () => setIsOpen( ( prevValue ) => ! prevValue ) }
+				className="button pods-color-select-button"
+			>
+				<ColorIndicator colorValue={ value } />
+
+				{ __( 'Select Color', 'pods' ) }
+			</button>
+
+			{ isOpen && (
+				<ColorPicker
+					color={ value }
+					onChangeComplete={ ( newValue ) => setValue( newValue.hex ) }
+					disableAlpha
+					className="pods-color-picker"
+				/>
+			) }
+		</div>
 	);
 };
 

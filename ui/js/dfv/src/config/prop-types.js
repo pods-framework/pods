c6940f398c7f1010e89f63ee3bdaa6909290ff8c
Scott Kingsley Clark <lol@scottkclark.com>
2020-10-26 09:37:57 -0500

    Add testing coverage for slug_fallback

--- ui/js/dfv/src/config/prop-types.js
+++ ui/js/dfv/src/config/prop-types.js
@@ -65,6 +65,7 @@ export const FIELD_PROP_TYPE_SHAPE = PropTypes.exact( {
 	] ),
 	sister_id: NUMBER_OR_NUMBER_AS_STRING,
 	slug_placeholder: PropTypes.string,
+	slug_fallback: PropTypes.string,
 	storage_type: PropTypes.string,
 	type: PropTypes.string.isRequired,
 	unique: PropTypes.string,

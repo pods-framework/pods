d8fa5aa101aa185638d0ef28467d8b40edf5de0e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-11-05 17:54:31 +0100

    Setup for pick get_object_data() tests

--- tests/phpunit/includes/tests-pods-field-pick.php
+++ tests/phpunit/includes/tests-pods-field-pick.php
@@ -100,4 +100,83 @@ class Test_PodsField_Pick extends Pods_UnitTestCase {
 		$this->markTestIncomplete( 'not yet implemented' );
 	}
 
+	/**
+	 * Covers ::get_object_data
+	 */
+	public function test_get_object_data() {
+		$api = pods_api();
+
+		$name     = 'pick_get_object_data';
+		$rel_name = 'related_multi';
+
+		$pod_id = $api->save_pod(
+			array(
+				'type' => 'post_type',
+				'storage' => 'meta',
+				'name' => 'pick_get_object_data',
+			)
+		);
+
+		$params   = array(
+			'pod'              => $name,
+			'pod_id'           => $pod_id,
+			'name'             => $rel_name,
+			'type'             => 'pick',
+			'pick_object'      => 'post_type',
+			'pick_val'         => $name,
+			'pick_format_type' => 'multi',
+		);
+
+		$api->save_field( $params );
+
+		$posts = array(
+			array(
+				'post_title' => 'obj1',
+				'post_date'  => date( 'Y-m-d H:i:s', time() )
+			),
+			array(
+				'post_title' => 'obj2',
+				'post_date'  => date( 'Y-m-d H:i:s', strtotime( '-1 week' ) )
+			),
+			array(
+				'post_title' => 'obj3',
+				'post_date'  => date( 'Y-m-d H:i:s', strtotime( '-1 year' ) )
+			),
+		);
+
+		$post_ids = array();
+		foreach ( $posts as $key => $post ) {
+			unset( $posts[ $key ] );
+			$post_id           = $this->factory()->post->create( $post );
+			$post_ids[]        = $post_id;
+			$posts[ $post_id ] = get_post( $post_id );
+		}
+
+		// Save relationships.
+		pods( $name, $post_ids[0] )->save( $rel_name, array( $post_ids[1], $post_ids[2] ) );
+
+		/**
+		 * Setup done.
+		 */
+
+		// Default.
+
+		$form = pods_form();
+
+		$params = array(
+			'id'      => $post_ids[0],
+			'name'    => $rel_name,
+			'value'   => null,
+			'pod'     => $api->load_pod( array( 'id' => (int) $pod_id ) ),
+			'options' => $api->load_field(
+				array(
+					'name'       => $rel_name,
+					'table_info' => true,
+				)
+			),
+		);
+
+		// @todo.
+		$values = $form::field_method( 'pick', 'get_object_data', $params );
+	}
 }

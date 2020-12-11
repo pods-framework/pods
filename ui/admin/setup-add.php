d15b398d1065dd733dd22eb6b946884a0b49dc3a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-12-11 16:44:07 +0100

    Re-use toggles var

--- ui/admin/setup-add.php
+++ ui/admin/setup-add.php
@@ -432,11 +432,10 @@
 		$document.Pods( 'confirm' );
 		$document.Pods( 'sluggable' );
 
-		$document.find( '.pods-dependent-toggle[data-name-clean]' ).trigger( 'change' );
-
 		var $admin = $( '.pods-admin' ),
 			$toggles = $admin.find( '.pods-dependent-toggle[data-name-clean]' );
 
+		$toggles.trigger( 'change' );
 		$admin.on( 'render', '.pods-form-ui-field', function ( e ) {
 			$toggles.trigger( 'change' );
 		} );

f9b56530686938d980088dc6c8ae2c7c975664e9
Scott Kingsley Clark <lol@scottkclark.com>
2020-12-17 08:45:39 -0600

    Code formatting fixes

--- classes/fields/color.php
+++ classes/fields/color.php
@@ -94,11 +94,12 @@ class PodsField_Color extends PodsField {
 	 * {@inheritdoc}
 	 */
 	public function validate( $value, $name = null, $options = null, $fields = null, $pod = null, $id = null, $params = null ) {
-		$return = parent::validate( $value, $name, $options, $fields, $pod, $id, $params );
+		$validate = parent::validate( $value, $name, $options, $fields, $pod, $id, $params );
 
 		$errors = array();
-		if ( is_array( $return ) ) {
-			$errors = $return;
+
+		if ( is_array( $validate ) ) {
+			$errors = $validate;
 		}
 
 		$check = $this->pre_save( $value, $id, $name, $options, $fields, $pod, $params );
@@ -124,7 +125,7 @@ class PodsField_Color extends PodsField {
 			return $errors;
 		}
 
-		return $return;
+		return $validate;
 	}
 
 	/**

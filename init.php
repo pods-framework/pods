5a7479983414a9aacd337875deb0b741d20594db
Scott Kingsley Clark <lol@scottkclark.com>
2021-01-08 15:50:55 -0600

    Update version

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 Plugin Name: Pods - Custom Content Types and Fields
 Plugin URI: https://pods.io/
 Description: Pods is a framework for creating, managing, and deploying customized content types and fields
-Version: 2.7.25
+Version: 2.7.26
 Author: Pods Framework Team
 Author URI: https://pods.io/about/
 Text Domain: pods
@@ -37,7 +37,7 @@ if ( defined( 'PODS_VERSION' ) || defined( 'PODS_DIR' ) ) {
 	add_action( 'init', 'pods_deactivate_pods_ui' );
 } else {
 	// Current version
-	define( 'PODS_VERSION', '2.7.25' );
+	define( 'PODS_VERSION', '2.7.26' );
 
 	// Version tracking between DB updates themselves
 	define( 'PODS_DB_VERSION', '2.3.5' );

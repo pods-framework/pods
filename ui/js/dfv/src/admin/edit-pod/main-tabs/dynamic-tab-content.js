2c024c02ccf83c0f9a4f32824060c4bf48917312
Zack Rothauser <zrothauser@gmail.com>
2021-01-25 19:37:42 -0600

    Make most of the DynamicTabContent a reusable component

--- ui/js/dfv/src/admin/edit-pod/main-tabs/dynamic-tab-content.js
+++ ui/js/dfv/src/admin/edit-pod/main-tabs/dynamic-tab-content.js
@@ -1,11 +1,18 @@
+/**
+ * External dependencies
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// WordPress Dependencies
+/**
+ * WordPress Dependencies
+ */
 import { __, sprintf } from '@wordpress/i18n';
 
-// Pods dependencies
-import FieldOption from 'dfv/src/components/field-wrapper';
+/**
+ * Pods dependencies
+ */
+import FieldSet from 'dfv/src/components/field-set';
 import { FIELD_PROP_TYPE_SHAPE } from 'dfv/src/config/prop-types';
 
 const MISSING = __( '[MISSING DEFAULT]', 'pods' );
@@ -24,39 +31,23 @@ const DynamicTabContent = ( {
 		return sprintf( labelFormat, param );
 	};
 
-	return tabOptions.map( ( field ) => {
-		const {
-			defaultValue,
-			label,
-			labelParam,
-			name,
-		} = field;
-
-		const dependencyValueEntries = Object
-			.keys( field[ 'depends-on' ] || {} )
-			.map( ( fieldName ) => ( [
-				fieldName,
-				optionValues[ fieldName ],
-			] ) );
-
-		return (
-			<FieldOption
-				key={ name }
-				field={ {
-					...field,
-					label: getLabelValue( label, labelParam, defaultValue ),
-				} }
-				value={ optionValues[ name ] }
-				setOptionValue={ setOptionValue }
-				dependencyValues={ Object.fromEntries( dependencyValueEntries ) }
-			/>
-		);
+	const fields = tabOptions.map( ( tabOption ) => {
+		return {
+			...tabOption,
+			label: getLabelValue( tabOption.label, tabOption.labelParam, tabOption.defaultValue ),
+		};
 	} );
+
+	return (
+		<FieldSet
+			fields={ fields }
+			optionValues={ optionValues }
+			setOptionValue={ setOptionValue }
+		/>
+	);
 };
 
 DynamicTabContent.propTypes = {
-	podType: PropTypes.string.isRequired,
-	podName: PropTypes.string.isRequired,
 	tabOptions: PropTypes.arrayOf( FIELD_PROP_TYPE_SHAPE ).isRequired,
 	optionValues: PropTypes.object.isRequired,
 	setOptionValue: PropTypes.func.isRequired,

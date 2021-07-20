01f6dc6d55761a42cb47d1256986be204b1c3f22
Dan Stefan Oprean <dan@designxenon.com>
2021-07-20 13:11:35 +0300

    Wp code style.

--- ui/admin/ui/edit.php
+++ ui/admin/ui/edit.php
@@ -5,8 +5,9 @@ if ( false !== $ui->icon ) {
 }
 ?>
 <div class="wrap pods-ui">
-	<div id="icon-edit-pages" class="icon32"<?php echo $icon_style; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>>
-		<br />
+	<div id="icon-edit-pages"
+		 class="icon32"<?php echo $icon_style; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>>
+		<br/>
 	</div>
 	<h2>
 		<?php
@@ -14,32 +15,34 @@ if ( false !== $ui->icon ) {
 
 		if ( ! in_array( 'add', $ui->actions_disabled ) && ! in_array( 'add', $ui->actions_hidden ) && ! $ui->restricted( 'add' ) ) {
 			$link = pods_query_arg(
-				array(
-					$ui->num_prefix . 'action' . $ui->num => 'add',
-					$ui->num_prefix . 'id' . $ui->num     => '',
-					$ui->num_prefix . 'do' . $ui->num     => '',
-				), $ui::$allowed, $ui->exclusion()
+					array(
+							$ui->num_prefix . 'action' . $ui->num => 'add',
+							$ui->num_prefix . 'id' . $ui->num     => '',
+							$ui->num_prefix . 'do' . $ui->num     => '',
+					), $ui::$allowed, $ui->exclusion()
 			);
 
 			if ( ! empty( $ui->action_links['add'] ) ) {
 				$link = $ui->action_links['add'];
 			}
 			?>
-			<a href="<?php echo esc_url( $link ); ?>" class="add-new-h2"><?php echo wp_kses_post( $ui->heading['add'] ); ?></a>
+			<a href="<?php echo esc_url( $link ); ?>"
+			   class="add-new-h2"><?php echo wp_kses_post( $ui->heading['add'] ); ?></a>
 			<?php
 		} elseif ( ! in_array( 'manage', $ui->actions_disabled ) && ! in_array( 'manage', $ui->actions_hidden ) && ! $ui->restricted( 'manage' ) ) {
 			$link = pods_query_arg(
-				array(
-					$ui->num_prefix . 'action' . $ui->num => 'manage',
-					$ui->num_prefix . 'id' . $ui->num     => '',
-				), self::$allowed, $ui->exclusion()
+					array(
+							$ui->num_prefix . 'action' . $ui->num => 'manage',
+							$ui->num_prefix . 'id' . $ui->num     => '',
+					), self::$allowed, $ui->exclusion()
 			);
 
 			if ( ! empty( $ui->action_links['manage'] ) ) {
 				$link = $ui->action_links['manage'];
 			}
 			?>
-			<a href="<?php echo esc_url( $link ); ?>" class="add-new-h2">&laquo; <?php echo sprintf( __( 'Back to %s', 'pods' ), $ui->heading['manage'] ); ?></a>
+			<a href="<?php echo esc_url( $link ); ?>"
+			   class="add-new-h2">&laquo; <?php echo sprintf( __( 'Back to %s', 'pods' ), $ui->heading['manage'] ); ?></a>
 			<?php
 		}//end if
 		?>

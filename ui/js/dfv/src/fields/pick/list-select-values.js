482a435e3dc0ded2b84d71030ac1e1eaf4204f95
Zack Rothauser <zrothauser@gmail.com>
2021-08-17 19:28:20 -0500

    Simplifies rendering logic for the List and Autocomplete fields, fixes for setting data loaded by ajax in autocomplete fields, fixes for loading values for list and autocomplete fields

--- ui/js/dfv/src/fields/pick/list-select-values.js
+++ ui/js/dfv/src/fields/pick/list-select-values.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React, { useState, useEffect } from 'react';
-import AsyncSelect from 'react-select/async';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
@@ -19,9 +18,7 @@ import {
 /**
  * Other Pods dependencies
  */
-import loadAjaxOptions from '../../helpers/loadAjaxOptions';
 import IframeModal from 'dfv/src/components/iframe-modal';
-import { PICK_OPTIONS, FIELD_PROP_TYPE } from 'dfv/src/config/prop-types';
 
 import './list-select.scss';
 
@@ -209,14 +206,12 @@ ListSelectItem.propTypes = {
 	setFieldItemData: PropTypes.func.isRequired,
 };
 
-const ListSelect = ( {
+const ListSelectValues = ( {
 	name,
 	value,
-	options,
 	fieldItemData,
 	setFieldItemData,
 	setValue,
-	placeholder,
 	isMulti,
 	limit,
 	defaultIcon,
@@ -225,7 +220,6 @@ const ListSelect = ( {
 	showEditLink,
 	editIframeTitle,
 	readOnly = false,
-	ajaxData,
 } ) => {
 	// Always have an array of values for the list, even if
 	// we were just passed a single object.
@@ -265,89 +259,67 @@ const ListSelect = ( {
 	};
 
 	return (
-		<>
-			{ ! readOnly && (
-				<div className="pods-ui-list-autocomplete">
-					<AsyncSelect
-						defaultOptions={ options }
-						loadOptions={ ajaxData?.ajax ? loadAjaxOptions( ajaxData ) : undefined }
-						value={ value }
-						placeholder={ placeholder }
-						isMulti={ isMulti }
-						controlShouldRenderValue={ false }
-						onChange={ ( newOption ) => {
-							if ( isMulti ) {
-								setValue( newOption.map( ( selection ) => selection.value ) );
-							} else {
-								setValue( newOption.value );
-							}
-						} }
-					/>
-				</div>
+		<div className="pods-pick-values">
+			{ !! arrayOfValues.length && (
+				<ul className="pods-dfv-list pods-relationship">
+					{ arrayOfValues.map( ( valueItem, index ) => {
+						const itemName = isMulti ? `${ name }[${ index }]` : name;
+						const itemId = isMulti ? `${ name }[${ index }]` : name;
+
+						// There may be additional data in an object from the fieldItemData
+						// array.
+						const moreData = fieldItemData.find(
+							( item ) => item?.id === valueItem.value
+						);
+
+						const icon = showIcon ? ( moreData?.icon || defaultIcon ) : undefined;
+
+						// May need to change the label, if it differs from the provided value.
+						const displayValue = valueItem;
+
+						const matchingFieldItemData = fieldItemData.find(
+							( item ) => Number( item.id ) === Number( valueItem.value )
+						);
+
+						if ( matchingFieldItemData && matchingFieldItemData.name ) {
+							displayValue.label = matchingFieldItemData.name;
+						}
+
+						return (
+							<ListSelectItem
+								key={ `${ name }-${ index }` }
+								fieldName={ name }
+								itemName={ itemName }
+								itemId={ itemId }
+								value={ displayValue }
+								isDraggable={ ! readOnly && ( 1 !== limit ) }
+								isRemovable={ ! readOnly }
+								editLink={ ! readOnly && showEditLink ? moreData?.edit_link : undefined }
+								viewLink={ showViewLink ? moreData?.link : undefined }
+								editIframeTitle={ editIframeTitle }
+								icon={ icon }
+								removeItem={ () => removeValueAtIndex( index ) }
+								setFieldItemData={ setFieldItemData }
+								moveUp={
+									( ! readOnly && index !== 0 )
+										? () => swapItems( index, index - 1 )
+										: undefined
+								}
+								moveDown={
+									( ! readOnly && index !== ( arrayOfValues.length - 1 ) )
+										? () => swapItems( index, index + 1 )
+										: undefined
+								}
+							/>
+						);
+					} ) }
+				</ul>
 			) }
-
-			<div className="pods-pick-values">
-				{ !! arrayOfValues.length && (
-					<ul className="pods-dfv-list pods-relationship">
-						{ arrayOfValues.map( ( valueItem, index ) => {
-							const itemName = isMulti ? `${ name }[${ index }]` : name;
-							const itemId = isMulti ? `${ name }[${ index }]` : name;
-
-							// There may be additional data in an object from the fieldItemData
-							// array.
-							const moreData = fieldItemData.find(
-								( item ) => item?.id === valueItem.value
-							);
-
-							const icon = showIcon ? ( moreData?.icon || defaultIcon ) : undefined;
-
-							// May need to change the label, if it differs from the provided value.
-							const displayValue = valueItem;
-
-							const matchingFieldItemData = fieldItemData.find(
-								( item ) => Number( item.id ) === Number( valueItem.value )
-							);
-
-							if ( matchingFieldItemData && matchingFieldItemData.name ) {
-								displayValue.label = matchingFieldItemData.name;
-							}
-
-							return (
-								<ListSelectItem
-									key={ `${ name }-${ index }` }
-									fieldName={ name }
-									itemName={ itemName }
-									itemId={ itemId }
-									value={ displayValue }
-									isDraggable={ ! readOnly && ( 1 !== limit ) }
-									isRemovable={ ! readOnly }
-									editLink={ ! readOnly && showEditLink ? moreData?.edit_link : undefined }
-									viewLink={ showViewLink ? moreData?.link : undefined }
-									editIframeTitle={ editIframeTitle }
-									icon={ icon }
-									removeItem={ () => removeValueAtIndex( index ) }
-									setFieldItemData={ setFieldItemData }
-									moveUp={
-										( ! readOnly && index !== 0 )
-											? () => swapItems( index, index - 1 )
-											: undefined
-									}
-									moveDown={
-										( ! readOnly && index !== ( arrayOfValues.length - 1 ) )
-											? () => swapItems( index, index + 1 )
-											: undefined
-									}
-								/>
-							);
-						} ) }
-					</ul>
-				) }
-			</div>
-		</>
+		</div>
 	);
 };
 
-ListSelect.propTypes = {
+ListSelectValues.propTypes = {
 	htmlAttributes: PropTypes.shape( {
 		id: PropTypes.string,
 		class: PropTypes.string,
@@ -367,12 +339,10 @@ ListSelect.propTypes = {
 		),
 	] ),
 	setValue: PropTypes.func.isRequired,
-	options: PICK_OPTIONS.isRequired,
 	fieldItemData: PropTypes.arrayOf(
 		PropTypes.any,
 	),
 	setFieldItemData: PropTypes.func.isRequired,
-	placeholder: PropTypes.string.isRequired,
 	isMulti: PropTypes.bool.isRequired,
 	limit: PropTypes.number.isRequired,
 	defaultIcon: PropTypes.string,
@@ -381,7 +351,6 @@ ListSelect.propTypes = {
 	showEditLink: PropTypes.bool.isRequired,
 	editIframeTitle: PropTypes.string,
 	readOnly: PropTypes.bool,
-	ajaxData: FIELD_PROP_TYPE.ajax_data,
 };
 
-export default ListSelect;
+export default ListSelectValues;

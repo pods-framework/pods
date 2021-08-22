e9fece492dd8c47386882894670d898134e826e9
Scott Kingsley Clark <lol@scottkclark.com>
2021-08-22 10:51:00 -0500

    Additional help info

--- src/Pods/Admin/Config/Pod.php
+++ src/Pods/Admin/Config/Pod.php
@@ -698,7 +698,7 @@ class Pod extends Base {
 				'post_type_built_in_taxonomies' => [
 					'name'          => 'post_type_built_in_taxonomies',
 					'label'         => __( 'Enable Connections to Taxonomy', 'pods' ),
-					'help'          => __( 'You can enable the ability to select terms from these Taxonomies on any post for this Post Type.', 'pods' ),
+					'help'          => __( 'You can enable the ability to select terms from these Taxonomies on any post for this Post Type. Once connected, posts from this Custom Post Type will appear in the Taxonomy archive page of the associated Taxonomies selected. Only Categories and Tag need to be specifically selected to be shown on Taxonomy archives on their own.', 'pods' ),
 					'type'          => 'boolean_group',
 					'boolean_group' => [],
 				],

6f200ab47fe3ecee2d91b94a6af7876a3dcabbcf
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-09-30 15:04:53 +0200

    add hook and unhook methods

--- src/Pods/Integrations/Polylang.php
+++ src/Pods/Integrations/Polylang.php
@@ -8,4 +8,20 @@ namespace Pods\Integrations;
  * @since 2.8.0
  */
 class Polylang {
+
+	/**
+	 * Add the class hooks.
+	 *
+	 * @since 2.8.0
+	 */
+	public function hook() {
+	}
+
+	/**
+	 * Remove the class hooks.
+	 *
+	 * @since 2.8.0
+	 */
+	public function unhook() {
+	}
 }

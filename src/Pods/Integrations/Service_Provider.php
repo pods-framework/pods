7ed8d07a4059a00065fcb7cb01424a59c7fe4436
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-09-30 19:49:43 +0200

    Enable i18n integration classes

--- src/Pods/Integrations/Service_Provider.php
+++ src/Pods/Integrations/Service_Provider.php
@@ -32,8 +32,34 @@ class Service_Provider extends tad_DI52_ServiceProvider {
 	 * @since 2.8.0
 	 */
 	protected function hooks() {
+
 		add_filter( 'pods_admin_config_pod_fields_post_type_supported_features', $this->container->callback( 'pods.integration.genesis', 'add_post_type_supports' ) );
 		add_filter( 'pods_admin_config_pod_fields_post_type_supported_features', $this->container->callback( 'pods.integration.yarpp', 'add_post_type_supports' ) );
 		add_filter( 'pods_admin_config_pod_fields_post_type_supported_features', $this->container->callback( 'pods.integration.jetpack', 'add_post_type_supports' ) );
+
+		if ( ! did_action( 'plugins_loaded' ) ) {
+			add_action( 'plugins_loaded', [ $this, 'plugins_loaded' ] );
+		} else {
+			$this->plugins_loaded();
+		}
+	}
+
+	/**
+	 * All plugins are loaded.
+	 *
+	 * @since 2.8.0
+	 */
+	public function plugins_loaded() {
+
+		if ( Polylang::is_active() ) {
+			$this->container->singleton( 'pods.integration.i18n', Polylang::class );
+		} elseif ( WPML::is_active() ) {
+			$this->container->singleton( 'pods.integration.i18n', WPML::class );
+		}
+
+		$i18n_hooks = $this->container->callback( 'pods.integration.i18n', 'hook' );
+		if ( is_callable( $i18n_hooks ) ) {
+			$i18n_hooks();
+		}
 	}
 }

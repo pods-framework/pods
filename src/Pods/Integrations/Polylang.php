fba65162382174f1de88184ece52776c746ef311
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-09-30 15:05:39 +0200

    Move PodsMeta PLL code to integration class

--- src/Pods/Integrations/Polylang.php
+++ src/Pods/Integrations/Polylang.php
@@ -15,6 +15,7 @@ class Polylang {
 	 * @since 2.8.0
 	 */
 	public function hook() {
+		add_action( 'pods_meta_init', [ $this, 'pods_meta_init' ] );
 	}
 
 	/**
@@ -23,5 +24,18 @@ class Polylang {
 	 * @since 2.8.0
 	 */
 	public function unhook() {
+		remove_action( 'pods_meta_init', [ $this, 'pods_meta_init' ] );
+	}
+
+	/**
+	 * @param \PodsMeta $pods_meta
+	 *
+	 * @since 2.8.0
+	 */
+	public function pods_meta_init( $pods_meta ) {
+
+		if ( function_exists( 'pll_current_language' ) ) {
+			add_action( 'init', array( $pods_meta, 'cache_pods' ), 101, 0 );
+		}
 	}
 }

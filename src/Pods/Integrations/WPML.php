e0a154188a7e0da53a708f7dac45cbb06de6f442
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-10-01 12:32:44 +0200

    Improve get table info

--- src/Pods/Integrations/WPML.php
+++ src/Pods/Integrations/WPML.php
@@ -158,7 +158,7 @@ class WPML extends Integration {
 	 * @since 2.8.0
 	 */
 	public function pods_api_get_table_info( $info, $object_type, $object, $name, $pod, $field, $pods_api ) {
-		global $wpdb, $sitepress;
+		global $wpdb;
 
 		if ( ! apply_filters( 'wpml_setting', true, 'auto_adjust_ids' ) ) {
 			return $info;
@@ -169,44 +169,46 @@ class WPML extends Integration {
 		// Get current language
 		$current_language = pods_i18n()->get_current_language();
 
+		$db_prefix = $wpdb->get_blog_prefix();
+
+		$wpml_translations = false;
+
 		switch ( $object_type ) {
 
 			case 'post':
 			case 'post_type':
 				if ( $this->is_translated_post_type( $object_name ) ) {
-					$info['join']['wpml_translations'] = "
-						LEFT JOIN `{$wpdb->prefix}icl_translations` AS `wpml_translations`
+					$wpml_translations = "
+						LEFT JOIN `{$db_prefix}icl_translations` AS `wpml_translations`
 							ON `wpml_translations`.`element_id` = `t`.`ID`
 								AND `wpml_translations`.`element_type` = 'post_" . pods_sanitize( $object_name ) . "'
 								AND `wpml_translations`.`language_code` = '" . pods_sanitize( $current_language ) . "'
 					";
-
-					$info['join']['wpml_languages'] = "
-						LEFT JOIN `{$wpdb->prefix}icl_languages` AS `wpml_languages`
-							ON `wpml_languages`.`code` = `wpml_translations`.`language_code` AND `wpml_languages`.`active` = 1
-					";
-
-					$info['where']['wpml_languages'] = "`wpml_languages`.`code` IS NOT NULL";
 				}
 				break;
 
 			case 'taxonomy':
 				if ( $this->is_translated_taxonomy( $object_name ) ) {
-					$info['join']['wpml_translations'] = "
-						LEFT JOIN `{$wpdb->prefix}icl_translations` AS `wpml_translations`
+					$wpml_translations = "
+						LEFT JOIN `{$db_prefix}icl_translations` AS `wpml_translations`
 							ON `wpml_translations`.`element_id` = `tt`.`term_taxonomy_id`
 								AND `wpml_translations`.`element_type` = 'tax_" . pods_sanitize( $object_name ) . "'
 								AND `wpml_translations`.`language_code` = '" . pods_sanitize( $current_language ) . "'
 					";
-
-					$info['join']['wpml_languages'] = "
-						LEFT JOIN `{$wpdb->prefix}icl_languages` AS `wpml_languages`
-							ON `wpml_languages`.`code` = `wpml_translations`.`language_code` AND `wpml_languages`.`active` = 1
-					";
-
-					$info['where']['wpml_languages'] = "`wpml_languages`.`code` IS NOT NULL";
 				}
 				break;
+		}
+
+		if ( $wpml_translations ) {
+
+			$info['join']['wpml_translations'] = $wpml_translations;
+
+			$info['join']['wpml_languages'] = "
+				LEFT JOIN `{$db_prefix}icl_languages` AS `wpml_languages`
+					ON `wpml_languages`.`code` = `wpml_translations`.`language_code` AND `wpml_languages`.`active` = 1
+			";
+
+			$info['where']['wpml_languages'] = "`wpml_languages`.`code` IS NOT NULL";
 		}
 
 		return $info;

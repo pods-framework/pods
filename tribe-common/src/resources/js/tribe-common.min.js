42e6eb81598b84687fd852cb769fa550d29f4dd1
Scott Kingsley Clark <lol@scottkclark.com>
2021-10-15 18:42:45 -0500

    Remove unnecessary files from tribe-common

--- tribe-common/src/resources/js/tribe-common.min.js
+++
@@ -1,1 +0,0 @@
-String.prototype.className=function(){return"string"!=typeof this&&!this instanceof String||"function"!=typeof this.replace?this:this.replace(".","")},String.prototype.varName=function(){return"string"!=typeof this&&!this instanceof String||"function"!=typeof this.replace?this:this.replace("-","_")};var tribe=tribe||{};

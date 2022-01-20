52df37761ff94b707ab0969d8fda87cd447a93b9
Scott Kingsley Clark <lol@scottkclark.com>
2022-01-20 17:49:09 -0600

    Pods Help page revamp

--- ui/admin/help.php
+++ ui/admin/help.php
@@ -1,12 +1,10 @@
 <div class="wrap pods-admin pods-admin--flex">
-	<div class="pods-admin__content-container">
+	<div class="pods-admin-container pods-admin__content-container">
 		<div id="icon-pods" class="icon32"><br /></div>
-		<h2><?php _e( 'Pods Help', 'pods' ); ?></h2>
+		<h2><?php _e( 'Get help with Pods', 'pods' ); ?></h2>
 		<img src="<?php echo esc_url( PODS_URL ); ?>ui/images/pods-logo-notext-rgb-transparent.png" class="pods-leaf-watermark-right" />
 
-		<h3><?php _e( 'Pods Support' , 'pods'); ?></h3>
-
-		<p><?php _e( 'There are many resources available to help you learn to use Pods on our site, <a href="https://pods.io/" target="_blank" rel="noopener noreferrer">Pods.io</a>:', 'pods'); ?></p>
+		<p><?php _e( 'There are many resources available to help you learn to use Pods <a href="https://pods.io/" target="_blank" rel="noopener noreferrer">on our website</a>', 'pods'); ?>.</p>
 		<ul class="ul-disc">
 			<li><?php _e('To learn more about using Pods, see our <a href="https://docs.pods.io/" target="_blank" rel="noopener noreferrer">documentation</a> and subscribe to our <a href="https://www.youtube.com/user/podsframework/" target="_blank" rel="noopener noreferrer">YouTube Channel</a>', 'pods'); ?>.
 
@@ -14,104 +12,175 @@
 
 			<li><?php _e('To report <strong>bugs or request features</strong>, go to our <a href="https://github.com/pods-framework/pods/issues?sort=updated&direction=desc&state=open" target="_blank" rel="noopener noreferrer">GitHub</a>.', 'pods' ); ?></li>
 
-			<li><?php _e( "Pods is open source, so if you want to get into the code and submit your own fixes or features, go at it, we'd love to have you contribute on our project! With GitHub, it's really easy to contribute back, so why not give it a try?", 'pods'); ?></li>
+			<li><?php _e( 'Pods is open source, so you can get into the code and submit your own fixes or features. We would love to help you contribute on our project over on our <a href="https://github.com/pods-framework/pods/blob/main/docs/CONTRIBUTING.md" target="_blank" rel="noopener noreferrer">GitHub</a>', 'pods'); ?>.</li>
 		</ul>
 
 		<hr />
 
-		<?php
-			include_once( ABSPATH . WPINC . '/feed.php' );
-
-			$feed = fetch_feed( 'https://wordpress.org/support/plugin/pods/feed/' );
-
-			if ( !is_wp_error( $feed ) ) {
-				$max_items = $feed->get_item_quantity( 10 );
-				$rss_items = $feed->get_items( 0, $max_items );
-
-				if ( 0 < $max_items ) {
-		?>
-			<h3><?php _e( 'Latest Forum Posts at <a href="https://wordpress.org/support/plugin/pods" target="_blank" rel="noopener noreferrer">wordpress.org</a>', 'pods'); ?></h3>
-
-			<ul class="ul-disc">
-				<?php
-					foreach ( $rss_items as $item ) {
-						$authors = $item->get_authors();
-
-						$author_text = '';
-
-						foreach ( $authors as $author ) {
-							$author_text = '<br /> by ';
-
-							if ( !empty( $author->link ) )
-								$author_text .= '<a href="' . $author->link . '" target="_blank" rel="noopener noreferrer">';
-
-							$author_text .= $author->name;
-
-							if ( !empty( $author->link ) )
-								$author_text .= '</a>';
-						}
-				?>
-					<li>
-						<a href="<?php echo esc_url( $item->get_permalink() ); ?>"><?php echo html_entity_decode( esc_html( $item->get_title() ) ); ?></a>
-						<?php echo $author_text; ?>
-						<br />
-						on <?php echo $item->get_date( 'm/d/Y' ); ?>
-					</li>
-				<?php
-					}
-				?>
-			</ul>
-
-			<hr />
-		<?php
-				}
+		<style>
+			.pods-admin-help-info {
+				max-width: 800px;
+			}
+			.pods-admin-help-info td {
+				vertical-align: middle;
 			}
 
-			$feed = fetch_feed( 'https://github.com/pods-framework/pods/commits/2.x.atom' );
-
-			if ( !is_wp_error( $feed ) ) {
-				// Silence warnings due to a bug in SimplePie's date parser with PHP 7.1+
-				// See: https://github.com/pods-framework/pods/issues/4363
-				$max_items = @$feed->get_item_quantity( 10 );
-				$rss_items = $feed->get_items( 0, $max_items );
-
-				if ( 0 < $max_items ) {
-		?>
-			<h3><?php _e( 'Latest Activity on <a href="http://github.com/pods-framework/pods" target="_blank" rel="noopener noreferrer">GitHub</a>', 'pods' ); ?></h3>
-
-			<ul class="ul-disc">
-				<?php
-					foreach ( $rss_items as $item ) {
-						$authors = $item->get_authors();
-
-						$author_text = '';
-
-						foreach ( $authors as $author ) {
-							$author_text = '<br /> by ';
-
-							if ( !empty( $author->link ) )
-								$author_text .= '<a href="' . $author->link . '" target="_blank" rel="noopener noreferrer">';
-
-							$author_text .= $author->name;
-
-							if ( !empty( $author->link ) )
-								$author_text .= '</a>';
-						}
-				?>
-					<li>
-						<a href="<?php echo esc_url( $item->get_permalink() ); ?>">Commit</a>: <?php echo esc_html( $item->get_title() ); ?>
-						<?php echo $author_text; ?>
-						<br />
-						on <?php echo $item->get_date( 'm/d/Y' ); ?>
-					</li>
-				<?php
-					}
-				?>
-			</ul>
-		<?php
-				}
+			.pods-admin-help-info thead tr th:nth-child(1),
+			.pods-admin-help-info tbody tr td:nth-child(1) {
+				width: 50px;
 			}
-		?>
+			.pods-admin-help-info thead tr th:nth-child(2),
+			.pods-admin-help-info tbody tr td:nth-child(2) {
+				width: 300px;
+			}
+		</style>
+
+		<table class="pods-admin-help-info widefat striped fixed">
+			<thead>
+				<tr>
+					<th></th>
+					<th><?php esc_html_e( 'Plugin', 'pods' ); ?></th>
+					<th><?php esc_html_e( 'Links', 'pods' ); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods Framework</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						| <a href="https://docs.pods.io/" title="Documentation">Docs</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<h2>Free Add-Ons</h2>
+		<table class="pods-admin-help-info widefat">
+			<thead>
+				<tr>
+					<th></th>
+					<th><?php esc_html_e( 'Plugin', 'pods' ); ?></th>
+					<th><?php esc_html_e( 'Links', 'pods' ); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-beaver-builder-themer-add-on/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods-beaver-builder-themer-add-on/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="Pods Beaver Themer Add-On" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-beaver-builder-themer-add-on/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods Beaver Themer Add-On</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods-beaver-builder-themer-add-on.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods-beaver-builder-themer-add-on/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						| <a href="https://docs.pods.io/plugins/pods-beaver-themer-add-on/" title="Documentation">Docs</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods-beaver-builder-themer-add-on" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-gravity-forms/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods-gravity-forms/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-gravity-forms/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods Gravity Forms Add-on</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods-gravity-forms.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods-gravity-forms/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						| <a href="https://docs.pods.io/plugins/pods-gravity-forms-add-on/" title="Documentation">Docs</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods-gravity-forms" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-alternative-cache/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods-alternative-cache/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="Pods Alternative Cache" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-alternative-cache/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods Alternative Cache</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods-alternative-cache.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods-alternative-cache/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						| <a href="https://docs.pods.io/plugins/pods-alternative-cache/" title="Documentation">Docs</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods-alternative-cache" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-seo/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods-seo/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="Pods SEO" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-seo/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods SEO</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods-seo.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods-seo/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods-seo" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-ajax-views/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">
+							<img width="50" height="50" src="https://ps.w.org/pods-ajax-views/assets/icon-256x256.png" class="attachment-thumbnail size-thumbnail" alt="" loading="lazy" >
+						</a>
+					</td>
+					<td>
+						<a href="https://wordpress.org/plugins/pods-ajax-views/" target="_blank" rel="noopener noreferrer" title="View plugin on WordPress.org">Pods AJAX Views</a>
+					</td>
+					<td>
+						<a href="https://downloads.wordpress.org/plugin/pods-ajax-views.zip" class="small-button" target="_blank" rel="noopener noreferrer">Download</a> |
+						<a href="https://wordpress.org/support/plugin/pods-ajax-views/" class="small-button" target="_blank" rel="noopener noreferrer" title="Support Forums">Support</a>
+
+						|
+						<a href="https://github.com/pods-framework/pods-ajax-views" class="small-button" target="_blank" rel="noopener noreferrer">GitHub</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="note">
+			<h3>Are you looking for help with Pods Pro by SKCDEV?</h3>
+			<p>Pods Pro is separate from Pods. You can get support on the
+				<a href="https://pods-pro.skc.dev/" target="_blank" rel="noopener noreferrer">Pods Pro by SKCDEV site</a>
+				by logging in and going to your Dashboard area.
+			</p>
+		</div>
 	</div>
 
 	<?php
@@ -122,4 +191,5 @@
 	 */
 	do_action( 'pods_admin_after_help' );
 	?>
+
 </div>

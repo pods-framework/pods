46cce310628eb1886ae0c57595efc11eccf11eb8
Scott Kingsley Clark <scott@skc.dev>
2022-07-03 12:41:47 -0500

    Help ensure custom heading tags are locked down more

--- ui/forms/list-row.php
+++ ui/forms/list-row.php
@@ -13,10 +13,10 @@
 	style="<?php echo esc_attr( 'hidden' == $field['type'] ? 'display:none;' : '' ); ?>">
 	<?php if ( 'heading' === $field['type'] ) : ?>
 		<?php $heading_tag = pods_v( $field['type'] . '_tag', $field, isset( $heading_tag ) ? $heading_tag : 'h2', true ); ?>
-		<<?php echo esc_html( $heading_tag ); ?>
+		<<?php echo esc_html( sanitize_key( $heading_tag ) ); ?>
 			class="pods-form-ui-heading pods-form-ui-heading-<?php echo esc_attr( $field['name'] ); ?>">
 			<?php echo esc_html( $field['label'] ); ?>
-		</<?php echo esc_html( $heading_tag ); ?>>
+		</<?php echo esc_html( sanitize_key( $heading_tag ) ); ?>>
 		<?php echo PodsForm::comment( $field_prefix . $field['name'], pods_v( 'description', $field ), $field ); ?>
 	<?php elseif ( 'html' === $field['type'] && 1 === (int) $field['html_no_label'] ) : ?>
 		<?php echo PodsForm::field( $field_prefix . $field['name'], $value, $field['type'], $field, $pod, $id ); ?>

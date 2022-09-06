1b2d41de5c59a6f964653f593aecd3097c522257
Zack Rothauser <zrothauser@gmail.com>
2022-09-05 19:36:00 -0500

    Finish basic conditional logic hook, with tests. Does not yet check for parent/grandparent field logic, and needs to be put into the FieldWrapper component

--- ui/js/dfv/src/hooks/test/useConditionalLogic.js
+++ ui/js/dfv/src/hooks/test/useConditionalLogic.js
@@ -32,15 +32,9 @@ const generateFieldConfigWithConditionalRules = (
 	},
 );
 
-const BASE_ALL_POD_VALUES = {
-	'some-company': '',
-	'some-name': '',
-};
-
-const BASE_ALL_POD_FIELDS_MAP = new Map( [
-	[ 'some-company', generateFieldConfig( 'some-company' ) ],
-	[ 'some-name', generateFieldConfig( 'some-name' ) ],
-] );
+const generateAllPodFieldsMap = ( fieldNames = [] ) => new Map(
+	fieldNames.map( ( fieldName ) => [ fieldName, generateFieldConfig( fieldName ) ] ),
+);
 
 describe( 'conditional logic validation hook', () => {
 	it( 'should pass if conditional logic is disabled', () => {
@@ -54,8 +48,8 @@ describe( 'conditional logic validation hook', () => {
 		const { result } = renderHook(
 			() => useConditionalLogic(
 				fieldConfig,
-				BASE_ALL_POD_VALUES,
-				BASE_ALL_POD_FIELDS_MAP,
+				{},
+				generateAllPodFieldsMap(),
 			)
 		);
 
@@ -73,8 +67,8 @@ describe( 'conditional logic validation hook', () => {
 		const { result } = renderHook(
 			() => useConditionalLogic(
 				fieldConfig,
-				BASE_ALL_POD_VALUES,
-				BASE_ALL_POD_FIELDS_MAP,
+				{},
+				generateAllPodFieldsMap(),
 			)
 		);
 
@@ -92,8 +86,8 @@ describe( 'conditional logic validation hook', () => {
 		const { result } = renderHook(
 			() => useConditionalLogic(
 				fieldConfig,
-				BASE_ALL_POD_VALUES,
-				BASE_ALL_POD_FIELDS_MAP,
+				{},
+				generateAllPodFieldsMap(),
 			)
 		);
 
@@ -111,51 +105,296 @@ describe( 'conditional logic validation hook', () => {
 		const { result } = renderHook(
 			() => useConditionalLogic(
 				fieldConfig,
-				BASE_ALL_POD_VALUES,
-				BASE_ALL_POD_FIELDS_MAP,
+				{},
+				generateAllPodFieldsMap(),
 			)
 		);
 
 		expect( result.current ).toBe( false );
 	} );
 
-	it( 'should validate rules with "any" logic', () => {} );
+	it( 'should validate rules with "any" logic', () => {
+		const fieldConfig = generateFieldConfigWithConditionalRules(
+			true,
+			'show',
+			'any',
+			[
+				{
+					field: 'field_one',
+					compare: '=',
+					value: '12345',
+				},
+				{
+					field: 'field_two',
+					compare: '=',
+					value: '1234567890',
+				},
+				{
+					field: 'field_three',
+					compare: '=',
+					value: 'abcdef',
+				},
+			]
+		);
 
-	it( 'should validate rules with "all" logic', () => {} );
+		const fieldsMap = generateAllPodFieldsMap( [ 'field_one', 'field_two', 'field_three' ] );
 
-	it( 'should validate "LIKE" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: '12345' }, fieldsMap )
+			).result.current
+		).toBe( true );
 
-	it( 'should validate "NOT LIKE" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: '12345', field_two: '1234567890' }, fieldsMap )
+			).result.current
+		).toBe( true );
 
-	it( 'should validate "BEGINS" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: 'abcdef', field_two: '1234567890' }, fieldsMap )
+			).result.current
+		).toBe( true );
 
-	it( 'should validate "NOT BEGINS" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: 'abcdef' }, fieldsMap )
+			).result.current
+		).toBe( false );
+	} );
 
-	it( 'should validate "ENDS" rules', () => {} );
+	it( 'should validate rules with "all" logic', () => {
+		const fieldConfig = generateFieldConfigWithConditionalRules(
+			true,
+			'show',
+			'all',
+			[
+				{
+					field: 'field_one',
+					compare: '=',
+					value: '12345',
+				},
+				{
+					field: 'field_two',
+					compare: '=',
+					value: '1234567890',
+				},
+				{
+					field: 'field_three',
+					compare: '=',
+					value: 'abcdef',
+				},
+			]
+		);
 
-	it( 'should validate "NOT ENDS" rules', () => {} );
+		const fieldsMap = generateAllPodFieldsMap( [ 'field_one', 'field_two', 'field_three' ] );
 
-	it( 'should validate "MATCHES" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: '12345' }, fieldsMap )
+			).result.current
+		).toBe( false );
 
-	it( 'should validate "NOT MATCHES" rules', () => {} );
+		expect(
+			renderHook(
+				() => useConditionalLogic( fieldConfig, { field_one: '12345', field_two: '1234567890', field_three: 'abcdef' }, fieldsMap )
+			).result.current
+		).toBe( true );
+	} );
 
-	it( 'should validate "IN" rules', () => {} );
+	const individualRulesMatrix = [
+		[ 'like', 'word', 'word', true ],
+		[ 'like', 'word', 'word2', true ],
+		[ 'like', 'word', '1word', true ],
+		[ 'like', 'word', 'Word', true ],
+		[ 'like', 'word', 'WORD', true ],
+		[ 'like', 'word', 'sentence with word in it', true ],
+		[ 'like', 'word', 'word starts sentence', true ],
+		[ 'like', 'word', 'sentence ends with word', true ],
+		[ 'like', 'word', 'wor d', false ],
+		[ 'like', 'word', 'sentence without that in it', false ],
 
-	it( 'should validate "NOT IN" rules', () => {} );
+		[ 'not like', 'word', 'wor d', true ],
+		[ 'not like', 'word', 'sentence without that in it', true ],
+		[ 'not like', 'word', 'word', false ],
+		[ 'not like', 'word', 'word2', false ],
+		[ 'not like', 'word', '1word', false ],
+		[ 'not like', 'word', 'Word', false ],
+		[ 'not like', 'word', 'WORD', false ],
+		[ 'not like', 'word', 'sentence with word in it', false ],
+		[ 'not like', 'word', 'word starts sentence', false ],
+		[ 'not like', 'word', 'sentence ends with word', false ],
 
-	it( 'should validate "EMPTY" rules', () => {} );
+		[ 'begins', 'word', 'word', true ],
+		[ 'begins', 'word', 'word', true ],
+		[ 'begins', 'word', 'word2', true ],
+		[ 'begins', 'word', 'Word', true ],
+		[ 'begins', 'word', 'WORD', true ],
+		[ 'begins', 'word', 'word starts sentence', true ],
+		[ 'begins', 'word', '1word', false ],
+		[ 'begins', 'word', 'sentence with word in it', false ],
+		[ 'begins', 'word', 'sentence ends with word', false ],
+		[ 'begins', 'word', 'wor d', false ],
+		[ 'begins', 'word', 'sentence without that in it', false ],
 
-	it( 'should validate "NOT EMPTY" rules', () => {} );
+		[ 'not begins', 'word', '1word', true ],
+		[ 'not begins', 'word', 'sentence with word in it', true ],
+		[ 'not begins', 'word', 'sentence ends with word', true ],
+		[ 'not begins', 'word', 'wor d', true ],
+		[ 'not begins', 'word', 'sentence without that in it', true ],
+		[ 'not begins', 'word', 'word', false ],
+		[ 'not begins', 'word', 'word2', false ],
+		[ 'not begins', 'word', 'Word', false ],
+		[ 'not begins', 'word', 'WORD', false ],
+		[ 'not begins', 'word', 'word starts sentence', false ],
 
-	it( 'should validate "=" rules', () => {} );
+		[ 'ends', 'word', '1word', true ],
+		[ 'ends', 'word', 'sentence ends with word', true ],
+		[ 'ends', 'word', 'word', true ],
+		[ 'ends', 'word', 'Word', true ],
+		[ 'ends', 'word', 'WORD', true ],
+		[ 'ends', 'word', 'sentence with word in it', false ],
+		[ 'ends', 'word', 'wor d', false ],
+		[ 'ends', 'word', 'sentence without that in it', false ],
+		[ 'ends', 'word', 'word2', false ],
+		[ 'ends', 'word', 'word starts sentence', false ],
 
-	it( 'should validate "!=" rules', () => {} );
+		[ 'not ends', 'word', 'sentence with word in it', true ],
+		[ 'not ends', 'word', 'wor d', true ],
+		[ 'not ends', 'word', 'sentence without that in it', true ],
+		[ 'not ends', 'word', 'word2', true ],
+		[ 'not ends', 'word', 'word starts sentence', true ],
+		[ 'not ends', 'word', '1word', false ],
+		[ 'not ends', 'word', 'sentence ends with word', false ],
+		[ 'not ends', 'word', 'word', false ],
+		[ 'not ends', 'word', 'Word', false ],
+		[ 'not ends', 'word', 'WORD', false ],
 
-	it( 'should validate "<" rules', () => {} );
+		[ 'matches', '^[a-z]+$', 'onlyletters', true ],
+		[ 'matches', '^[a-z]+$', 'letters with spaces', false ],
+		[ 'matches', '^[a-z]+$', 'letterswithnumberslike12345', false ],
+		[ 'matches', '^[a-z]+$', 'lettersWithUpperCase', false ],
 
-	it( 'should validate "<=" rules', () => {} );
+		[ 'not matches', '^[a-z]+$', 'letters with spaces', true ],
+		[ 'not matches', '^[a-z]+$', 'letterswithnumberslike12345', true ],
+		[ 'not matches', '^[a-z]+$', 'lettersWithUpperCase', true ],
+		[ 'not matches', '^[a-z]+$', 'onlyletters', false ],
 
-	it( 'should validate ">" rules', () => {} );
+		[ 'in', [ '123456', '7890' ], 123456, true ],
+		[ 'in', [ '123456', '7890' ], '123456', true ],
+		[ 'in', [ '123456', '7890' ], 7890, true ],
+		[ 'in', [ '123456', '7890' ], '7890', true ],
+		[ 'in', [ '123456', '7890' ], 1234, false ],
+		[ 'in', [ '123456', '7890' ], 'some other value', false ],
+		[ 'in', [ '123456', '7890' ], [ '123456' ], false ],
 
-	it( 'should validate ">=" rules', () => {} );
+		[ 'not in', [ '123456', '7890' ], 1234, true ],
+		[ 'not in', [ '123456', '7890' ], 'some other value', true ],
+		[ 'not in', [ '123456', '7890' ], [ '123456' ], true ],
+		[ 'not in', [ '123456', '7890' ], 123456, false ],
+		[ 'not in', [ '123456', '7890' ], '123456', false ],
+		[ 'not in', [ '123456', '7890' ], 7890, false ],
+		[ 'not in', [ '123456', '7890' ], '7890', false ],
+
+		[ 'empty', '', '', true ],
+		[ 'empty', '', null, true ],
+		[ 'empty', '', [], true ],
+		[ 'empty', '', false, true ],
+		[ 'empty', '', 'some value', false ],
+		[ 'empty', '', true, false ],
+		[ 'empty', '', 0, false ],
+		[ 'empty', '', 1, false ],
+		[ 'empty', '', '0', false ],
+		[ 'empty', '', 'null', false ],
+		[ 'empty', '', '[]', false ],
+		[ 'empty', '', 'false', false ],
+
+		[ 'not empty', '', 'some value', true ],
+		[ 'not empty', '', true, true ],
+		[ 'not empty', '', 0, true ],
+		[ 'not empty', '', 1, true ],
+		[ 'not empty', '', '0', true ],
+		[ 'not empty', '', 'null', true ],
+		[ 'not empty', '', '[]', true ],
+		[ 'not empty', '', 'false', true ],
+		[ 'not empty', '', '', false ],
+		[ 'not empty', '', null, false ],
+		[ 'not empty', '', [], false ],
+		[ 'not empty', '', false, false ],
+
+		[ '=', '123456', 123456, true ],
+		[ '=', '123456', '123456', true ],
+		[ '=', '123456', 1234, false ],
+		[ '=', '123456', 'some other value', false ],
+		[ '=', '123456', [ '123456' ], false ],
+
+		[ '!=', '123456', 1234, true ],
+		[ '!=', '123456', 'some other value', true ],
+		[ '!=', '123456', [ '123456' ], true ],
+		[ '!=', '123456', 123456, false ],
+		[ '!=', '123456', '123456', false ],
+
+		[ '<', '123456', 123457, true ],
+		[ '<', '123456', '123457', true ],
+		[ '<', '123456', 123455, false ],
+		[ '<', '123456', 123456, false ],
+		[ '<', '123456', 1234, false ],
+		[ '<', '123456', [ 123457 ], false ],
+
+		[ '<=', '123456', 123457, true ],
+		[ '<=', '123456', '123457', true ],
+		[ '<=', '123456', 123456, true ],
+		[ '<=', '123456', '123456', true ],
+		[ '<=', '123456', 123455, false ],
+		[ '<=', '123456', 1234, false ],
+		[ '<=', '123456', [ 123457 ], false ],
+
+		[ '>', '123456', 123455, true ],
+		[ '>', '123456', '123455', true ],
+		[ '>', '123456', 1234, true ],
+		[ '>', '123456', '1234', true ],
+		[ '>', '123456', 123457, false ],
+		[ '>', '123456', 123456, false ],
+		[ '>', '123456', [ 123455 ], false ],
+
+		[ '>=', '123456', 123455, true ],
+		[ '>=', '123456', '123455', true ],
+		[ '>=', '123456', 123456, true ],
+		[ '>=', '123456', '123456', true ],
+		[ '>=', '123456', 1234, true ],
+		[ '>=', '123456', '1234', true ],
+		[ '>=', '123456', 123457, false ],
+		[ '>=', '123456', [ 123455 ], false ],
+	];
+
+	it.each( individualRulesMatrix )(
+		'should validate passing and nonpassing rules: rule %p, rule value %p, value %p, should match %p',
+		( rule, ruleValue, testSubject, expected ) => {
+			const fieldConfig = generateFieldConfigWithConditionalRules(
+				true,
+				'show',
+				'any',
+				[
+					{
+						field: 'field_one',
+						compare: rule,
+						value: ruleValue,
+					},
+				]
+			);
+
+			const fieldsMap = generateAllPodFieldsMap( [ 'field_one' ] );
+
+			expect(
+				renderHook(
+					() => useConditionalLogic( fieldConfig, { field_one: testSubject }, fieldsMap )
+				).result.current
+			).toBe( expected );
+		}
+	);
+
+	it( 'should validate based on parent field conditional logic', () => {} );
+
+	it( 'should validate based on grandparent field conditional logic', () => {} );
 } );
--- ui/js/dfv/src/hooks/useConditionalLogic.js
+++ ui/js/dfv/src/hooks/useConditionalLogic.js
@@ -1,5 +1,142 @@
 import { toBool } from 'dfv/src/helpers/booleans';
-// import { validateFieldDependencies, formatDependency } from 'dfv/src/helpers/validateFieldDependencies';
+
+/**
+ * Helper function to compare values of differing items, which allows strings
+ * to match numbers.
+ *
+ * Comparing an array of 1 item could create false positives, because
+ * `[ '123' ].toString() === '123'`, so compare objects (usually arrays)
+ * without using toString().
+ *
+ * @param {any} item1 First item to compare.
+ * @param {any} item2 Second item to compare.
+ *
+ * @return {boolean} True if matches.
+ */
+const looseStringEqualityCheck = ( item1, item2 ) => {
+	if (
+		'object' === typeof ( item1 ) || 'object' === typeof ( item2 )
+	) {
+		return JSON.stringify( item1 ) === JSON.stringify( item2 );
+	}
+
+	return item1.toString() === item2.toString();
+};
+
+/**
+ * Helper function to validate values for conditional logic.
+ *
+ * @param {string} rule        Any of the possible conditional rules: 'like', 'not like',
+ *                             'begins', 'not begins', 'ends', 'not ends', 'matches', 'not matches',
+ *                             'in', 'not in', 'empty', 'not empty', '=', '!=', '<', '<=', '>', '>='.
+ * @param {string} ruleValue   The value to compare against.
+ * @param {string} valueToTest The value to be tested.
+ *
+ * @return {boolean} True if the test passes.
+ */
+const validateConditionalValue = ( rule, ruleValue, valueToTest ) => {
+	// Bail if the current value is not set at all.
+	if ( 'undefined' === typeof rule ) {
+		return false;
+	}
+
+	switch ( rule ) {
+		case 'like':
+			return ( valueToTest.toLowerCase() ).includes( ruleValue.toLowerCase() );
+		case 'not like':
+			return ! ( valueToTest.toLowerCase() ).includes( ruleValue.toLowerCase() );
+		case 'begins':
+			return ( valueToTest.toLowerCase() ).startsWith( ruleValue.toLowerCase() );
+		case 'not begins':
+			return ! ( valueToTest.toLowerCase() ).startsWith( ruleValue.toLowerCase() );
+		case 'ends':
+			return valueToTest.toLowerCase().endsWith( ruleValue.toLowerCase() );
+		case 'not ends':
+			return ! valueToTest.toLowerCase().endsWith( ruleValue.toLowerCase() );
+		case 'matches':
+			return valueToTest.match( ruleValue );
+		case 'not matches':
+			return ! valueToTest.match( ruleValue );
+		case 'in': {
+			// We can't compare 'in' if the rule's value is not an array.
+			if ( ! Array.isArray( ruleValue ) ) {
+				return false;
+			}
+
+			return ruleValue.some(
+				( ruleValueItem ) => looseStringEqualityCheck( ruleValueItem, valueToTest )
+			);
+		}
+		case 'not in': {
+			// We can't compare 'not in' if the rule's value is not an array.
+			if ( ! Array.isArray( ruleValue ) ) {
+				return false;
+			}
+
+			return ! ruleValue.some(
+				( ruleValueItem ) => looseStringEqualityCheck( ruleValueItem, valueToTest )
+			);
+		}
+		case 'empty': {
+			if ( Array.isArray( valueToTest ) ) {
+				return valueToTest.length === 0;
+			} else if ( [ '0', 0 ].includes( valueToTest ) ) {
+				// The string '0' and '0' are not considered "empty".
+				return false;
+			}
+
+			return ! Boolean( valueToTest );
+		}
+		case 'not empty': {
+			if ( Array.isArray( valueToTest ) ) {
+				return valueToTest.length > 0;
+			} else if ( 0 === valueToTest ) {
+				// The integer 0 is considered "not empty".
+				return true;
+			}
+
+			return Boolean( valueToTest );
+		}
+		case '=':
+			return looseStringEqualityCheck( ruleValue, valueToTest );
+		case '!=':
+			return ! looseStringEqualityCheck( ruleValue, valueToTest );
+		case '<': {
+			// Don't compare arrays.
+			if ( Array.isArray( ruleValue ) || Array.isArray( valueToTest ) ) {
+				return false;
+			}
+
+			return Number( ruleValue ) < Number( valueToTest );
+		}
+		case '<=': {
+			// Don't compare arrays.
+			if ( Array.isArray( ruleValue ) || Array.isArray( valueToTest ) ) {
+				return false;
+			}
+
+			return Number( ruleValue ) <= Number( valueToTest );
+		}
+		case '>': {
+			// Don't compare arrays.
+			if ( Array.isArray( ruleValue ) || Array.isArray( valueToTest ) ) {
+				return false;
+			}
+
+			return Number( ruleValue ) > Number( valueToTest );
+		}
+		case '>=': {
+			// Don't compare arrays.
+			if ( Array.isArray( ruleValue ) || Array.isArray( valueToTest ) ) {
+				return false;
+			}
+
+			return Number( ruleValue ) >= Number( valueToTest );
+		}
+		default:
+			return false;
+	}
+};
 
 const recursiveCheckConditionalLogicForField = (
 	fieldConfig,
@@ -20,7 +157,58 @@ const recursiveCheckConditionalLogicForField = (
 		return true;
 	}
 
-	return true;
+	// No need to go through rules if the array is empty.
+	if ( 0 === rules.length ) {
+		return ( 'show' === action ) ? true : false;
+	}
+
+	// If logic is set to 'any', we just need to find the first rule that matches.
+	// If logic is set to 'all', we need to go through each rule.
+	let meetsRules = false;
+
+	const rulesCallback = ( rule ) => {
+		const {
+			compare,
+			value: ruleValue,
+			field: fieldToTest,
+		} = rule;
+
+		// Return if the rule is invalid.
+		if ( ! compare || ! fieldToTest ) {
+			return true;
+		}
+
+		const valueToTest = allPodValues[ fieldToTest ];
+
+		// If the value to test is not set, then it can't pass.
+		if ( 'undefined' === typeof valueToTest ) {
+			return false;
+		}
+
+		return validateConditionalValue(
+			compare,
+			ruleValue,
+			valueToTest,
+		);
+	};
+
+	if ( 'all' === logic ) {
+		meetsRules = rules.every( rulesCallback );
+	} else {
+		meetsRules = rules.some( rulesCallback );
+	}
+
+	// @todo
+	// Go up the tree of dependencies. This works two different ways:
+	// parents from a 'depends-on-any' match should have at least one
+	// where the whole tree passes. For the other types, all parents need to match.
+
+	// Inverse the result if the action is 'hide' instead of 'show'.
+	if ( 'hide' === action ) {
+		meetsRules = ! meetsRules;
+	}
+
+	return meetsRules;
 };
 
 const useConditionalLogic = (

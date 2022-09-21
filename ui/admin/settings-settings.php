30ae636d094686426c672f442c820bad989a47a1
Scott Kingsley Clark <scott@skc.dev>
2022-09-20 21:37:24 -0500

    Adjust clear pods cache help text

--- ui/admin/settings-settings.php
+++ ui/admin/settings-settings.php
@@ -83,7 +83,7 @@ $do = 'save';
 
 <h3><?php _e( 'Clear Pods Cache', 'pods' ); ?></h3>
 
-<p><?php esc_html_e( 'This tool will clear all of the transients and object cache that are used by Pods and your site.', 'pods' ); ?></p>
+<p><?php esc_html_e( 'This will clear all of the transients and object cache that are used by Pods and your site.', 'pods' ); ?></p>
 
 <p class="submit">
 	<input type="submit" class="button button-primary" name="pods_cache_flush" value="<?php esc_attr_e( 'Clear Pods Cache', 'pods' ); ?>" />

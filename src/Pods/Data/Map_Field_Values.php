fb56c5c5b241a63de085968950769d752b3d90f7
Scott Kingsley Clark <scott@skc.dev>
2023-03-05 22:59:14 -0600

    Fix handling for dot notation on attachments/post_thumbnail
    Fixes #7005

--- src/Pods/Data/Map_Field_Values.php
+++ src/Pods/Data/Map_Field_Values.php
@@ -598,8 +598,6 @@ class Map_Field_Values {
 		if ( isset( $traverse_params[0] ) ) {
 			$size = $traverse_params[0];
 
-			array_shift( $traverse_params );
-
 			if ( pods_is_image_size( $size ) ) {
 				// Force image request since a valid size parameter is passed.
 				$is_image = true;

7d59e27150bca07611d1af5cf39bd147410b5cd1
Scott Kingsley Clark <scott@skc.dev>
2023-04-25 11:40:43 -0500

    Fix for email validation always showing as invalid

--- ui/js/dfv/src/helpers/validators.js
+++ ui/js/dfv/src/helpers/validators.js
@@ -55,7 +55,7 @@ export const minValidator = ( minValue ) => ( value ) => {
 export const emailValidator = () => ( value ) => {
 	const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	if ( ! value || ! value.match( EMAIL_REGEX ) ) {
+	if ( value && ! value.match( EMAIL_REGEX ) ) {
 		throw __( 'Invalid email address format.', 'pods' );
 	}
 

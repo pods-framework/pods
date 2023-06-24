042aeeacabfa3ce8e9a270c7f181f19137839cfa
Scott Kingsley Clark <scott@skc.dev>
2023-06-23 22:40:22 -0500

    Remove tribe-common folder

--- tribe-common/src/deprecated/Tribe__Events__Abstract_Deactivation.php
+++
@@ -1,4 +0,0 @@
-<?php
-_deprecated_file( __FILE__, '4.0', 'Tribe__Abstract_Deactivation.php' );
-
-abstract class Tribe__Events__Abstract_Deactivation extends Tribe__Abstract_Deactivation {}
--- tribe-common/src/deprecated/Tribe__Events__Template_Part_Cache.php
+++
@@ -1,4 +0,0 @@
-<?php
-_deprecated_file( __FILE__, '4.0', 'Tribe__Template_Part_Cache.php' );
-
-class Tribe__Events__Template_Part_Cache extends Tribe__Template_Part_Cache {}

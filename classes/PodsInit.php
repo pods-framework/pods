b24ba4c23c683f1649b1d37d385fa688bc06ea99
Scott Kingsley Clark <scott@skc.dev>
2023-09-20 20:23:57 -0500

    Remove extra can_export

--- classes/PodsInit.php
+++ classes/PodsInit.php
@@ -1274,7 +1274,6 @@ class PodsInit {
 					'has_archive'         => ( (boolean) pods_v( 'has_archive', $post_type, false ) ) ? pods_v( 'has_archive_slug', $post_type, true, true ) : false,
 					'rewrite'             => $cpt_rewrite,
 					'query_var'           => ( false !== (boolean) pods_v( 'query_var', $post_type, true ) ? pods_v( 'query_var_string', $post_type, $post_type_name, true ) : false ),
-					'can_export'          => (boolean) pods_v( 'can_export', $post_type, true ),
 					'delete_with_user'    => (boolean) pods_v( 'delete_with_user', $post_type, true ),
 					'_provider'           => 'pods',
 				);

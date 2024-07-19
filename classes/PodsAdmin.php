3bb2fd25edbe91403d6bf5a83fbe532ecbdc6ad8
Scott Kingsley Clark <scott@skc.dev>
2024-07-19 07:37:02 -0500

    Fix access rights callout default

--- classes/PodsAdmin.php
+++ classes/PodsAdmin.php
@@ -2030,7 +2030,7 @@ class PodsAdmin {
 				'access_rights'     => (
 					PodsInit::$version_last
 					&& version_compare( PodsInit::$version_last, '3.1.0-a-1', '<' )
-				) ? 0 : 1,
+				) ? 1 : 0,
 			];
 
 			update_option( 'pods_callouts', $callouts );

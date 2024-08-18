c34720803d5f9e89c4e0038389cd531ff169b979
Scott Kingsley Clark <scott@skc.dev>
2024-08-18 08:36:24 -0500

    Wrap pagination in a p tag instead of just spans

--- ui/front/pagination/paginate.php
+++ ui/front/pagination/paginate.php
@@ -4,20 +4,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 ?>
-<span class="pods-pagination-paginate <?php echo esc_attr( $params->class ); ?>">
-	<?php
-	$args = array(
-		'base'      => $params->base,
-		'format'    => $params->format,
-		'total'     => $params->total,
-		'current'   => $params->page,
-		'end_size'  => $params->end_size,
-		'mid_size'  => $params->mid_size,
-		'prev_next' => $params->prev_next,
-		'prev_text' => $params->prev_text,
-		'next_text' => $params->next_text,
-	);
+<p class="pods-pagination">
+	<span class="pods-pagination-paginate <?php echo esc_attr( $params->class ); ?>">
+		<?php
+		$args = [
+			'base'      => $params->base,
+			'format'    => $params->format,
+			'total'     => $params->total,
+			'current'   => $params->page,
+			'end_size'  => $params->end_size,
+			'mid_size'  => $params->mid_size,
+			'prev_next' => $params->prev_next,
+			'prev_text' => $params->prev_text,
+			'next_text' => $params->next_text,
+		];
 
-	echo paginate_links( $args );
-	?>
-</span>
+		echo paginate_links( $args );
+		?>
+	</span>
+</p>

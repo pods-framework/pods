fb14490d9ecae2f8d31f96f93de16fcc42d83cc8
Scott Kingsley Clark <scott@skc.dev>
2024-09-07 23:06:12 -0500

    Tweak heading text

--- ui/admin/setup-add.php
+++ ui/admin/setup-add.php
@@ -156,7 +156,7 @@ $quick_actions = apply_filters( 'pods_admin_setup_add_quick_actions', $quick_act
 									<?php if ( ! empty( $quick_actions ) ) : ?>
 										<div
 											id='pods-wizard-quick-actions'<?php echo( $submit_from_linked ? ' class="hidden"' : '' ); ?>>
-											<h2 class="pods-wizard-one-click-actions-heading"><?php esc_html_e( 'One-Click Quick Extend', 'pods' ); ?></h2>
+											<h2 class="pods-wizard-one-click-actions-heading"><?php esc_html_e( 'One-Click Extend', 'pods' ); ?></h2>
 											<ul class="pods-wizard-one-click-actions">
 												<?php foreach ( $quick_actions as $quick_action_key => $quick_action ) : ?>
 													<li class="pods-wizard-one-click-action">

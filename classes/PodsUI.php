34e426abbc5e8cdde66534961e7d8b6350d99c19
Scott Kingsley Clark <scott@skc.dev>
2025-02-07 17:17:56 -0600

    Fix logic to match before and cast instead
    Signed-off-by: Scott Kingsley Clark <scott@skc.dev>

--- classes/PodsUI.php
+++ classes/PodsUI.php
@@ -3000,7 +3000,7 @@ class PodsUI {
 
 							echo PodsForm::submit_button( $this->header['search'], 'button', false, false, array( 'id' => $this->num_prefix . 'search' . $this->num . '-submit' ) );
 
-							if ( NULL === $this->search ) {
+							if ( 0 < strlen( (string) $this->search ) ) {
 								$clear_filters = array(
 									$this->num_prefix . 'search' . $this->num => false,
 								);

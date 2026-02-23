9dff00436160a86ec921ca19fa583d12f185b762
Scott Kingsley Clark <scott@skc.dev>
2026-02-22 18:09:18 -0600

    Update plugin header
    Signed-off-by: Scott Kingsley Clark <scott@skc.dev>

--- init.php
+++ init.php
@@ -16,7 +16,7 @@
  * Text Domain:       pods
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Requires at least: 6.0
+ * Requires at least: 6.3
  * Requires PHP:      7.2
  * GitHub Plugin URI: https://github.com/pods-framework/pods
  * Primary Branch:    main

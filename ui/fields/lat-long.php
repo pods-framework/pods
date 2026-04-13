e312c9767d09834eaf8f4ef062857212d015e06c
JoryHogeveen <info@keraweb.nl>
2026-04-14 01:48:11 +0200

    latlng formatting

--- ui/fields/lat-long.php
+++ ui/fields/lat-long.php
@@ -6,9 +6,9 @@ $attributes['tabindex'] = 2;
 $attributes             = PodsForm::merge_attributes( $attributes, $name, PodsForm::$field_type, $options );
 ?>
 <?php echo PodsForm::label( $name . '[lat]', 'Latitude' ); ?>
-<?php echo PodsForm::field( $name . '[lat]', pods_v( 'lat', $value ), 'number' ) ?>
+<?php echo PodsForm::field( $name . '[lat]', pods_v( 'lat', $value ), 'number', array( 'number_decimals' => 10, 'number_format' => '9999.99' ) ) ?>
 
 <?php echo PodsForm::label( $name . '[long]', 'Longitude' ); ?>
-<?php echo PodsForm::field( $name . '[long]', pods_v( 'long', $value ), 'number' ) ?>
+<?php echo PodsForm::field( $name . '[long]', pods_v( 'long', $value ), 'number', array( 'number_decimals' => 10, 'number_format' => '9999.99' ) ) ?>
 <?php
 PodsForm::regex( PodsForm::$field_type, $options );

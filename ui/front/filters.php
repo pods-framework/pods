ecce3060cfed8486eafe78e3e062b704e4571b3b
Scott Kingsley Clark <lol@scottkclark.com>
2026-06-07 22:38:29 -0500

    fix(filters): add WCAG labels to front-end filter form
    - Add visible <label> for each pick/taxonomy filter dropdown with 'Filter by {field_label}' text
    - Add visible label, placeholder, and aria-label to keyword search input
    - Labels customizable via existing pods_form_ui_label_text filter
    - Drop placeholder option when label visible (BC: returns when label suppressed)
    
    Fixes #7561

--- ui/front/filters.php
+++ ui/front/filters.php
@@ -19,23 +19,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php
 	foreach ( $fields as $name => $field ) {
 		if ( in_array( $field['type'], array( 'pick', 'taxonomy' ), true ) && 'pick-custom' !== $field['pick_object'] && ! empty( $field['pick_object'] ) ) {
-			$field['pick_format_type']             = 'single';
-			$field['pick_format_single']           = 'dropdown';
-			$field['pick_select_text']             = '-- ' . $field['label'] . ' --';
-			$field['pick_show_select_text']        = 1;
-			$field['pick_select_text_always_show'] = 1;
+			$filter_name  = $pod->filter_var . '_' . $name;
+			$filter_label = sprintf( __( 'Filter by %s', 'pods' ), $field['label'] );
 
-			$filter = sanitize_text_field( pods_v( $pod->filter_var . '_' . $name, 'get', '' ) );
+			$field['pick_format_type']       = 'single';
+			$field['pick_format_single']     = 'dropdown';
+			$field['pick_show_select_text']  = 0;
+
+			$filter = sanitize_text_field( pods_v( $filter_name, 'get', '' ) );
 
 			// @todo Support other field types.
 			$field['type'] = 'pick';
 
-			PodsForm::output_field( $pod->filter_var . '_' . $name, $filter, $field['type'], $field, $pod->pod, $pod->id() );
+			PodsForm::output_label( $filter_name, $filter_label, '', $field );
+			PodsForm::output_field( $filter_name, $filter, $field['type'], $field, $pod->pod, $pod->id() );
 		}
 	}
+
+	$search_id = 'pods-form-filters-search-' . sanitize_key( $pod->pod );
 	?>
 
-	<input type="text" class="pods-form-filters-search" name="<?php echo esc_attr( $pod->search_var ); ?>" value="<?php echo esc_attr( $search ); ?>" />
+	<label for="<?php echo esc_attr( $search_id ); ?>" class="pods-form-filters-label"><?php esc_html_e( 'Search', 'pods' ); ?></label>
+	<input type="text" id="<?php echo esc_attr( $search_id ); ?>" class="pods-form-filters-search" name="<?php echo esc_attr( $pod->search_var ); ?>" value="<?php echo esc_attr( $search ); ?>" placeholder="<?php esc_attr_e( 'Search…', 'pods' ); ?>" aria-label="<?php esc_attr_e( 'Search', 'pods' ); ?>" />
 
 	<input type="submit" class="pods-form-filters-submit" value="<?php echo esc_attr( $label ); ?>" />
 </form>

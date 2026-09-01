b977d7bbda49cd8b60881ff15654434a21bff337
Scott Kingsley Clark <lol@scottkclark.com>
2026-09-01 12:14:35 -0500

    Remove Plugin ID from init.php header
    Removed the Plugin ID from the header comments.
    
    Signed-off-by: Scott Kingsley Clark <lol@scottkclark.com>

--- init.php
+++ init.php
@@ -20,7 +20,6 @@
  * Requires PHP:      7.2
  * GitHub Plugin URI: https://github.com/pods-framework/pods
  * Primary Branch:    main
- * Plugin ID:         did:plc:e3rm6t7cspgpzaf47kn3nnsl
  */
 
 /*
